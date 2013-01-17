a38a4afe2bf09a57633cbe3aed014175be7d57ac
Robotboy655 <robotboy655@gmail.com>
2013-01-17 21:45:26 +0200

    Added map categories for most mountable games
    Zeno clash and Left 4 Dead 1 are not added, since I don't have them.
    Alien Swarm and Left 4 Dead 2 are added, even though they aren't
    mountable.
    Reorganized the file a bit.
    Added ability to specify single maps for categories.
    
    Oh and by the way, is matNoIcon even used?

--- garrysmod/lua/menu/getmaps.lua
+++ garrysmod/lua/menu/getmaps.lua
@@ -10,6 +10,7 @@ local function UpdateMapPatterns()
 	MapPatterns[ "^de_" ] = "Counter-Strike"
 	MapPatterns[ "^cs_" ] = "Counter-Strike"
 	MapPatterns[ "^es_" ] = "Counter-Strike"
+
 	MapPatterns[ "^cp_" ] = "Team Fortress 2"
 	MapPatterns[ "^ctf_" ] = "Team Fortress 2"
 	MapPatterns[ "^mvm_" ] = "Team Fortress 2"
@@ -20,6 +21,7 @@ local function UpdateMapPatterns()
 	MapPatterns[ "^plr_" ] = "Team Fortress 2"
 	MapPatterns[ "^arena_" ] = "Team Fortress 2"
 	MapPatterns[ "^koth_" ] = "Team Fortress 2"
+	MapPatterns[ "itemtest" ] = "Team Fortress 2"
 
 	MapPatterns[ "^dod_" ] = "Day Of Defeat"
 
@@ -27,6 +29,7 @@ local function UpdateMapPatterns()
 	MapPatterns[ "^d2_" ] = "Half-Life 2"
 	MapPatterns[ "^d3_" ] = "Half-Life 2"
 	MapPatterns[ "credits" ] = "Half-Life 2"
+	MapPatterns[ "intro" ] = "Half-Life 2"
 
 	MapPatterns[ "^ep1_" ] = "Half-Life 2: Episode 1"
 	MapPatterns[ "^ep2_" ] = "Half-Life 2: Episode 2"
@@ -34,8 +37,6 @@ local function UpdateMapPatterns()
 
 	MapPatterns[ "^escape_" ] = "Portal"
 	MapPatterns[ "^testchmb_" ] = "Portal"
-
-	MapPatterns[ "^phys_" ] = "Physics Maps"
 
 	MapPatterns[ "^c0a" ] = "Half-Life: Source"
 	MapPatterns[ "^c1a" ] = "Half-Life: Source"
@@ -57,45 +58,74 @@ local function UpdateMapPatterns()
 	MapPatterns[ "undertow" ] = "Half-Life: Source Deathmatch"
 	MapPatterns[ "lambda_bunker" ] = "Half-Life: Source Deathmatch"
 
-	MapPatterns[ "dm_" ] = "Half-Life 2 Deathmatch"
-
 	MapPatterns[ "^dys_" ] = "Dystopia"
+	MapPatterns[ "^pb_" ] = "Dystopia"
+	MapPatterns[ "dys_lobby" ] = "Dystopia"
+	
+	MapPatterns[ "^dm_" ] = "Half-Life 2 Deathmatch"
 	MapPatterns[ "^ins_" ] = "Insurgency"
 	MapPatterns[ "^aoc_" ] = "Age Of Chivalry"
 	MapPatterns[ "^mp_coop_" ] = "Portal 2"
+
 	MapPatterns[ "^ur_" ] = "DIPRIP"
+	MapPatterns[ "dm_village" ] = "DIPRIP"
+	MapPatterns[ "dm_supermarket" ] = "DIPRIP"
+	MapPatterns[ "dm_refinery" ] = "DIPRIP"
+	MapPatterns[ "dm_dam" ] = "DIPRIP"
+	MapPatterns[ "dm_city" ] = "DIPRIP"
+	MapPatterns[ "de_dam" ] = "DIPRIP"
+
 	MapPatterns[ "^tw_" ] = "Pirates, Vikings and Knights II"
 	MapPatterns[ "^bt_" ] = "Pirates, Vikings and Knights II"
+	MapPatterns[ "^lts_" ] = "Pirates, Vikings and Knights II"
+	MapPatterns[ "^te_" ] = "Pirates, Vikings and Knights II"
+
 	MapPatterns[ "^zps_" ] = "Zombie Panic! Source"
+	MapPatterns[ "^zpa_" ] = "Zombie Panic! Source"
+	MapPatterns[ "^zpo_" ] = "Zombie Panic! Source"
+	MapPatterns[ "^zpl_" ] = "Zombie Panic! Source"
+	
+	MapPatterns[ "^phys_" ] = "Physics Maps"
 	MapPatterns[ "^rp_" ] = "Roleplay"
 	MapPatterns[ "^zs_" ] = "Zombie Survival"
 	MapPatterns[ "^jb_" ] = "Jailbreak"
 	MapPatterns[ "^ba_jail_" ] = "Jailbreak"
 	MapPatterns[ "^deathrun_" ] = "Deathrun"
+	MapPatterns[ "^dr_" ] = "Deathrun"
 	MapPatterns[ "^sb_" ] = "Spacebuild"
 
+	MapPatterns[ "^asi-" ] = "Alien Swarm"
+	MapPatterns[ "lobby" ] = "Alien Swarm"
+	
+	MapPatterns[ "tutorial_standards" ] = "Left 4 Dead 2"
+	MapPatterns[ "tutorial_standards_vs" ] = "Left 4 Dead 2"
+	MapPatterns[ "^c1m" ] = "Left 4 Dead 2"
+	MapPatterns[ "^c2m" ] = "Left 4 Dead 2"
+	MapPatterns[ "^c3m" ] = "Left 4 Dead 2"
+	MapPatterns[ "^c4m" ] = "Left 4 Dead 2"
+	MapPatterns[ "^c5m" ] = "Left 4 Dead 2"
+	MapPatterns[ "^c6m" ] = "Left 4 Dead 2" -- DLCs
+	MapPatterns[ "^c7m" ] = "Left 4 Dead 2"
+	MapPatterns[ "^c8m" ] = "Left 4 Dead 2"
+	MapPatterns[ "^c9m" ] = "Left 4 Dead 2"
+	MapPatterns[ "^c10m" ] = "Left 4 Dead 2"
+	MapPatterns[ "^c11m" ] = "Left 4 Dead 2"
+	MapPatterns[ "^c12m" ] = "Left 4 Dead 2"
+	MapPatterns[ "^c13m" ] = "Left 4 Dead 2"
+	
 	local GamemodeList = engine.GetGamemodes()
 
 	for k, gm in pairs( GamemodeList ) do
-
-		local Name			= gm.title or "Unnammed Gamemode"
-		local Maps			= string.Split( gm.maps, "|" )
+		local Name = gm.title or "Unnammed Gamemode"
+		local Maps = string.Split( gm.maps, "|" )
 
 		if ( Maps && gm.maps != "" ) then
-	
 			for k, pattern in pairs( Maps ) do
 				MapPatterns[ pattern ] = Name
 			end
-
 		end
-	
 	end
-
 end
-
-
-
-
 
 local IgnoreMaps = { "background", "^test_", "^styleguide", "^devtest" }
 
@@ -119,13 +149,12 @@ local function RefreshMaps()
 		
 		-- Don't add useless maps
 		if ( !Ignore ) then
-		
 			local Mat = nil
 			local Category = "Other"
 			local name = string.gsub( v, ".bsp", "" )
 			local lowername = string.lower( v )
 		
-			Mat = "../maps/"..name..".png"
+			Mat = "../maps/" .. name .. ".png"
 			
 			for pattern, category in pairs( MapPatterns ) do
 				if ( string.find( lowername, pattern ) ) then
@@ -133,32 +162,23 @@ local function RefreshMaps()
 				end
 			end
 
+			if ( MapPatterns[ name ] ) then Category = MapPatterns[ name ] end
+			
 			g_MapList[ v ] = { Material = Mat, Name = name, Category = Category }
-						 
 		end
-
 	end
 
 	for k, v in pairs( g_MapList ) do
-
 		g_MapListCategorised[ v.Category ] = g_MapListCategorised[ v.Category ] or {}
 		g_MapListCategorised[ v.Category ][ v.Name ] = v
 
 	end
-
-
 end
 
-
 hook.Add( "MenuStart", "FindMaps", function()
-
 	RefreshMaps()
-
 end )
 
-
 hook.Add( "GameContentChanged", "RefreshMaps", function()
-
 	RefreshMaps()
-
 end )
