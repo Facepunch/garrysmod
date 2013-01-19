a6fe09e6e5d4cb08360fbb1b1d1165294355b036
Cloud Chiller <steamruler@gmail.com>
2013-01-19 23:42:41 +0100

    Scaled down cl_weaponcolor to be max 1, like it should be.

--- garrysmod/gamemodes/sandbox/gamemode/player_class/player_sandbox.lua
+++ garrysmod/gamemodes/sandbox/gamemode/player_class/player_sandbox.lua
@@ -5,7 +5,7 @@ DEFINE_BASECLASS( "player_default" )
 if ( CLIENT ) then
 
 	CreateConVar( "cl_playercolor", "0.24 0.34 0.41", { FCVAR_ARCHIVE, FCVAR_USERINFO, FCVAR_DONTRECORD }, "The value is a Vector - so between 0-1 - not between 0-255" )
-	CreateConVar( "cl_weaponcolor", "0.30 1.80 2.10", { FCVAR_ARCHIVE, FCVAR_USERINFO, FCVAR_DONTRECORD }, "The value is a Vector - so between 0-1 - not between 0-255" )
+	CreateConVar( "cl_weaponcolor", "0.14 0.85 1", { FCVAR_ARCHIVE, FCVAR_USERINFO, FCVAR_DONTRECORD }, "The value is a Vector - so between 0-1 - not between 0-255" )
 
 end
 
