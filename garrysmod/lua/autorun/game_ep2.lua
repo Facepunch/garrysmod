bdd6b7e7768807be5f50471e8f20031736446fed
Robotboy655 <robotboy655@gmail.com>
2013-01-29 10:48:19 +0200

    Added Material field for custom NPCs
    Also added EP2 Antlion Guardian (the yellow one) as an example.

--- garrysmod/lua/autorun/game_ep2.lua
+++ garrysmod/lua/autorun/game_ep2.lua
@@ -36,6 +36,7 @@ Category = "Zombies + Enemy Aliens"
 	list.Set( "NPC",  "npc_zombine",		{ Name = "Zombine",				Class = "npc_zombine",				Category = Category	} )
 	list.Set( "NPC", "npc_antlion_worker",	{ Name = "Antlion Worker",		Class = "npc_antlion_worker",		Category = Category	} )
 	list.Set( "NPC", "npc_antlion_grub",	{ Name = "Antlion Grub",		Class = "npc_antlion_grub",			Category = Category,	NoDrop = true, Offset = 1 } )
+	list.Set( "NPC", "npc_antlionguardian", { Name = "Antlion Guardian",	Class = "npc_antlionguard",			Category = Category,	KeyValues = { cavernbreed = 1, incavern = 1 },	Material = "Models/antlion_guard/antlionGuard2" } )
 
 Category = "Combine"
 	list.Set( "NPC", "npc_hunter",			{ Name = "Hunter", Class = "npc_hunter", Category = Category } )
