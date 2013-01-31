e6612d0c005a2ef1680b94dbd0dcb42e081fac56
Lexi R <lexi@lexi.org.uk>
2013-01-31 16:28:17 +0100

    Added some boilerplate functions to base_entity
    Added RemoveIfInvalidPhysics() to ensure your SEnt is physical
    Added WakeIfValidPhysics() to wake your SEnt if it's physical but not to
     do anything if it isn't (Not particularly useful imo but lots of people
     use this particular bit of boilerplate so what can you do?)
    Added WakePhysics() as a shorthand for self:GetPhysicsObject():Wake()

--- garrysmod/gamemodes/base/entities/entities/base_entity/init.lua
+++ garrysmod/gamemodes/base/entities/entities/base_entity/init.lua
@@ -25,7 +25,7 @@ end
 --[[---------------------------------------------------------
    Name: OnRestore
    Desc: The game has just been reloaded. This is usually the right place
-		to call the GetNetworked* functions to restore the script's values.
+      to call the GetNetworked* functions to restore the script's values.
 -----------------------------------------------------------]]
 function ENT:OnRestore()
 end
@@ -36,7 +36,7 @@ end
    Desc: Accepts input, return true to override/accept input
 -----------------------------------------------------------]]
 function ENT:AcceptInput( name, activator, caller, data )
-	return false
+   return false
 end
 
 
@@ -45,7 +45,7 @@ end
    Desc: Set the transmit state
 -----------------------------------------------------------]]
 function ENT:UpdateTransmitState()
-	return TRANSMIT_PVS
+   return TRANSMIT_PVS
 end
 
 
@@ -54,4 +54,37 @@ end
    Desc: Entity's think function. 
 -----------------------------------------------------------]]
 function ENT:Think()
+end
+
+--[[---------------------------------------------------------
+   Name: RemoveIfInvalidPhysics
+   Desc: Deletes the entity and prints an error if it has no valid physobj
+-----------------------------------------------------------]]
+function ENT:RemoveIfInvalidPhysics()
+   local PhysObj = self:GetPhysicsObject()
+   if ( IsValid( PhysObj ) ) then
+      return
+   end
+   local Model = self:GetModel()
+   self:Remove()
+   error( "No Physics Object available for entity '" .. self.ClassName .. "'! Do you have the model '" .. Model .. "' installed?", 2 )
+end
+
+--[[---------------------------------------------------------
+   Name: WakeIfValidPhysics
+   Desc: If the entity has a valid physics object, wake it.
+-----------------------------------------------------------]]
+function ENT:WakeIfValidPhysics()
+   local PhysObj = self:GetPhysicsObject()
+   if ( IsValid( PhysObj ) ) then
+      PhysObj:Wake()
+   end
+end
+
+--[[---------------------------------------------------------
+   Name: WakePhysics
+   Desc: Wake the entity's physobj without checking if it's valid first
+-----------------------------------------------------------]]
+function ENT:WakePhysics()
+   self:GetPhysicsObject():Wake()
 end
