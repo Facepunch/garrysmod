2138c1f3c18b2492341183416989d31f248976c6
garrynewman <garrynewman@gmail.com>
2013-04-25 09:25:18 -0700

    DOD hands

--- garrysmod/lua/autorun/game_dod.lua
+++ garrysmod/lua/autorun/game_dod.lua
@@ -5,7 +5,9 @@ if ( !IsMounted( "dod" ) ) then return end
 ---Americans
 list.Set( "PlayerOptionsModel", "american_assault", "models/player/dod_american.mdl" )
 player_manager.AddValidModel( "american_assault", "models/player/dod_american.mdl" )
+player_manager.AddValidHands( "american_assault",			"models/weapons/c_arms_dod.mdl",		0,		"10000000" )
 
 ---German
 list.Set( "PlayerOptionsModel", "german_assault", "models/player/dod_german.mdl" )
 player_manager.AddValidModel( "german_assault", "models/player/dod_german.mdl" )
+player_manager.AddValidHands( "german_assault",			"models/weapons/c_arms_dod.mdl",		1,		"10000000" )
