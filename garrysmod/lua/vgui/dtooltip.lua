c7f4593346c6ae6c48251bcad32233fc17d1517f
Josh <acecoolcompany@gmail.com>
2013-05-02 11:06:37 -0300

    Update dtooltip.lua
    Kept spaces, edited the font, and added logic to keep tool-tip on screen.

--- garrysmod/lua/vgui/dtooltip.lua
+++ garrysmod/lua/vgui/dtooltip.lua
@@ -14,61 +14,81 @@ local tooltip_delay = CreateClientConVar( "tooltip_delay", "0.5", true, false )
 
 PANEL = {}
 
+
 --[[---------------------------------------------------------
 
 -----------------------------------------------------------]]
 function PANEL:Init()
+
 	self:SetDrawOnTop( true )
 	self.DeleteContentsOnClose = false
 	self:SetText( "" )
 	self:SetFont( "Default" )
+
 end
 
 --[[---------------------------------------------------------
 	UpdateColours
 -----------------------------------------------------------]]
 function PANEL:UpdateColours( skin )
+
 	return self:SetTextStyleColor( skin.Colours.TooltipText )
+
 end
 
 --[[---------------------------------------------------------
 
 -----------------------------------------------------------]]
 function PANEL:SetContents( panel, bDelete )
+
 	panel:SetParent( self )
+
 	self.Contents = panel
 	self.DeleteContentsOnClose = bDelete or false	
 	self.Contents:SizeToContents()
 	self:InvalidateLayout( true )
+	
 	self.Contents:SetVisible( false )
+
 end
 
 --[[---------------------------------------------------------
 
 -----------------------------------------------------------]]
 function PANEL:PerformLayout()
+
 	if ( self.Contents ) then
+	
 		self:SetWide( self.Contents:GetWide() + 8 )
 		self:SetTall( self.Contents:GetTall() + 8 )
 		self.Contents:SetPos( 4, 4 )
+		
 	else
+	
 		local w, h = self:GetContentSize()
 		self:SetSize( w + 8, h + 6 )
 		self:SetContentAlignment( 5 )
+	
 	end
+
 end
+
 local Mat = Material( "vgui/arrow" )
 
 --[[---------------------------------------------------------
 
 -----------------------------------------------------------]]
 function PANEL:DrawArrow( x, y )
+
 	self.Contents:SetVisible( true )
+	
 	surface.SetMaterial( Mat )	
 	surface.DrawTexturedRect( self.ArrowPosX+x, self.ArrowPosY+y, self.ArrowWide, self.ArrowTall )
+
 end
 
 function PANEL:PositionTooltip()
+
 	if ( !IsValid( self.TargetPanel ) ) then
 		self:Remove()
 		return;
@@ -88,44 +108,57 @@ function PANEL:PositionTooltip()
 	
 	// Fixes being able to be drawn off screen - Acecool
 	self:SetPos( math.Clamp( x - w * 0.5, 0, ScrW( ) - self:GetWide( ) ), math.Clamp( y, 0, ScrH( ) - self:GetTall( ) ) )
+
 end
 
 --[[---------------------------------------------------------
 
 -----------------------------------------------------------]]
 function PANEL:Paint( w, h )
+
 	self:PositionTooltip();
 	derma.SkinHook( "Paint", "Tooltip", self, w, h )
+
 end
 
 --[[---------------------------------------------------------
 
 -----------------------------------------------------------]]
 function PANEL:OpenForPanel( panel )
+	
 	self.TargetPanel = panel
 	self:PositionTooltip();	
+	
 	if ( tooltip_delay:GetFloat() > 0 ) then
+	
 		self:SetVisible( false )
 		timer.Simple( tooltip_delay:GetFloat(), function() 
-			if ( !IsValid( self ) ) then return end
-			if ( !IsValid( panel ) ) then return end
+		
+													if ( !IsValid( self ) ) then return end
+													if ( !IsValid( panel ) ) then return end
 
-			self:PositionTooltip();	
-			self:SetVisible( true )
-		end )
+													self:PositionTooltip();	
+													self:SetVisible( true )
+												
+												end )
 	end
+
 end
 
 --[[---------------------------------------------------------
 
 -----------------------------------------------------------]]
 function PANEL:Close()
+
 	if ( !self.DeleteContentsOnClose && self.Contents ) then
+	
 		self.Contents:SetVisible( false )
 		self.Contents:SetParent( nil )
+	
 	end
 	
 	self:Remove()
+
 end
 
 
@@ -133,12 +166,15 @@ end
    Name: GenerateExample
 -----------------------------------------------------------]]
 function PANEL:GenerateExample( ClassName, PropertySheet, Width, Height )
+
 	local ctrl = vgui.Create( "DButton" )
-	ctrl:SetText( "Hover me" )
-	ctrl:SetWide( 200 )
-	ctrl:SetTooltip( "This is a tooltip" )
+		ctrl:SetText( "Hover me" )
+		ctrl:SetWide( 200 )
+		ctrl:SetTooltip( "This is a tooltip" )
 	
 	PropertySheet:AddSheet( ClassName, ctrl, nil, true, true )
+
 end
+
 
 derma.DefineControl( "DTooltip", "", PANEL, "DLabel" )
