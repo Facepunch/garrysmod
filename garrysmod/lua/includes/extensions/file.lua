9cfd592a42945733bca1ed8f76765043b83b3419
garrynewman <garrynewman@gmail.com>
2013-06-08 13:09:37 -0700

    Previously it always read/wrote in binary mode anyway. So lets use that functionality in these functions.

--- garrysmod/lua/includes/extensions/file.lua
+++ garrysmod/lua/includes/extensions/file.lua
@@ -6,7 +6,7 @@ function file.Read( filename, path )
 	if ( path == true ) then path = "GAME" end
 	if ( path == nil || path == false ) then path = "DATA" end
 
-	local f = file.Open( filename, "r", path )
+	local f = file.Open( filename, "rb", path )
 	if ( !f ) then return end
 
 	local str = f:Read( f:Size() )
@@ -20,7 +20,7 @@ end
 
 function file.Write( filename, contents )
 
-	local f = file.Open( filename, "w", "DATA" )
+	local f = file.Open( filename, "wb", "DATA" )
 	if ( !f ) then return end
 
 	f:Write( contents )
@@ -30,7 +30,7 @@ end
 
 function file.Append( filename, contents )
 
-	local f = file.Open( filename, "a", "DATA" )
+	local f = file.Open( filename, "ab", "DATA" )
 	if ( !f ) then return end
 
 	f:Write( contents )
