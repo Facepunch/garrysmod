2de891370139df6fb393e045c1b9394efbdb9b54
Stefan van der Meer <stefanvandermeer@gmail.com>
2013-06-09 10:52:14 +0200

    TTT: Possible fix for player colors resetting
    Getting reports of colors resetting when round starts. Can't reproduce
    it, but this may help.

--- garrysmod/gamemodes/terrortown/gamemode/player_ext.lua
+++ garrysmod/gamemodes/terrortown/gamemode/player_ext.lua
@@ -268,6 +268,8 @@ end
 -- Preps a player for a new round, spawning them if they should. If dead_only is
 -- true, only spawns if player is dead, else just makes sure he is healed.
 function plymeta:SpawnForRound(dead_only)
+   GAMEMODE:PlayerSetModel(self)
+
    -- wrong alive status and not a willing spec who unforced after prep started
    -- (and will therefore be "alive")
    if dead_only and self:Alive() and (not self:IsSpec()) then
