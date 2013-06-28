cc26f901a6306af253610960fa89984bd2a4dbbe
Dessi <MegaDeath409@gmail.com>
2013-06-28 00:46:26 -0400

    Added Variadic Return capabilities to hooks Used anonymous function encapsulation in order to keep the setRet closure static while preventing its recreation on every call Moved gamemode-fetch lines to just above where they are needed in order to reduce unnecessary calls to gmod.GetGamemode when gamemode hooks are overridden

--- garrysmod/lua/includes/modules/hook.lua
+++ garrysmod/lua/includes/modules/hook.lua
@@ -4,6 +4,7 @@ local pairs			= pairs
 local isfunction	= isfunction
 local isstring		= isstring
 local IsValid		= IsValid
+local unpack = unpack
 
 module( "hook" )
 
@@ -50,74 +51,82 @@ function Run( name, ... )
 	return Call( name, nil, ... )
 end
 
+
+
 --
 -- Called by the engine
+-- Call( name, gamemode, ... )
 --
-function Call( name, gm, ... )
-
+Call = (function()
 	local ret
-
-	--
-	-- If called from hook.Run then gm will be nil.
-	--
-	if ( gm == nil && gmod != nil ) then
-		gm = gmod.GetGamemode()
-	end
-
-	--
-	-- Run hooks
-	--
-	local HookTable = Hooks[ name ]
-	if ( HookTable != nil ) then
-	
-		local a, b, c, d, e, f;
-
-		for k, v in pairs( HookTable ) do 
-			
-			if ( isstring( k ) ) then
-				
-				--
-				-- If it's a string, it's cool
-				--
-				a, b, c, d, e, f = v( ... )
-
-			else
-
-				--
-				-- If the key isn't a string - we assume it to be an entity
-				-- Or panel, or something else that IsValid works on.
-				--
-				if ( IsValid( k ) ) then
-					--
-					-- If the object is valid - pass it as the first argument (self)
-					--
-					a, b, c, d, e, f = v( k, ... )
-				else
-					--
-					-- If the object has become invalid - remove it
-					--
-					HookTable[ k ] = nil
-				end
-			end
-
-			--
-			-- Hook returned a value - it overrides the gamemode function
-			--
-			if ( a != nil ) then
-				return a, b, c, d, e, f
-			end
-			
+	local function setRet(first, ...)
+		if(first ~= nil)then
+			return
 		end
+		ret = { first, ... }
 	end
-	
-	--
-	-- Call the gamemode function
-	--
-	if ( !gm ) then return end
-	
-	local GamemodeFunction = gm[ name ]
-	if ( GamemodeFunction == nil ) then return end
+	return function( name, gm, ... )
+		--
+		-- Run hooks
+		--
+		local HookTable = Hooks[ name ]
+		if ( HookTable != nil ) then
+			ret = nil
+
+			for k, v in pairs( HookTable ) do 
+				
+				if ( isstring( k ) ) then
+					
+					--
+					-- If it's a string, it's cool
+					--
+					setRet( v( ... ) )
+
+				else
+
+					--
+					-- If the key isn't a string - we assume it to be an entity
+					-- Or panel, or something else that IsValid works on.
+					--
+					if ( IsValid( k ) ) then
+						--
+						-- If the object is valid - pass it as the first argument (self)
+						--
+						setRet( v( k, ... ) )
+					else
+						--
+						-- If the object has become invalid - remove it
+						--
+						HookTable[ k ] = nil
+					end
+				end
+
+				--
+				-- Hook returned a value - it overrides the gamemode function
+				--
+				if ( ret ) then
+					return unpack( ret )
+				end
+				
+			end
+		end
+
+		--
+		-- If called from hook.Run then gm will be nil.
+		--
+		if ( gm == nil && gmod != nil ) then
+			gm = gmod.GetGamemode()
+		end
 		
-	return GamemodeFunction( gm, ... )	
-	
-end
+		--
+		-- Call the gamemode function
+		--
+		if ( !gm ) then return end
+		
+		local GamemodeFunction = gm[ name ]
+		if ( GamemodeFunction == nil ) then return end
+			
+		return GamemodeFunction( gm, ... )	
+		
+	end
+end)()
