b666e09eb6d7f9d3a41a92469133edbdb5d575fe
Robotboy655 <robotboy655@gmail.com>
2013-10-27 15:15:46 +0200

    Fixes for slider tool
    * Fixed double left click on world which prevented future tool use
    unless rightclicked
    * Fixed console error spam about gmod_anchor not being AddCSLuaFile'd

---
+++ garrysmod/gamemodes/sandbox/entities/entities/gmod_anchor.lua
@@ -0,0 +1,28 @@
+
+AddCSLuaFile()
+
+ENT.Type = "anim"
+
+if ( CLIENT ) then return end
+
+function ENT:Initialize()
+
+
+	self:SetModel( "models/Combine_Helicopter/helicopter_bomb01.mdl" ) 
+	self:PhysicsInit( SOLID_VPHYSICS )
+	
+	self:SetNotSolid( true )
+	self:SetNoDraw( true )
+	self:DrawShadow( false )
+	
+	local Phys = self:GetPhysicsObject()
+	if ( Phys ) then
+	
+		Phys:EnableMotion( false )
+		Phys:EnableCollisions( false )
+		
+	end
+	
+	self:SetUnFreezable( true )
+	
+end
