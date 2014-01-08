59b2fa5c046676860e64eabd61bac3e0b87d1d59
Matt Stevens <matt@dev-zilla.net>
2014-01-08 20:58:26 +0000

    Move ply.IsFullyAuthed to where it sets usergroups. Prevents ply:Is/GetUserGroup() returning a bad result, also gives admin mods more flexability if they want to deal with non authenticated players differently.

--- garrysmod/lua/includes/extensions/player_auth.lua
+++ garrysmod/lua/includes/extensions/player_auth.lua
@@ -5,14 +5,11 @@ if not meta then return end
     Name: IsAdmin
     Desc: Returns if a player is an admin.
 -----------------------------------------------------------]]
-function meta:IsAdmin()
-	-- Admin SteamID need to be fully authenticated by Steam!
-	if self.IsFullyAuthenticated and not self:IsFullyAuthenticated() then return false end
-	
-	if self:IsSuperAdmin() then return true end
-	if self:IsUserGroup("admin") then return true end
+function meta:IsAdmin() 
+    if self:IsSuperAdmin() then return true end
+    if self:IsUserGroup("admin") then return true end
 
-	return false
+    return false
 end
 
 --[[---------------------------------------------------------
@@ -20,10 +17,7 @@ end
     Desc: Returns if a player is a superadmin.
 -----------------------------------------------------------]]
 function meta:IsSuperAdmin()
-	-- Admin SteamID need to be fully authenticated by Steam!
-	if self.IsFullyAuthenticated and not self:IsFullyAuthenticated() then return false end
-
-	return self:IsUserGroup("superadmin")
+    return self:IsUserGroup("superadmin")
 end
 
 --[[---------------------------------------------------------
@@ -31,9 +25,9 @@ end
     Desc: Returns if a player is in the specified usergroup.
 -----------------------------------------------------------]]
 function meta:IsUserGroup(name)
-	if not self:IsValid() then return false end
+    if not self:IsValid() then return false end
 
-	return self:GetNetworkedString("UserGroup") == name
+    return self:GetNetworkedString("UserGroup") == name
 end
 
 --[[---------------------------------------------------------
@@ -41,7 +35,7 @@ end
     Desc: Sets the player's usergroup. ( Serverside Only )
 -----------------------------------------------------------]]
 function meta:SetUserGroup(name)
-	self:SetNetworkedString("UserGroup", name)
+    self:SetNetworkedString("UserGroup", name)
 end
 
 --[[---------------------------------------------------------
@@ -49,7 +43,7 @@ end
     Desc: Returns the player's usergroup.
 -----------------------------------------------------------]]
 function meta:GetUserGroup()
-	return self:GetNetworkedString("UserGroup", "user")
+    return self:GetNetworkedString("UserGroup", "user")
 end
 
 
@@ -61,8 +55,8 @@ if not SERVER then return end
 
 -- SteamIds table..
 -- STEAM_0:1:7099:
---	 	 name	=	garry
---	 	 group	=	superadmin
+--     name   =   garry
+--     group  =   superadmin
 local SteamIDs = {}
 
 -- Load the users file
@@ -70,27 +64,35 @@ local UsersKV = util.KeyValuesToTable( file.Read( "settings/users.txt", "GAME" )
 
 -- Extract the data into the SteamIDs table
 for key, tab in pairs( UsersKV ) do
-	for name, steamid in pairs( tab ) do
-		SteamIDs[ steamid ] = {}
-		SteamIDs[ steamid ].name = name
-		SteamIDs[ steamid ].group = key
-	end
+    for name, steamid in pairs( tab ) do
+        SteamIDs[ steamid ] = {}
+        SteamIDs[ steamid ].name = name
+        SteamIDs[ steamid ].group = key
+    end
 end
 
 hook.Add("PlayerInitialSpawn", "PlayerAuthSpawn", function(ply)
-	local steamid = ply:SteamID()
-	
-	if game.SinglePlayer() or ply:IsListenServerHost() then
-		ply:SetUserGroup("superadmin")
-		return
-	end
-	
-	if SteamIDs[steamid] == nil then
-		ply:SetUserGroup("user")
-		return
-	end
+    local steamid = ply:SteamID()
+    
+    if game.SinglePlayer() or ply:IsListenServerHost() then
+        ply:SetUserGroup("superadmin")
+        return
+    end
+    
+    if SteamIDs[steamid] == nil then
+        ply:SetUserGroup("user")
+        return
+    end
 
-	ply:SetUserGroup(SteamIDs[steamid].group)
-	ply:ChatPrint(string.format("Hey '%s' - You're in the '%s' group on this server.",
-		SteamIDs[steamid].name, SteamIDs[steamid].group))
+    -- Admin SteamID need to be fully authenticated by Steam!
+    if ply.IsFullyAuthenticated and not ply:IsFullyAuthenticated() then
+        ply:ChatPrint(string.format("Hey '%s' - Your SteamID wasn't fully authenticated, so your usergroup has not been set to '%s.'",
+            SteamIDs[steamid].name, SteamIDs[steamid].group))
+        ply:ChatPrint("Try restarting Steam.")
+        return
+    end
+
+    ply:SetUserGroup(SteamIDs[steamid].group)
+    ply:ChatPrint(string.format("Hey '%s' - You're in the '%s' group on this server.",
+        SteamIDs[steamid].name, SteamIDs[steamid].group))
 end)
