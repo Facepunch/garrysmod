7068cff60513acc770ab9984ac3d2f537499a0d9
Robotboy655 <robotboy655@gmail.com>
2014-02-22 22:19:34 +0200

    table.GetKeys PERFORMANCE

--- garrysmod/lua/includes/extensions/table.lua
+++ garrysmod/lua/includes/extensions/table.lua
@@ -740,9 +740,11 @@ end
 function table.GetKeys( tab )
 	
 	local keys = {}
+	local id = 1
 
 	for k, v in pairs( tab ) do
-		table.insert( keys, k )
+		keys[ id ] = k
+		id = id + 1
 	end
 	
 	return keys
