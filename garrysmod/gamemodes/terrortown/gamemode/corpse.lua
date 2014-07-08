7546a3a38f0f7a34e0cb63ef96e09939f9618fe7
CallMePyro <jacobasmuth@gmail.com>
2014-07-08 14:12:06 -0700

    Update corpse.lua

--- garrysmod/gamemodes/terrortown/gamemode/corpse.lua
+++ garrysmod/gamemodes/terrortown/gamemode/corpse.lua
@@ -80,7 +80,7 @@ local function IdentifyBody(ply, rag)
 
          SCORE:HandleBodyFound(ply, deadply)
       end
-	hook.Call( "TTTBodyFound", GAMEMODE, ply, deadply, rag )
+      hook.Call( "TTTBodyFound", GAMEMODE, ply, deadply, rag )
       CORPSE.SetFound(rag, true)
    else
       -- re-set because nwvars are unreliable
