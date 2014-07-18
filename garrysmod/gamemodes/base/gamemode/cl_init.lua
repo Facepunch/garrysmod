6656fa94e7d71217d105bb8868098ff48dd841fb
UselessGhost <excellentdesu@gmail.com>
2014-07-17 18:19:16 -0700

    Add hooks to modify rendering of hands

--- garrysmod/gamemodes/base/gamemode/cl_init.lua
+++ garrysmod/gamemodes/base/gamemode/cl_init.lua
@@ -604,7 +604,10 @@ function GM:PostDrawViewModel( ViewModel, Player, Weapon )
 
 		local hands = Player:GetHands()
 		if ( IsValid( hands ) ) then
-			hands:DrawModel()
+			if ( not hook.Call( "PreDrawPlayerHands", self, Hands, ViewModel, Player, Weapon ) ) then
+				hands:DrawModel()
+				hook.Call( "PostDrawPlayerHands", self, Hands, ViewModel, Player, Weapon )
+			end
 		end
 
 	end
