5b20d3b2a2fa9dbc874abd30ecbc014d2f3f374c
Robotboy655 <robotboy655@gmail.com>
2014-08-07 08:23:01 +0300

    Minor code changes

--- garrysmod/gamemodes/sandbox/entities/weapons/gmod_tool/stools/nocollide.lua
+++ garrysmod/gamemodes/sandbox/entities/weapons/gmod_tool/stools/nocollide.lua
@@ -53,7 +53,7 @@ function TOOL:LeftClick( trace )
 end
 
 hook.Add( "EntityRemoved", "nocollide_fix", function( ent )
-	if ( ent.GetClass and ent:GetClass() == "logic_collision_pair" ) then
+	if ( IsValid( ent ) && ent:GetClass() == "logic_collision_pair" ) then
 		ent:Fire( "EnableCollisions" )
 	end
 end )
