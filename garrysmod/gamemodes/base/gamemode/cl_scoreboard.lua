92ed794041dc8011b552db5223e4dd6bcc558504
Rubat <robotboy655@gmail.com>
2014-08-07 03:40:25 +0300

    Changed base scoreboard player line colors
    Match the text color to the button color in base scoreboard

--- garrysmod/gamemodes/base/gamemode/cl_scoreboard.lua
+++ garrysmod/gamemodes/base/gamemode/cl_scoreboard.lua
@@ -32,7 +32,7 @@ local PLAYER_LINE = {
 		self.Name = self:Add( "DLabel" )
 		self.Name:Dock( FILL )
 		self.Name:SetFont( "ScoreboardDefault" )
-		self.Name:SetDark( true )
+		self.Name:SetTextColor( Color( 93, 93, 93 ) )
 		self.Name:DockMargin( 8, 0, 0, 0 )
 
 		self.Mute = self:Add( "DImageButton" )
@@ -43,21 +43,21 @@ local PLAYER_LINE = {
 		self.Ping:Dock( RIGHT )
 		self.Ping:SetWidth( 50 )
 		self.Ping:SetFont( "ScoreboardDefault" )
-		self.Ping:SetDark( true )
+		self.Ping:SetTextColor( Color( 93, 93, 93 ) )
 		self.Ping:SetContentAlignment( 5 )
 
 		self.Deaths = self:Add( "DLabel" )
 		self.Deaths:Dock( RIGHT )
 		self.Deaths:SetWidth( 50 )
 		self.Deaths:SetFont( "ScoreboardDefault" )
-		self.Deaths:SetDark( true )
+		self.Deaths:SetTextColor( Color( 93, 93, 93 ) )
 		self.Deaths:SetContentAlignment( 5 )
 
 		self.Kills = self:Add( "DLabel" )
 		self.Kills:Dock( RIGHT )
 		self.Kills:SetWidth( 50 )
 		self.Kills:SetFont( "ScoreboardDefault" )
-		self.Kills:SetDark( true )
+		self.Kills:SetTextColor( Color( 93, 93, 93 ) )
 		self.Kills:SetContentAlignment( 5 )
 
 		self:Dock( TOP )
