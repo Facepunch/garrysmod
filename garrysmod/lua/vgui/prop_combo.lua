ab46858a9725e30beaded4b9f5c353dde2386573
Rubat <robotboy655@gmail.com>
2014-08-28 19:49:44 +0300

    Update DProperty_Combo to be usable for ent edit

--- garrysmod/lua/vgui/prop_combo.lua
+++ garrysmod/lua/vgui/prop_combo.lua
@@ -1,26 +1,36 @@
+
 local PANEL = {}
 
 function PANEL:Init()
 end
 
 function PANEL:Setup( vars )
-	
+
 	self:Clear()
-	
+
 	local combo = vgui.Create( "DComboBox", self )
 	combo:Dock( FILL )
-	combo:SetValue( vars or "Select..." )
+	combo:DockMargin( 0, 1, 2, 2 )
+	combo:SetValue( vars.text or "Select..." )
+
+	for id, thing in pairs( vars.values or {} ) do
+		combo:AddChoice( id, thing )
+	end
 	
 	self.IsEditing = function( self )
 		return combo:IsMenuOpen()
 	end
 	
-	self.SetValue = function( self, id )
-		combo:ChooseOptionID( id )
+	self.SetValue = function( self, val )
+		for id, data in pairs( combo.Data ) do
+			if ( data == val ) then
+				combo:ChooseOptionID( id )
+			end
+		end
 	end
 	
 	combo.OnSelect = function( _, id, val, data )
-		self:ValueChanged( { Index = id, Value = val, Data = data }, true )
+		self:ValueChanged( data, true )
 	end
 	
 	combo.Paint = function( combo, w, h )
@@ -30,7 +40,7 @@ function PANEL:Setup( vars )
 		end
 		
 	end
-	
+
 	self:GetRow().AddChoice = function( self, value, data, select )
 		combo:AddChoice( value, data, select )
 	end
@@ -38,7 +48,6 @@ function PANEL:Setup( vars )
 	self:GetRow().SetSelected = function( self, id )
 		combo:ChooseOptionID( id )
 	end
-	
 
 end
 
