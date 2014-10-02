d62ceedb13baf775ad02ed4d578daf665ee8a1a0
Bo Anderson <bo@toxicflames.co.uk>
2014-10-02 17:26:59 +0100

    Added confirmation to "Uninstall All" addons button.

--- garrysmod/html/js/menu/control.Addons.js
+++ garrysmod/html/js/menu/control.Addons.js
@@ -7,7 +7,7 @@ function ControllerAddons( $scope, $element, $rootScope, $location )
 	$rootScope.ShowBack = true;
 	Scope = $scope;
 
-	$scope.AddonTypes = 
+	$scope.AddonTypes =
 	[
 		"gamemode",
 		"map",
@@ -24,35 +24,35 @@ function ControllerAddons( $scope, $element, $rootScope, $location )
 
 	$scope.Subscribe = function( file )
 	{
-		lua.Run( "steamworks.Subscribe( %s );", String( file.id ) )
+		lua.Run( "steamworks.Subscribe( %s )", String( file.id ) );
 
 		// Update files if viewing subscribed list?
 	};
 
 	$scope.Unsubscribe = function ( file )
 	{
-		lua.Run( "steamworks.Unsubscribe( %s );", String( file.id ) )
-		
+		lua.Run( "steamworks.Unsubscribe( %s )", String( file.id ) );
+
 		// Update files if viewing subscribed list?
 	};
 
-	$scope.UnsubscribeAll = function ( file )
-	{
-		subscriptions.SetAllEnabled( false );
-		lua.Run( "for k, v in pairs (engine.GetAddons()) do steamworks.Unsubscribe(v.wsid) end steamworks.ApplyAddons()" )
-		// Something like a fus ro dah on the addons.
-	};
-	
 	$scope.DisableAllSubscribed = function()
 	{
 		subscriptions.SetAllEnabled( false );
-		lua.Run( "steamworks.ApplyAddons();" )
+		lua.Run( "steamworks.ApplyAddons()" );
 	}
 
 	$scope.EnableAllSubscribed=function()
 	{
 		subscriptions.SetAllEnabled( true );
-		lua.Run( "steamworks.ApplyAddons();" )
+		lua.Run( "steamworks.ApplyAddons()" );
+	}
+
+	$scope.UnsubscribeAllSubscribed=function()
+	{
+		subscriptions.SetAllEnabled( false );
+		subscriptions.UnsubscribeAll();
+		lua.Run( "steamworks.ApplyAddons()" );
 	}
 
 	$scope.IsSubscribed = function ( file )
@@ -67,14 +67,14 @@ function ControllerAddons( $scope, $element, $rootScope, $location )
 
 	$scope.Disable = function ( file )
 	{
-		lua.Run( "steamworks.SetShouldMountAddon( %s, false );", String( file.id ) )
-		lua.Run( "steamworks.ApplyAddons();" )
+		lua.Run( "steamworks.SetShouldMountAddon( %s, false )", String( file.id ) );
+		lua.Run( "steamworks.ApplyAddons()" );
 	};
 
 	$scope.Enable = function ( file )
 	{
-		lua.Run( "steamworks.SetShouldMountAddon( %s, true );", String( file.id ) )
-		lua.Run( "steamworks.ApplyAddons();" )
+		lua.Run( "steamworks.SetShouldMountAddon( %s, true )", String( file.id ) );
+		lua.Run( "steamworks.ApplyAddons()" );
 	};
 
 }
