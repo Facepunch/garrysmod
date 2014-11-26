7ea9f20f3049520e771ec0aeaef0d2258a0bb283
Exho1 <nat.adams149@gmail.com>
2014-11-25 18:55:16 -0600

    Add 4 more shape types to DShape
    I added Textured Rectangle, Textured Rectangle Rotated, Circle, and Outlined Rectangle. Hopefully this element will become useful again!

--- garrysmod/lua/vgui/dshape.lua
+++ garrysmod/lua/vgui/dshape.lua
@@ -4,34 +4,23 @@
  /'_` | /'__`\( '__)/' _ ` _ `\ /'_` )
 ( (_| |(  ___/| |   | ( ) ( ) |( (_| |
 `\__,_)`\____)(_)   (_) (_) (_)`\__,_) 
-
 	DShape
-
 --]]
-
-function VGUIRect( x, y, w, h )
-	local shape = vgui.Create( "DShape" )
-	shape:SetType( "Rect" )
-	shape:SetPos( x, y )
-	shape:SetSize( w, h )
-	return shape
-end
 
 local PANEL = {}
 
-AccessorFunc( PANEL, "m_Color", 		"Color" )
-AccessorFunc( PANEL, "m_BorderColor", 	"BorderColor" )
-AccessorFunc( PANEL, "m_Type", 		"Type" )
+AccessorFunc( PANEL, "m_colColor", 			"Color" )
+AccessorFunc( PANEL, "m_colBorderColor", 		"BorderColor" )
+AccessorFunc( PANEL, "m_strType", 			"Type" )
 
-local RenderTypes = {}
+AccessorFunc( PANEL, "m_intRadius", 		"Radius" )
+AccessorFunc( PANEL, "m_strTexture", 		"Texture" )
+AccessorFunc( PANEL, "m_strTextureID", 		"TextureID" )
+AccessorFunc( PANEL, "m_intRotation", 		"Rotation" )
 
-function RenderTypes.Rect( pnl )
-	surface.SetDrawColor( pnl:GetColor() )
-	surface.DrawRect( 0, 0, pnl:GetSize() )
-end
 
 --[[---------------------------------------------------------
-
+	Init
 -----------------------------------------------------------]]
 function PANEL:Init()
 
@@ -43,12 +32,69 @@ function PANEL:Init()
 end
 
 --[[---------------------------------------------------------
+	Texture
+-----------------------------------------------------------]]
+function PANEL:SetTexture( strMat )
+	
+	self:SetTextureID( surface.GetTextureID( strMat ) )
 
+end
+
+local RenderTypes = {} -- Create a table with the different shape types
+
+function RenderTypes.Rect( pnl )
+
+	surface.SetDrawColor( pnl:GetColor() )
+	surface.DrawRect( 0, 0, pnl:GetSize() )
+	
+end
+
+function RenderTypes.OutlinedRect( pnl )
+
+	surface.SetDrawColor( pnl:GetColor() )
+	surface.DrawOutlinedRect( 0, 0, pnl:GetSize() )
+
+end
+
+function RenderTypes.TexturedRect( pnl )
+
+	surface.SetTexture( pnl.m_strTextureID )
+	surface.SetDrawColor( pnl:GetColor() )
+	surface.DrawTexturedRect( 0, 0, pnl:GetSize() )
+
+end
+
+function RenderTypes.TexturedRectRotated( pnl )
+
+	local w, h = pnl:GetSize()
+	surface.SetTexture( pnl:GetTextureID() )
+	surface.SetDrawColor( pnl:GetColor() )
+	surface.DrawTexturedRectRotated( 0, 0, w, h, pnl:GetRotation() )
+
+end
+
+function RenderTypes.Circle( pnl )
+
+	-- Offset the X and Y otherwise the shape will always be cut off
+	surface.DrawCircle( pnl:GetRadius(), pnl:GetRadius(), pnl:GetRadius(), pnl:GetColor() )	
+	
+end
+
+--[[---------------------------------------------------------
+	Paint
 -----------------------------------------------------------]]
 function PANEL:Paint()
-
-	RenderTypes[ self.m_Type ]( self )
+	
+	RenderTypes[ self.m_strType ]( self )
 
 end
 
 derma.DefineControl( "DShape", "A shape", PANEL, "DPanel" )
+
+function VGUIRect( x, y, w, h )
+	local shape = vgui.Create( "DShape" )
+	shape:SetType( "Rect" )
+	shape:SetPos( x, y )
+	shape:SetSize( w, h )
+	return shape
+end
