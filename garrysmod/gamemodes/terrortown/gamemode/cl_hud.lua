6e02a2d39d3943680bac503710f18906067b042d
meharryp <harryhahahpollard@aol.co.uk>
2015-02-10 18:26:16 +0000

    TTT: Make disabling parts of the HUD easier
    Should make creating HUDs much more graceful than replacing cl_hud.lua.

--- garrysmod/gamemodes/terrortown/gamemode/cl_hud.lua
+++ garrysmod/gamemodes/terrortown/gamemode/cl_hud.lua
@@ -318,28 +318,50 @@ end
 function GM:HUDPaint()
    local client = LocalPlayer()
 
-   hook.Call( "HUDDrawTargetID", GAMEMODE )
-
-   MSTACK:Draw(client)
+   if hook.Call( "HUDShouldDraw", GAMEMODE, "TTTTargetID" ) then
+       hook.Call( "HUDDrawTargetID", GAMEMODE )
+   end
+   
+   if hook.Call( "HUDShouldDraw", GAMEMODE, "TTTMStack" ) then
+       MSTACK:Draw(client)
+   end
 
    if (not client:Alive()) or client:Team() == TEAM_SPEC then
-      SpecHUDPaint(client)
+      if hook.Call( "HUDShouldDraw", GAMEMODE, "TTTSpecHUD" ) then
+          SpecHUDPaint(client)
+      end
 
       return
    end
 
+   if hook.Call( "HUDShouldDraw", GAMEMODE, "TTTRadar" ) then
+       RADAR:Draw(client)
+   end
+   
+   if hook.Call( "HUDShouldDraw", GAMEMODE, "TTTTButton" ) then
+       TBHUD:Draw(client)
+   end
+   
+   if hook.Call( "HUDShouldDraw", GAMEMODE, "TTTWSwitch" ) then
+       WSWITCH:Draw(client)
+   end
 
-   RADAR:Draw(client)
-   TBHUD:Draw(client)
-   WSWITCH:Draw(client)
+   if hook.Call( "HUDShouldDraw", GAMEMODE, "TTTVoice" ) then
+       VOICE.Draw(client)
+   end
+   
+   if hook.Call( "HUDShouldDraw", GAMEMODE, "TTTDisguise" ) then
+       DISGUISE.Draw(client)
+   end
 
-   VOICE.Draw(client)
-   DISGUISE.Draw(client)
-
-   hook.Call( "HUDDrawPickupHistory", GAMEMODE )
+   if hook.Call( "HUDShouldDraw", GAMEMODE, "TTTPickupHistory" ) then
+       hook.Call( "HUDDrawPickupHistory", GAMEMODE )
+   end
 
    -- Draw bottom left info panel
-   InfoPaint(client)
+   if hook.Call( "HUDShouldDraw", GAMEMODE, "TTTInfoPanel" ) then
+       InfoPaint(client)
+   end
 end
 
 -- Hide the standard HUD stuff
