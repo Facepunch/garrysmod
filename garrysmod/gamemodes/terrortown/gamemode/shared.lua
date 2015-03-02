4c491ae5717e7d10e16ee80edcba5ad9e9896612
code_gs <collinharris509@icloud.com>
2015-03-01 20:52:52 -0600

    Bumped TTT version (March 2015)
    http://facepunch.com/showthread.php?t=1450721&p=47240072&viewfull=1#post47240072

--- garrysmod/gamemodes/terrortown/gamemode/shared.lua
+++ garrysmod/gamemodes/terrortown/gamemode/shared.lua
@@ -3,7 +3,7 @@ GM.Author = "Bad King Urgrain"
 GM.Email = "thegreenbunny@gmail.com"
 GM.Website = "ttt.badking.net"
 -- Date of latest changes (YYYY-MM-DD)
-GM.Version = "2014-06-22"
+GM.Version = "2015-03-02"
 
 
 GM.Customized = false
