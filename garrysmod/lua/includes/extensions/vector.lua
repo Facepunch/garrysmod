574a13b3717a7b67e4500c547111fdc419f97520
code_gs <collinharris509@icloud.com>
2015-03-05 02:42:54 -0600

    Added vector unpack

--- garrysmod/lua/includes/extensions/vector.lua
+++ garrysmod/lua/includes/extensions/vector.lua
@@ -1,12 +1,19 @@
-local meta = FindMetaTable( "Vector" )
-
--- Nothing in here, still leaving this file here just in case
+local VECTOR = FindMetaTable( "Vector" )
 
 --[[---------------------------------------------------------
 	Converts Vector To Color - alpha precision lost, must reset
 -----------------------------------------------------------]]
-function meta:ToColor( )
+function VECTOR:ToColor()
 
 	return Vector( self.x * 255, self.y * 255, self.z * 255, 255 )
 
+end
+
+--[[---------------------------------------------------------
+	Unpacks vector into three variables
+-----------------------------------------------------------]]
+function VECTOR:Unpack()
+	
+	return self.x, self.y, self.z
+	
 end
