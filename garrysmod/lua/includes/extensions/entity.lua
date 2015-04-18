124a29af9544f37c750328829e244b12dec174f7
Exho1 <Exho1@users.noreply.github.com>
2015-04-17 19:46:10 -0500

    Door and Activator functions

--- garrysmod/lua/includes/extensions/entity.lua
+++ garrysmod/lua/includes/extensions/entity.lua
@@ -496,26 +496,6 @@ if ( SERVER ) then
 		return self:GetSaveTable().m_eDoorState
 	end
 	
-	function meta:IsDoorClosed()
-		return self:GetDoorState() == DOOR_STATE_CLOSED
-	end
-	
-	function meta:IsDoorClosing()
-		return self:GetDoorState() == DOOR_STATE_CLOSING
-	end
-	
-	function meta:IsDoorOpening()
-		return self:GetDoorState() == DOOR_STATE_OPENING
-	end
-	
-	function meta:IsDoorOpen()
-		return self:GetDoorState() == DOOR_STATE_OPEN
-	end
-	
-	function meta:IsDoorAjar()
-		return self:GetDoorState() == DOOR_STATE_AJAR
-	end
-	
 	function meta:IsDoorBlocked()
 		return IsValid( self:GetSaveTable().m_hBlocker ) 
 	end
@@ -535,14 +515,6 @@ if ( SERVER ) then
 	function meta:IsActivator( ent )
 		return ent == self:GetSaveTable().m_hActivator
 	end
-	
-	function meta:IsPlayerOpening()
-		return (self:GetActivator() and self:GetActivator():IsPlayer())
-	end
-	
-	function meta:IsNPCOpening()
-		return (self:GetActivator() and self:GetActivator():IsNPC())
-	end
 end
 
 --
@@ -550,7 +522,7 @@ end
 --
 function meta:IsDoor()
 	local class = self:GetClass()
-	if class == "prop_door_rotating" or class == "func_door" or class == "func_door_rotating" or class == "prop_dynamic" then
+	if class == "prop_door_rotating" or class == "func_door" or class == "func_door_rotating" then
 		return true
 	end
 	return false
