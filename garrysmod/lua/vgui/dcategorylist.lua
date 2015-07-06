f838b27b54a300b2a6521780b7f80dbe3be1c761
Bo Anderson <bo@toxicflames.co.uk>
2015-07-06 13:19:48 +0100

    Added DCategoryList example for derma_controls concommand.

--- garrysmod/lua/vgui/dcategorylist.lua
+++ garrysmod/lua/vgui/dcategorylist.lua
@@ -1,12 +1,12 @@
---[[   _                                
-	( )                               
-   _| |   __   _ __   ___ ___     _ _ 
+--[[   _
+	( )
+   _| |   __   _ __   ___ ___     _ _
  /'_` | /'__`\( '__)/' _ ` _ `\ /'_` )
 ( (_| |(  ___/| |   | ( ) ( ) |( (_| |
-`\__,_)`\____)(_)   (_) (_) (_)`\__,_) 
+`\__,_)`\____)(_)   (_) (_) (_)`\__,_)
 
 	DPanelList
-	
+
 	A window.
 
 --]]
@@ -29,7 +29,7 @@ function PANEL:AddItem( item )
 	item:Dock( TOP )
 	DScrollPanel.AddItem( self, item )
 	self:InvalidateLayout()
-	
+
 end
 
 --[[---------------------------------------------------------
@@ -40,10 +40,10 @@ function PANEL:Add( name )
 	local Category = vgui.Create( "DCollapsibleCategory", self )
 	Category:SetLabel( name )
 	Category:SetList( self )
-	
+
 	self:AddItem( Category )
-	
-	return Category	
+
+	return Category
 
 end
 
@@ -60,12 +60,34 @@ end
 function PANEL:UnselectAll()
 
 	for k, v in pairs( self:GetChildren() ) do
-	
+
 		if ( v.UnselectAll ) then
 			v:UnselectAll()
 		end
-	
+
 	end
+
+end
+
+function PANEL:GenerateExample( ClassName, PropertySheet, Width, Height )
+
+	local ctrl = vgui.Create( ClassName )
+		ctrl:SetSize( 300, 300 )
+
+		local Cat = ctrl:Add( "Test category with text contents" )
+		Cat:Add( "Item 1" )
+		Cat:Add( "Item 2" )
+
+		-- The contents can be any panel, even a DPanelList
+		local Cat2 = ctrl:Add( "Test category with panel contents" )
+		Cat2:SetTall( 100 )
+		local Contents = vgui.Create( "DButton" )
+		Contents:SetText( "This is the content of the category" )
+		Cat2:SetContents( Contents )
+
+		ctrl:InvalidateLayout( true )
+
+	PropertySheet:AddSheet( ClassName, ctrl, nil, true, true )
 
 end
 
