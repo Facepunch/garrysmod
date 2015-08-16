8c5bfa41594d132793532ddc1aa1176fa46ce9a6
Garrett Brown <grbrown93@sbcglobal.net>
2015-08-16 00:38:56 -0500

    Unrestrict thin ropes.

--- garrysmod/lua/includes/modules/constraint.lua
+++ garrysmod/lua/includes/modules/constraint.lua
@@ -327,7 +327,7 @@ function CreateKeyframeRope( Pos, width, material, Constraint, Ent1, LPos1, Bone
 	if ( width <= 0 ) then return nil end
 	
 	-- Clamp the rope to a sensible width
-	width = math.Clamp( width, 1, 100 )
+	width = math.Clamp( width, 0, 100 )
 
 	local rope = ents.Create( "keyframe_rope" )
 	rope:SetPos( Pos )
