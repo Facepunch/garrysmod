a351e4a6b391b64c70b78b4573dc6e3cafdf2d98
code_gs <collinharris509@icloud.com>
2015-08-30 20:48:25 -0500

    Added hexadecimal/integer support to Color object
    Supports RRGGBB and AARRGGBB hexes. Due to how Lua handles hexadecimals, setting the alpha to 0 through an int will not work, and will default to 255. Also added color to int/hex conversion.

--- garrysmod/lua/includes/util/color.lua
+++ garrysmod/lua/includes/util/color.lua
@@ -4,17 +4,38 @@ COLOR.__index = COLOR
 --[[---------------------------------------------------------
 	Register our metatable to make it accessible using FindMetaTable
 -----------------------------------------------------------]]
-
 debug.getregistry().Color = COLOR
 
 --[[---------------------------------------------------------
-	To easily create a colour table
+	Creates color object
 -----------------------------------------------------------]]
 function Color( r, g, b, a )
 
-	a = a or 255
-	return setmetatable( { r = math.min( tonumber(r), 255 ), g =  math.min( tonumber(g), 255 ), b =  math.min( tonumber(b), 255 ), a =  math.min( tonumber(a), 255 ) }, COLOR )
-	
+	r = tonumber( r )
+
+	if ( g != nil ) then
+		a = tonumber( a ) or 255
+
+		return setmetatable( { r = math.min( r, 255 ), g =  math.min( tonumber( g ), 255 ), b =  math.min( tonumber( b ), 255 ), a =  math.min( a, 255 ) }, COLOR )	
+	end
+
+	if ( r <= 0xFFFFFF ) then
+		a = 255
+	else
+		a = bit.band( bit.rshift( r, 24 ), 0xFF )
+	end
+
+	return setmetatable( { r = bit.band( bit.rshift( r, 16 ), 0xFF ), g = bit.band( bit.rshift( r, 8 ), 0xFF ), b = bit.band( bit.rshift( r, 0 ), 0xFF ), a = a }, COLOR )
+
+end
+
+--[[---------------------------------------------------------
+	Converts color to AARRGGBB hex/int
+-----------------------------------------------------------]]
+function COLOR:ToInt()
+
+	return bit.lshift( self.a, 24 ) + bit.lshift( self.r, 16 ) + bit.lshift( self.g, 8 ) + bit.lshift( self.b, 0 )
+
 end
 
 --[[---------------------------------------------------------
@@ -23,7 +44,7 @@ end
 function ColorAlpha( c, a )
 
 	return Color( c.r, c.g, c.b, a )
-	
+
 end
 
 --[[---------------------------------------------------------
@@ -31,7 +52,7 @@ end
 -----------------------------------------------------------]]
 function IsColor( obj )
 
-	return getmetatable(obj) == COLOR
+	return getmetatable( obj ) == COLOR
 
 end
 
@@ -40,27 +61,27 @@ end
 	Returns color as a string
 -----------------------------------------------------------]]
 function COLOR:__tostring()
-	
+
 	return string.format( "%d %d %d %d", self.r, self.g, self.b, self.a )
-	
+
 end
 
 --[[---------------------------------------------------------
 	Compares two colors
 -----------------------------------------------------------]]
 function COLOR:__eq( c )
-	
+
 	return self.r == c.r and self.g == c.g and self.b == c.b and self.a == c.a
-	
+
 end
 
 --[[---------------------------------------------------------
 	Converts a color to HSV
 -----------------------------------------------------------]]
 function COLOR:ToHSV()
-	
+
 	return ColorToHSV( self )
-	
+
 end
 
 --[[---------------------------------------------------------
