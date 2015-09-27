e0602f9e441f8d16e92f2d21b6c96e64486bc077
BillyOnWiiUIn144p <billyvenner@gmail.com>
2015-09-27 22:10:43 +0100

    Added optional values to math.Clamp
    Now, doing math.Clamp(-1,0) returns 0 and therefore doesn't need a max value. This means the number can then be infinite.
    Forgive me if the code is a bit messy, I have never used Github before :)

--- garrysmod/lua/includes/extensions/math.lua
+++ garrysmod/lua/includes/extensions/math.lua
@@ -40,11 +40,23 @@ end
 
 --[[---------------------------------------------------------
    Name: Clamp( in, low, high )
-   Desc: Clamp value between 2 values
+   Desc: Clamp value between 2 optional values
 ------------------------------------------------------------]]
 function math.Clamp( _in, low, high )
-	if (_in < low ) then return low end
-	if (_in > high ) then return high end
+	if ( low ~= nil and high ~= nil ) then
+		if (_in < low) then return low end
+		if (_in > high) then return high end
+	end
+	if ( low ~= nil and high == nil ) then
+		if ( _in < low ) then
+			return low
+		end
+	end
+	if ( low == nil and high ~= nil ) then
+		if ( _in > high ) then
+			return high
+		end
+	end
 	return _in
 end
 
