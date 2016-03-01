72caa6c5ed075735a183f61c3f7a9aceb5df1c0d
Robotboy655 <robotboy655@gmail.com>
2016-03-01 17:29:42 +0200

    Cleaned up DTextEntry, draw library
    Cleaned it up huh

--- garrysmod/lua/derma/init.lua
+++ garrysmod/lua/derma/init.lua
@@ -1,9 +1,9 @@
 --=============================================================================--
 --  ___  ___   _   _   _    __   _   ___ ___ __ __
 -- |_ _|| __| / \ | \_/ |  / _| / \ | o \ o \\ V /
---  | | | _| | o || \_/ | ( |_n| o ||   /   / \ / 
+--  | | | _| | o || \_/ | ( |_n| o ||   /   / \ /
 --  |_| |___||_n_||_| |_|  \__/|_n_||_|\\_|\\ |_|  2012
---										 
+--
 --=============================================================================--
 
 --
@@ -58,70 +58,68 @@ include( "derma_animation.lua" )
 include( "derma_utils.lua" )
 include( "derma_gwen.lua" )
 
-
 function Derma_Hook( panel, functionname, hookname, typename )
 
-	panel[ functionname ] = function ( self, a, b, c, d ) 
-									return derma.SkinHook( hookname, typename, self, a, b, c, d ) 
-							end
-end
+	panel[ functionname ] = function ( self, a, b, c, d )
+		return derma.SkinHook( hookname, typename, self, a, b, c, d )
+	end
 
+end
 
 --[[
 
 	ConVar Functions
-	
+
 	To associate controls with convars. The controls automatically
 	update from the value of the control, and automatically update
 	the value of the convar from the control.
-	
-	Controls must:
-	
-		Call ConVarStringThink or ConVarNumberThink from the 
-		Think function to get any changes from the ConVars.
-		
-		Have SetValue( value ) implemented, to receive the 
-		value.
 
+	Controls must:
+
+	Call ConVarStringThink or ConVarNumberThink from the
+	Think function to get any changes from the ConVars.
+
+	Have SetValue( value ) implemented, to receive the
+	value.
 
 --]]
+
 function Derma_Install_Convar_Functions( PANEL )
 
 	function PANEL:SetConVar( strConVar )
 		self.m_strConVar = strConVar
 	end
-	
+
 	function PANEL:ConVarChanged( strNewValue )
-	
-		if ( !self.m_strConVar ) then return end	
+
+		if ( !self.m_strConVar ) then return end
 		RunConsoleCommand( self.m_strConVar, tostring( strNewValue ) )
-		
+
 	end
-	
+
+	-- Todo: Think only every 0.1 seconds?
 	function PANEL:ConVarStringThink()
-	
-		if ( !self.m_strConVar ) then return end	
-		
-		-- Todo: Think only every 0.1 seconds?
-		
+
+		if ( !self.m_strConVar ) then return end
+
 		local strValue = GetConVarString( self.m_strConVar )
 		if ( self.m_strConVarValue == strValue ) then return end
-		
+
 		self.m_strConVarValue = strValue
 		self:SetValue( self.m_strConVarValue )
-		
+
 	end
-	
+
 	function PANEL:ConVarNumberThink()
-	
-		if ( !self.m_strConVar ) then return end	
-		
+
+		if ( !self.m_strConVar ) then return end
+
 		local strValue = GetConVarNumber( self.m_strConVar )
 		if ( self.m_strConVarValue == strValue ) then return end
-		
+
 		self.m_strConVarValue = strValue
 		self:SetValue( self.m_strConVarValue )
-		
+
 	end
 
 end
--- garrysmod/lua/vgui/dtextentry.lua
+++ garrysmod/lua/vgui/dtextentry.lua
@@ -1,60 +1,39 @@
---[[   _                                
-	( )                               
-   _| |   __   _ __   ___ ___     _ _ 
- /'_` | /'__`\( '__)/' _ ` _ `\ /'_` )
-( (_| |(  ___/| |   | ( ) ( ) |( (_| |
-`\__,_)`\____)(_)   (_) (_) (_)`\__,_) 
-
-	DTextEntry
-	
-	In Engine Commands:
-	
-	self:SetMultiline( bool )
-	self:IsMultiline()
-
---]]
 
 local PANEL = {}
 
 local strAllowedNumericCharacters = "1234567890.-"
 
-AccessorFunc( PANEL, "m_bAllowEnter", 		"EnterAllowed", FORCE_BOOL )
-AccessorFunc( PANEL, "m_bUpdateOnType", 	"UpdateOnType", FORCE_BOOL )	-- Update the convar as we type
-AccessorFunc( PANEL, "m_bNumeric", 			"Numeric", FORCE_BOOL )
-AccessorFunc( PANEL, "m_bHistory", 			"HistoryEnabled", FORCE_BOOL )
-AccessorFunc( PANEL, "m_bDisableTabbing", 	"TabbingDisabled", 	FORCE_BOOL )
-AccessorFunc( PANEL, "m_FontName", 			"Font" )
-AccessorFunc( PANEL, "m_bBorder", 			"DrawBorder" )
-AccessorFunc( PANEL, "m_bBackground", 		"DrawBackground" )
+AccessorFunc( PANEL, "m_bAllowEnter",		"EnterAllowed", FORCE_BOOL )
+AccessorFunc( PANEL, "m_bUpdateOnType",		"UpdateOnType", FORCE_BOOL ) -- Update the convar as we type
+AccessorFunc( PANEL, "m_bNumeric",			"Numeric", FORCE_BOOL )
+AccessorFunc( PANEL, "m_bHistory",			"HistoryEnabled", FORCE_BOOL )
+AccessorFunc( PANEL, "m_bDisableTabbing",	"TabbingDisabled", FORCE_BOOL )
+AccessorFunc( PANEL, "m_FontName",			"Font" )
+AccessorFunc( PANEL, "m_bBorder",			"DrawBorder" )
+AccessorFunc( PANEL, "m_bBackground",		"DrawBackground" )
 
-AccessorFunc( PANEL, "m_colText", 			"TextColor" )
-AccessorFunc( PANEL, "m_colHighlight", 		"HighlightColor" )
-AccessorFunc( PANEL, "m_colCursor", 		"CursorColor" )
+AccessorFunc( PANEL, "m_colText",			"TextColor" )
+AccessorFunc( PANEL, "m_colHighlight",		"HighlightColor" )
+AccessorFunc( PANEL, "m_colCursor",			"CursorColor" )
 
-AccessorFunc( PANEL, "m_bDisabled", 		"Disabled" )
-
-
-
+AccessorFunc( PANEL, "m_bDisabled",			"Disabled" )
 
 Derma_Install_Convar_Functions( PANEL )
 
---[[---------------------------------------------------------
-
------------------------------------------------------------]]
 function PANEL:Init()
 
 	self:SetHistoryEnabled( false )
 	self.History = {}
 	self.HistoryPos = 0
-	
+
 	--
-	-- We're going to draw these ourselves in 
+	-- We're going to draw these ourselves in
 	-- the skin system - so disable them here.
 	-- This will leave it only drawing text.
 	--
 	self:SetPaintBorderEnabled( false )
 	self:SetPaintBackgroundEnabled( false )
-	
+
 	--
 	-- These are Lua side commands
 	-- Defined above using AccessorFunc
@@ -65,17 +44,17 @@ function PANEL:Init()
 	self:SetUpdateOnType( false )
 	self:SetNumeric( false )
 	self:SetAllowNonAsciiCharacters( true )
-		
-	
+
+
 	-- Nicer default height
 	self:SetTall( 20 )
-	
+
 	-- Clear keyboard focus when we click away
 	self.m_bLoseFocusOnClickAway = true
-	
+
 	-- Beam Me Up Scotty
 	self:SetCursor( "beam" )
-	
+
 	-- Apply scheme settings now, allow the user to override them later.
 	derma.SkinHook( "Scheme", "TextEntry", self )
 
@@ -87,49 +66,43 @@ function PANEL:IsEditing()
 	return self == vgui.GetKeyboardFocus()
 end
 
---[[---------------------------------------------------------
-
------------------------------------------------------------]]
 function PANEL:OnKeyCodeTyped( code )
 
 	self:OnKeyCode( code )
 
 	if ( code == KEY_ENTER && !self:IsMultiline() && self:GetEnterAllowed() ) then
-	
+
 		if ( IsValid( self.Menu ) ) then
 			self.Menu:Remove()
 		end
-	
+
 		self:FocusNext()
 		self:OnEnter()
 		self.HistoryPos = 0
-		
+
 	end
-	
+
 	if ( self.m_bHistory || IsValid( self.Menu ) ) then
-	
+
 		if ( code == KEY_UP ) then
-			self.HistoryPos = self.HistoryPos - 1;
+			self.HistoryPos = self.HistoryPos - 1
 			self:UpdateFromHistory()
 		end
-		
-		if ( code == KEY_DOWN || code == KEY_TAB ) then	
-			self.HistoryPos = self.HistoryPos + 1;
+
+		if ( code == KEY_DOWN || code == KEY_TAB ) then
+			self.HistoryPos = self.HistoryPos + 1
 			self:UpdateFromHistory()
 		end
-	
+
 	end
-	
+
 end
 
 function PANEL:OnKeyCode( code )
-	
 end
 
 function PANEL:ApplySchemeSettings()
-
 	self:SetFontInternal( self.m_FontName )
-
 end
 
 function PANEL:UpdateFromHistory()
@@ -138,20 +111,19 @@ function PANEL:UpdateFromHistory()
 		return self:UpdateFromMenu()
 	end
 
-
 	local pos = self.HistoryPos
 	-- Is the Pos within bounds?
 	if ( pos < 0 ) then pos = #self.History end
 	if ( pos > #self.History ) then pos = 0 end
-	
+
 	local text = self.History[ pos ]
 	if ( !text ) then text = "" end
-	
+
 	self:SetText( text )
 	self:SetCaretPos( text:len() )
 
 	self:OnTextChanged()
-	
+
 	self.HistoryPos = pos
 
 end
@@ -160,27 +132,28 @@ function PANEL:UpdateFromMenu()
 
 	local pos = self.HistoryPos
 	local num = self.Menu:ChildCount()
-	
+
 	self.Menu:ClearHighlights()
 
 	if ( pos < 0 ) then pos = num end
 	if ( pos > num ) then pos = 0 end
-	
+
 	local item = self.Menu:GetChild( pos )
 	if ( !item ) then
 		self:SetText( "" )
 		self.HistoryPos = pos
-	return end
-	
+		return
+	end
+
 	self.Menu:HighlightItem( item )
-	
+
 	local txt = item:GetText()
-	
+
 	self:SetText( txt )
 	self:SetCaretPos( txt:len() )
 
-	self:OnTextChanged(true)
-	
+	self:OnTextChanged( true )
+
 	self.HistoryPos = pos
 
 end
@@ -188,55 +161,52 @@ end
 --[[---------------------------------------------------------
 	OnTextChanged
 -----------------------------------------------------------]]
-function PANEL:OnTextChanged(noMenuRemoval)
-	
+function PANEL:OnTextChanged( noMenuRemoval )
+
 	self.HistoryPos = 0
-	
+
 	if ( self:GetUpdateOnType() ) then
 		self:UpdateConvarValue()
 		self:OnValueChange( self:GetText() )
 	end
-	
+
 	if ( IsValid( self.Menu ) and not noMenuRemoval ) then
 		self.Menu:Remove()
 	end
-	
+
 	local tab = self:GetAutoComplete( self:GetText() )
 	if ( tab ) then
 		self:OpenAutoComplete( tab )
 	end
-	
+
 	self:OnChange()
-	
+
 end
 
 --[[---------------------------------------------------------
 	OnChange
 -----------------------------------------------------------]]
 function PANEL:OnChange()
-
-	
-		
 end
 
 function PANEL:OpenAutoComplete( tab )
 
 	if ( !tab ) then return end
 	if ( #tab == 0 ) then return end
-	
-	self.Menu = DermaMenu()
-	
-		for k, v in pairs( tab ) do
-			
-			self.Menu:AddOption( v, function() self:SetText( v ) self:SetCaretPos( v:len() ) self:RequestFocus() end )		
 
-		end
-	
+	self.Menu = DermaMenu()
+
+	for k, v in pairs( tab ) do
+
+		self.Menu:AddOption( v, function() self:SetText( v ) self:SetCaretPos( v:len() ) self:RequestFocus() end )
+
+	end
+
 	local x, y = self:LocalToScreen( 0, self:GetTall() )
 	self.Menu:SetMinimumWidth( self:GetWide() )
 	self.Menu:Open( x, y, true, self )
 	self.Menu:SetPos( x, y )
-	self.Menu:SetMaxHeight( (ScrH() - y) - 10 )
+	self.Menu:SetMaxHeight( ( ScrH() - y ) - 10 )
 
 end
 
@@ -293,7 +263,7 @@ end
 
 
 --[[---------------------------------------------------------
-   Name: SetValue ( A simple redirect for the ConVar stuff )
+	Name: SetValue ( A simple redirect for the ConVar stuff )
 -----------------------------------------------------------]]
 function PANEL:SetValue( strValue )
 
@@ -305,119 +275,119 @@ function PANEL:SetValue( strValue )
 
 	self:SetText( strValue )
 	self:OnValueChange( strValue )
-	
+
 	self:SetCaretPos( CaretPos )
 
 end
 
 
 --[[---------------------------------------------------------
-   Name: For Override
+	Name: OnValueChange
 -----------------------------------------------------------]]
 function PANEL:OnValueChange( strValue )
-
+	-- For Override
 end
 
 --[[---------------------------------------------------------
-   Name: CheckNumeric
+	Name: CheckNumeric
 -----------------------------------------------------------]]
 function PANEL:CheckNumeric( strValue )
 
 	-- Not purely numeric, don't run the check
 	if ( !self:GetNumeric() ) then return false end
-	
+
 	-- God I hope numbers look the same in every language
-	if ( !string.find ( strAllowedNumericCharacters, strValue, 1, true ) ) then
-	
+	if ( !string.find( strAllowedNumericCharacters, strValue, 1, true ) ) then
+
 		-- Noisy Error?
 		return true
-		
+
 	end
 
-	return false	
+	return false
 
 end
 
 --[[---------------------------------------------------------
-   Name: AllowInput
+	Name: AllowInput
 -----------------------------------------------------------]]
 function PANEL:AllowInput( strValue )
 
-	-- This is layed out like this so you can easily override and 
+	-- This is layed out like this so you can easily override and
 	-- either keep or remove this numeric check.
 	if ( self:CheckNumeric( strValue ) ) then return true end
 
 end
 
 --[[---------------------------------------------------------
-   Name: SetEditable
+	Name: SetEditable
 -----------------------------------------------------------]]
 function PANEL:SetEditable( b )
-	
+
 	self:SetKeyboardInputEnabled( b )
 	self:SetMouseInputEnabled( b )
-	
+
 end
 
 --[[---------------------------------------------------------
-   Name: OnGetFocus
+	Name: OnGetFocus
 -----------------------------------------------------------]]
 function PANEL:OnGetFocus()
-	
+
 	--
 	-- These hooks are here for the sake of things like the spawn menu
-	--  which don't have key focus until you click on one of the text areas.
+	-- which don't have key focus until you click on one of the text areas.
 	--
 	-- If you make a control for the spawnmenu that requires keyboard input
 	-- You should have these 3 functions in your panel, so it can handle it.
 	--
 
 	hook.Run( "OnTextEntryGetFocus", self )
-	
+
 end
 
 --[[---------------------------------------------------------
-   Name: OnLoseFocus
+	Name: OnLoseFocus
 -----------------------------------------------------------]]
 function PANEL:OnLoseFocus()
-	
+
 	self:UpdateConvarValue()
-	
+
 	hook.Call( "OnTextEntryLoseFocus", nil, self )
-	
+
 end
 
 --[[---------------------------------------------------------
-   Name: OnMousePressed
+	Name: OnMousePressed
 -----------------------------------------------------------]]
 function PANEL:OnMousePressed( mcode )
-	
+
 	self:OnGetFocus()
-	
+
 end
 
 --[[---------------------------------------------------------
-   Name: AddHistory
+	Name: AddHistory
 -----------------------------------------------------------]]
 function PANEL:AddHistory( txt )
-	
-	if ( !txt || txt == "" ) then return; end
-	
-	table.RemoveByValue( self.History, txt )	
+
+	if ( !txt || txt == "" ) then return end
+
+	table.RemoveByValue( self.History, txt )
 	table.insert( self.History, txt )
-	
+
 end
 
 
 --[[---------------------------------------------------------
-   Name: OnMousePressed
+	Name: OnMousePressed
 -----------------------------------------------------------]]
 function PANEL:GetAutoComplete( txt )
 	-- for override. Return a table of strings.
 end
 
 --[[---------------------------------------------------------
-   Name: GetInt
+	Name: GetInt
 -----------------------------------------------------------]]
 function PANEL:GetInt()
 
@@ -426,7 +396,7 @@ function PANEL:GetInt()
 end
 
 --[[---------------------------------------------------------
-   Name: GetFloat
+	Name: GetFloat
 -----------------------------------------------------------]]
 function PANEL:GetFloat()
 
@@ -435,26 +405,23 @@ function PANEL:GetFloat()
 end
 
 --[[---------------------------------------------------------
-   Name: GenerateExample
+	Name: GenerateExample
 -----------------------------------------------------------]]
 function PANEL:GenerateExample( ClassName, PropertySheet, Width, Height )
 
 	local ctrl = vgui.Create( ClassName )
-		ctrl:SetText( "Edit Me!" )
-		ctrl:SetWide( 150 )
-		
-		ctrl.OnEnter = function( self ) Derma_Message( "You Typed: "..self:GetValue() ) end
-	
+	ctrl:SetText( "Edit Me!" )
+	ctrl:SetWide( 150 )
+	ctrl.OnEnter = function( self ) Derma_Message( "You Typed: " .. self:GetValue() ) end
+
 	PropertySheet:AddSheet( ClassName, ctrl, nil, true, true )
 
 end
 
-
 derma.DefineControl( "DTextEntry", "A simple TextEntry control", PANEL, "TextEntry" )
 
-
 --[[---------------------------------------------------------
-   Clear the focus when we click away from us..
+	Clear the focus when we click away from us..
 -----------------------------------------------------------]]
 function TextEntryLoseFocus( panel, mcode )
 
@@ -462,7 +429,7 @@ function TextEntryLoseFocus( panel, mcode )
 	if ( !pnl ) then return end
 	if ( pnl == panel ) then return end
 	if ( !pnl.m_bLoseFocusOnClickAway ) then return end
-	
+
 	pnl:FocusNext()
 
 end
