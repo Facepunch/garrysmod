621220c67da0140529993236af627a456fe35d78
Garrett Brown <grbrown93@sbcglobal.net>
2016-04-15 03:41:24 -0500

    Add ordering for hooks.

--- garrysmod/lua/includes/modules/hook.lua
+++ garrysmod/lua/includes/modules/hook.lua
@@ -3,10 +3,13 @@ local pairs                        = pairs
 local isfunction        = isfunction
 local isstring                = isstring
 local IsValid                = IsValid
+local tinsert                = table.insert
+local tremove               = table.remove
 
 module( "hook" )
 
 local Hooks = {}
+local HooksOrdered = {}
 
 --[[---------------------------------------------------------
     Name: GetTable
@@ -20,16 +23,33 @@ function GetTable() return Hooks end
     Args: string hookName, any identifier, function func
     Desc: Add a hook to listen to the specified event.
 -----------------------------------------------------------]]
-function Add( event_name, name, func )
+function Add( event_name, name, func, order )
 
 	if ( !isfunction( func ) ) then return end
 	if ( !isstring( event_name ) ) then return end
+	if ( !order ) then order = 0 end
 
-	if (Hooks[ event_name ] == nil) then
-			Hooks[ event_name ] = {}
+	if ( Hooks[ event_name ] == nil ) then
+		Hooks[ event_name ] = {}
+		HooksOrdered[ event_name ] = {}
 	end
 
+	-- Check if the hook has been added before and remove it's previous ordering if so.
+	if ( Hooks[ event_name ][ name ] != nil ) then
+		Remove( event_name, name )
+	end
 	Hooks[ event_name ][ name ] = func
+
+	-- Find the position of the next greater order. table.insert here will move greater orders to the right.
+	local hooks_ordered = HooksOrdered[ event_name ]
+	local position = #hooks_ordered + 1
+	for i = 1, #hooks_ordered do
+		if hooks_ordered[ i ].order > order then
+			position = i
+			break
+		end
+	end
+	tinsert( hooks_ordered, position, { name = name, order = order, func = func } )
 
 end
 
@@ -44,8 +64,42 @@ function Remove( event_name, name )
 	if ( !isstring( event_name ) ) then return end
 	if ( !Hooks[ event_name ] ) then return end
 
-	Hooks[ event_name ][ name ] = nil
+	if ( Hooks[ event_name ][ name ] != nil ) then
+		-- Find the ordered index of the hook and remove it
+		local hooks_ordered = HooksOrdered[ event_name ]
+		for i = 1, #hooks_ordered do
+			if hooks_ordered[ i ].name == name then
+				tremove( hooks_ordered, i )
+				break
+			end
+		end
+		-- Remove from the string indexed table
+		Hooks[ event_name ][ name ] = nil
+	end
 
+end
+
+
+--[[---------------------------------------------------------
+    Name: GetOrder
+    Args: string hookName, identifier
+    Desc: Returns the position of the hook in the order table or nil if it wasn't found.
+-----------------------------------------------------------]]
+function GetOrder( event_name, name )
+
+	if ( !isstring( event_name ) ) then return end
+	if ( !Hooks[ event_name ] ) then return end
+
+	local hooks_ordered = HooksOrdered[ event_name ]
+	local ret = nil
+	for i = 1, #hooks_ordered do
+		if hooks_ordered[ i ].name == name then
+			ret = i
+			break
+		end
+	end
+	return ret
+	
 end
 
 
@@ -69,19 +123,21 @@ function Call( name, gm, ... )
 	--
 	-- Run hooks
 	--
-	local HookTable = Hooks[ name ]
+	local HookTable = HooksOrdered[ name ]
 	if ( HookTable != nil ) then
 	
 		local a, b, c, d, e, f;
 
-		for k, v in pairs( HookTable ) do 
+		for _, v in pairs( HookTable ) do
+			local k = v.name
+			local func = v.func
 			
 			if ( isstring( k ) ) then
 				
 				--
 				-- If it's a string, it's cool
 				--
-				a, b, c, d, e, f = v( ... )
+				a, b, c, d, e, f = func( ... )
 
 			else
 
@@ -93,12 +149,12 @@ function Call( name, gm, ... )
 					--
 					-- If the object is valid - pass it as the first argument (self)
 					--
-					a, b, c, d, e, f = v( k, ... )
+					a, b, c, d, e, f = func( k, ... )
 				else
 					--
 					-- If the object has become invalid - remove it
 					--
-					HookTable[ k ] = nil
+					Remove( name, k )
 				end
 			end
 
