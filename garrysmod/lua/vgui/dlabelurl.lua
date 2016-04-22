9f164bea795a77ee83a67dc33f308b5f8a74404c
Robotboy655 <robotboy655@gmail.com>
2016-04-22 19:00:58 +0300

    Clean up DLabelURL/Editable
    Renamed DLabelEditable.OnTextChanged to
    DLabelEditable.OnLabelTextChanged to prevent confusion/problems with
    DTextEntry.OnTextChanged

--- garrysmod/lua/vgui/dlabelurl.lua
+++ garrysmod/lua/vgui/dlabelurl.lua
@@ -1,19 +1,10 @@
---[[   _                                
-    ( )                               
-   _| |   __   _ __   ___ ___     _ _ 
- /'_` | /'__`\( '__)/' _ ` _ `\ /'_` )
-( (_| |(  ___/| |   | ( ) ( ) |( (_| |
-`\__,_)`\____)(_)   (_) (_) (_)`\__,_) 
-
-	DLabelURL
---]]
 
 local PANEL = {}
 
-AccessorFunc( PANEL, "m_colText", 				"TextColor" )
-AccessorFunc( PANEL, "m_colTextStyle", 			"TextStyleColor" )
+AccessorFunc( PANEL, "m_colText",				"TextColor" )
+AccessorFunc( PANEL, "m_colTextStyle",			"TextStyleColor" )
 
-AccessorFunc( PANEL, "m_bAutoStretchVertical", 	"AutoStretchVertical" )
+AccessorFunc( PANEL, "m_bAutoStretchVertical",	"AutoStretchVertical" )
 
 --[[---------------------------------------------------------
 	Init
@@ -21,14 +12,14 @@ AccessorFunc( PANEL, "m_bAutoStretchVertical", 	"AutoStretchVertical" )
 function PANEL:Init()
 
 	self:SetTextColor( Color( 0, 0, 255 ) )
-	
+
 	-- Nicer default height
 	self:SetTall( 20 )
-	
+
 	-- This turns off the engine drawing
 	self:SetPaintBackgroundEnabled( false )
 	self:SetPaintBorderEnabled( false )
-	
+
 end
 
 --[[---------------------------------------------------------
@@ -36,11 +27,11 @@ end
 -----------------------------------------------------------]]
 function PANEL:ApplySchemeSettings( col )
 
-	self:UpdateColours( self:GetSkin() );
-	
+	self:UpdateColours( self:GetSkin() )
+
 	local col = self.m_colTextStyle
 	if ( self.m_colText ) then col = self.m_colText end
-	
+
 	self:SetFGColor( col.r, col.g, col.b, col.a )
 
 end
@@ -56,15 +47,14 @@ function PANEL:GetColor()
 
 end
 
-
 --[[---------------------------------------------------------
 	Exited
 -----------------------------------------------------------]]
 function PANEL:OnCursorEntered()
-	
+
 	self:InvalidateLayout()
 	self:SetTextColor( Color( 0, 50, 255 ) )
-	
+
 end
 
 --[[---------------------------------------------------------
@@ -74,15 +64,13 @@ function PANEL:OnCursorExited()
 
 	self:InvalidateLayout()
 	self:SetTextColor( Color( 0, 50, 255 ) )
-	
+
 end
 
 --[[---------------------------------------------------------
 	UpdateColours
 -----------------------------------------------------------]]
 function PANEL:UpdateColours( skin )
-
 end
-
 
 derma.DefineControl( "DLabelURL", "A Label", PANEL, "URLLabel" )
