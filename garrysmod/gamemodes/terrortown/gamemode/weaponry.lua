71103c91f2802b7168c6c7fb78e2b8557e1b6098
Markus <markus28988@gmail.com>
2016-05-16 20:05:40 +0200

    TTT: Fixed edge case where Spectators could pick up weapons (#1177)

--- garrysmod/gamemodes/terrortown/gamemode/weaponry.lua
+++ garrysmod/gamemodes/terrortown/gamemode/weaponry.lua
@@ -8,6 +8,7 @@ local IsEquipment = WEPS.IsEquipment
 -- Prevent players from picking up multiple weapons of the same type etc
 function GM:PlayerCanPickupWeapon(ply, wep)
    if not IsValid(wep) and not IsValid(ply) then return end
+   if ply:IsSpec() then return false end
 
    -- Disallow picking up for ammo
    if ply:HasWeapon(wep:GetClass()) then
