4630abaa4c126358d2dd0520d470767e1e4af37e
Robotboy655 <robotboy655@gmail.com>
2016-06-22 03:06:55 +0300

    Cleaned up gmod_tool
    Improved handling of TOOL.Information with "use" name

--- garrysmod/gamemodes/sandbox/entities/weapons/gmod_tool/object.lua
+++ garrysmod/gamemodes/sandbox/entities/weapons/gmod_tool/object.lua
@@ -1,60 +1,44 @@
-	
-	
---[[---------------------------------------------------------
-   Sets which stage a tool is at
------------------------------------------------------------]]
+
 function ToolObj:UpdateData()
-	
+
 	self:SetStage( self:NumObjects() )
-	
+
 end
-	
---[[---------------------------------------------------------
-   Sets which stage a tool is at
------------------------------------------------------------]]
+
 function ToolObj:SetStage( i )
-	
+
 	if ( SERVER ) then
 		self:GetWeapon():SetNWInt( "Stage", i, true )
 	end
-	
+
 end
 
---[[---------------------------------------------------------
-   Gets which stage a tool is at
------------------------------------------------------------]]
 function ToolObj:GetStage()
 	return self:GetWeapon():GetNWInt( "Stage", 0 )
 end
 
---[[---------------------------------------------------------
------------------------------------------------------------]]
+function ToolObj:SetOperation( i )
+
+	if ( SERVER ) then
+		self:GetWeapon():SetNWInt( "Op", i, true )
+	end
+
+end
+
 function ToolObj:GetOperation()
 	return self:GetWeapon():GetNWInt( "Op", 0 )
 end
 
---[[---------------------------------------------------------
------------------------------------------------------------]]
-function ToolObj:SetOperation( i )
-	
-	if ( SERVER ) then
-		self:GetWeapon():SetNWInt( "Op", i, true )
-	end
-	
-end
 
---[[---------------------------------------------------------
-   ClearObjects - clear the selected objects
------------------------------------------------------------]]
+-- Clear the selected objects
 function ToolObj:ClearObjects()
 
 	self:ReleaseGhostEntity()
 	self.Objects = {}
 	self:SetStage( 0 )
 	self:SetOperation( 0 )
-	
-end
 
+end
 
 --[[---------------------------------------------------------
 	Since we're going to be expanding this a lot I've tried
@@ -63,8 +47,8 @@ end
 -----------------------------------------------------------]]
 function ToolObj:GetEnt( i )
 
-	if (!self.Objects[i]) then return NULL end
-	
+	if ( !self.Objects[i] ) then return NULL end
+
 	return self.Objects[i].Ent
 end
 
@@ -76,55 +60,47 @@ end
 -----------------------------------------------------------]]
 function ToolObj:GetPos( i )
 
-	if (self.Objects[i].Ent:EntIndex() == 0) then
+	if ( self.Objects[i].Ent:EntIndex() == 0 ) then
 		return self.Objects[i].Pos
 	else
-		if (self.Objects[i].Phys ~= nil && self.Objects[i].Phys:IsValid()) then
-			return self.Objects[i].Phys:LocalToWorld(self.Objects[i].Pos)
+		if ( IsValid( self.Objects[i].Phys ) ) then
+			return self.Objects[i].Phys:LocalToWorld( self.Objects[i].Pos )
 		else
-			return self.Objects[i].Ent:LocalToWorld(self.Objects[i].Pos)
+			return self.Objects[i].Ent:LocalToWorld( self.Objects[i].Pos )
 		end
 	end
-	
+
 end
 
---[[---------------------------------------------------------
-	Returns the local position of the numbered hit
------------------------------------------------------------]]
+-- Returns the local position of the numbered hit
 function ToolObj:GetLocalPos( i )
 	return self.Objects[i].Pos
 end
 
-
---[[---------------------------------------------------------
-	Returns the physics bone number of the hit (ragdolls)
------------------------------------------------------------]]
+-- Returns the physics bone number of the hit (ragdolls)
 function ToolObj:GetBone( i )
 	return self.Objects[i].Bone
 end
 
 function ToolObj:GetNormal( i )
-	if (self.Objects[i].Ent:EntIndex() == 0) then
+	if ( self.Objects[i].Ent:EntIndex() == 0 ) then
 		return self.Objects[i].Normal
 	else
 		local norm
-		if (self.Objects[i].Phys ~= nil && self.Objects[i].Phys:IsValid()) then
-			norm = self.Objects[i].Phys:LocalToWorld(self.Objects[i].Normal)
+		if ( IsValid( self.Objects[i].Phys ) ) then
+			norm = self.Objects[i].Phys:LocalToWorld( self.Objects[i].Normal )
 		else
-			norm = self.Objects[i].Ent:LocalToWorld(self.Objects[i].Normal)
+			norm = self.Objects[i].Ent:LocalToWorld( self.Objects[i].Normal )
 		end
-		
+
 		return norm - self:GetPos(i)
 	end
 end
 
-
---[[---------------------------------------------------------
-	Returns the physics object for the numbered hit
------------------------------------------------------------]]
+-- Returns the physics object for the numbered hit
 function ToolObj:GetPhys( i )
 
-	if (self.Objects[i].Phys == nil) then
+	if ( self.Objects[i].Phys == nil ) then
 		return self:GetEnt(i):GetPhysicsObject()
 	end
 
@@ -132,9 +108,7 @@ function ToolObj:GetPhys( i )
 end
 
 
---[[---------------------------------------------------------
-	Sets a selected object
------------------------------------------------------------]]
+-- Sets a selected object
 function ToolObj:SetObject( i, ent, pos, phys, bone, norm )
 
 	self.Objects[i] = {}
@@ -144,56 +118,50 @@ function ToolObj:SetObject( i, ent, pos, phys, bone, norm )
 	self.Objects[i].Normal = norm
 
 	-- Worldspawn is a special case
-	if (ent:EntIndex() == 0) then
+	if ( ent:EntIndex() == 0 ) then
 
 		self.Objects[i].Phys = nil
 		self.Objects[i].Pos = pos
-		
+
 	else
-		
+
 		norm = norm + pos
-	
+
 		-- Convert the position to a local position - so it's still valid when the object moves
 		if ( IsValid( phys ) ) then
-			self.Objects[i].Normal = self.Objects[i].Phys:WorldToLocal(norm)
-			self.Objects[i].Pos = self.Objects[i].Phys:WorldToLocal(pos)
+			self.Objects[i].Normal = self.Objects[i].Phys:WorldToLocal( norm )
+			self.Objects[i].Pos = self.Objects[i].Phys:WorldToLocal( pos )
 		else
-			self.Objects[i].Normal = self.Objects[i].Ent:WorldToLocal(norm)
-			self.Objects[i].Pos = self.Objects[i].Ent:WorldToLocal(pos)
+			self.Objects[i].Normal = self.Objects[i].Ent:WorldToLocal( norm )
+			self.Objects[i].Pos = self.Objects[i].Ent:WorldToLocal( pos )
 		end
-		
+
 	end
-	
-	if (SERVER) then
+
+	if ( SERVER ) then
 		-- Todo: Make sure the client got the same info
 	end
-	
+
 end
 
 
---[[---------------------------------------------------------
-	Returns the number of objects in the list
------------------------------------------------------------]]
+-- Returns the number of objects in the list
 function ToolObj:NumObjects()
 
 	if ( CLIENT ) then
-	
+
 		return self:GetStage()
-	
+
 	end
 
 	return #self.Objects
-	
+
 end
 
 
---[[---------------------------------------------------------
-	Returns the number of objects in the list
------------------------------------------------------------]]
+-- Returns the number of objects in the list
 function ToolObj:GetHelpText()
 
 	return "#tool." .. GetConVarString( "gmod_toolmode" ) .. "." .. self:GetStage()
-	
+
 end
-
-
