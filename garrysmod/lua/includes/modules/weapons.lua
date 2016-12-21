c7d6ee5f475ce27551b2a750d4e21e2ffa7c7b0b
Collin (code_gs) <collings509@gmail.com>
2016-12-21 01:42:18 -0600

    Weapons now copy their userdata and metatables

--- garrysmod/lua/includes/modules/weapons.lua
+++ garrysmod/lua/includes/modules/weapons.lua
@@ -10,18 +10,35 @@ local WeaponList = {}
 local function TableInherit( t, base )
 
 	for k, v in pairs( base ) do
-
 		if ( t[ k ] == nil ) then
 			t[ k ] = v
-		elseif ( k != "BaseClass" && istable( t[ k ] ) ) then
+		elseif ( istable( t[ k ] ) && istable( v ) ) then
 			TableInherit( t[ k ], v )
 		end
-
 	end
 
-	t[ "BaseClass" ] = base
+end
 
-	return t
+local function DeepCopy( t )
+
+	local new = {}
+	setmetatable( new, debug.getmetatable( t ) )
+
+	for k, v in pairs( t ) do
+		if ( istable( v ) ) then
+			new[ k ] = DeepCopy( v )
+		elseif ( isvector( v ) ) then
+			new[ k ] = Vector( v )
+		elseif ( isangle( v ) ) then
+			new[ k ] = Angle( v )
+		--[[elseif ( ismatrix( v ) ) then
+			new[ k ] = Matrix( v )]]
+		else
+			new[ k ] = v
+		end
+	end
+
+	return new
 
 end
 
@@ -63,6 +80,10 @@ function Register( t, name )
 	-- Allow all SWEPS to be duplicated, unless specified
 	if ( !t.DisableDuplicator ) then
 		duplicator.Allow( name )
+	end
+
+	if ( !t.Base ) then
+		retval.Base = "weapon_base"
 	end
 
 	--
@@ -130,27 +151,36 @@ end
 function Get( name )
 
 	local Stored = GetStored( name )
-	if ( !Stored ) then return nil end
+
+	if ( !Stored ) then
+		return nil
+	end
 
 	-- Create/copy a new table
-	local retval = table.Copy( Stored )
-	retval.Base = retval.Base or "weapon_base"
+	local retval = DeepCopy( Stored )
 
 	-- If we're not derived from ourselves (a base weapon)
 	-- then derive from our 'Base' weapon.
 	if ( retval.Base != name ) then
-
 		local BaseWeapon = Get( retval.Base )
 
-		if ( !BaseWeapon ) then
-			Msg( "SWEP (", name, ") is derived from non existant SWEP (", retval.Base, ") - Expect errors!\n" )
-		else
-			retval = TableInherit( retval, Get( retval.Base ) )
-		end
+		if ( BaseWeapon ) then
+			for k, v in pairs( BaseWeapon ) do
+				if ( retval[ k ] == nil ) then
+					retval[ k ] = v
+				elseif ( k != "BaseClass" && istable( retval[ k ] ) && istable( v ) ) then
+					TableInherit( retval[ k ], v )
+				end
+			end
 
+			retval[ "BaseClass" ] = BaseWeapon
+		else
+			MsgN( "SWEP (", name, ") is derived from non-existant SWEP (", retval.Base, ") - expect errors!" )
+		end
 	end
 
 	return retval
+
 end
 
 --[[---------------------------------------------------------
