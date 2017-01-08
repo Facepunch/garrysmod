Facepunch/garrysmod

commit 4780d871a6a5595e89cd86a39d702db3428389ce

TTT: use net msg for moving AFK players to spectator (#1279)
Makes sure aliasing/rebindin the spectator mode console command does not prevent AFK players from being moved to the spectator team.

garrysmod/gamemodes/terrortown/gamemode/cl_init.lua
@@ -52,7 +52,9 @@ function GM:Initialize()
 
    self.BaseClass:Initialize()
 
-   RunConsoleCommand("ttt_spectate", GetConVar("ttt_spectator_mode"):GetInt())
+   net.Start("TTT_Spectate")
+     net.WriteBool(GetConVar("ttt_spectator_mode"):GetBool())
+   net.SendToServer()
 end
 
 function GM:InitPostEntity()
@@ -368,6 +370,9 @@ function CheckIdle()
 
          timer.Simple(0.3, function()
                               RunConsoleCommand("ttt_spectator_mode", 1)
+                               net.Start("TTT_Spectate")
+                                 net.WriteBool(true)
+                               net.SendToServer()
                               RunConsoleCommand("ttt_cl_idlepopup")
                            end)
       elseif CurTime() > (idle.t + (idle_limit / 2)) then

garrysmod/gamemodes/terrortown/gamemode/init.lua
@@ -154,7 +154,7 @@ util.AddNetworkString("TTT_ShowPrints")
 util.AddNetworkString("TTT_ScanResult")
 util.AddNetworkString("TTT_FlareScorch")
 util.AddNetworkString("TTT_Radar")
-
+util.AddNetworkString("TTT_Spectate")
 ---- Round mechanics
 function GM:Initialize()
    MsgN("Trouble In Terrorist Town gamemode initializing...")

garrysmod/gamemodes/terrortown/gamemode/traitor_state.lua
@@ -1,4 +1,3 @@
-
 function GetTraitors()
    local trs = {}
    for k,v in ipairs(player.GetAll()) do
@@ -174,5 +173,7 @@ local function force_spectate(ply, cmd, arg)
    end
 end
 concommand.Add("ttt_spectate", force_spectate)
-
+net.Receive("TTT_Spectate", function(l, pl)
+   force_spectate(pl, nil, { net.ReadBool() and 1 or 0 })
+end)
 

commit 648d5c7c221c815b1d6512fc23913408932a0db9

TTT: fix spectator mode not syncing after level change
Should fix problem with spectator mode losing its effect after a map
switch, introduced by 4780d871a6a.

When sent in `Initialize`, the net msg didn't seem to actually arrive.
We don't actually need it to be that early in the process, and moving it
to `InitPostEntity` works.

garrysmod/gamemodes/terrortown/gamemode/cl_init.lua
@@ -51,14 +51,14 @@ function GM:Initialize()
    LANG.Init()
 
    self.BaseClass:Initialize()
-
-   net.Start("TTT_Spectate")
-     net.WriteBool(GetConVar("ttt_spectator_mode"):GetBool())
-   net.SendToServer()
 end
 
 function GM:InitPostEntity()
    MsgN("TTT Client post-init...")
+
+   net.Start("TTT_Spectate")
+     net.WriteBool(GetConVar("ttt_spectator_mode"):GetBool())
+   net.SendToServer()
 
    if not game.SinglePlayer() then
       timer.Create("idlecheck", 5, 0, CheckIdle)