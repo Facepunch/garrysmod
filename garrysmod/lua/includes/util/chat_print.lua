acd5ce5e09af8ed50afb97337a1abde8b5a97078
Bradley Walters <zerfgog202@gmail.com>
2017-02-18 18:21:32 -0700

    Make chat_print.lua adhere to GMod repo code style

--- garrysmod/lua/includes/util/chat_print.lua
+++ garrysmod/lua/includes/util/chat_print.lua
@@ -1,52 +1,52 @@
-if SERVER then
-	util.AddNetworkString("_ChatPrint")
+if ( SERVER ) then
+	util.AddNetworkString( "_ChatPrint" )
 end
 
-if CLIENT then
-	net.Receive("_ChatPrint", function()
+if ( CLIENT ) then
+	net.Receive( "_ChatPrint", function()
 		local parts = {}
 
-		for _ = 1, net.ReadUInt(16) do
-			parts[#parts + 1] = net.ReadType()
+		for _ = 1, net.ReadUInt( 16 ) do
+			parts[ #parts + 1 ] = net.ReadType()
 		end
 
-		chat.AddText(unpack(parts))
-	end)
+		chat.AddText( unpack( parts ) )
+	end )
 end
 
-local function WriteChatParts(...)
-	local parts = {...}
+local function WriteChatParts( ... )
+	local parts = { ... }
 
-	net.WriteUInt(#parts, 16)
+	net.WriteUInt( #parts, 16 )
 
 	for i = 1, #parts do
-		net.WriteType(parts[i])
+		net.WriteType( parts[i] )
 	end
 end
 
-if CLIENT then
+if ( CLIENT ) then
 	ChatPrint = chat.AddText
 else
 	-- Prints a colored message to the chat box from the client or server.
-	function ChatPrint(...)
-		net.Start("_ChatPrint")
-			WriteChatParts(...)
+	function ChatPrint( ... )
+		net.Start( "_ChatPrint" )
+			WriteChatParts( ... )
 		net.Broadcast()
 	end
 end
 
-local PLAYER = FindMetaTable("Player")
+local PLAYER = FindMetaTable( "Player" )
 
 -- Prints a colored message to the chat box of the given player
 -- from the client or server.
-function PLAYER:ChatPrint(...)
-	if CLIENT then
-		if self == LocalPlayer() then
-			chat.AddText(...)
+function PLAYER:ChatPrint( ... )
+	if ( CLIENT ) then
+		if ( self == LocalPlayer() ) then
+			chat.AddText( ... )
 		end
 	else
-		net.Start("_ChatPrint")
-			WriteChatParts(...)
-		net.Send(self)
+		net.Start( "_ChatPrint" )
+			WriteChatParts( ... )
+		net.Send( self )
 	end
 end
