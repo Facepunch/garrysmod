67b83b1725c600ef95973e51bc283c8c57619d0e
Robotboy655 <robotboy655@gmail.com>
2017-04-29 07:58:05 +0300

    Clamp certain Lamp Tool params in MP

--- garrysmod/gamemodes/sandbox/entities/entities/gmod_lamp.lua
+++ garrysmod/gamemodes/sandbox/entities/entities/gmod_lamp.lua
@@ -109,15 +109,19 @@ function ENT:OnSwitch( bOn )
 
 	-- The local positions are the offsets from parent..
 	self.flashlight:SetLocalPos( Vector( 0, 0, 0 ) )
-	self.flashlight:SetLocalAngles( Angle(0,0,0) )
+	self.flashlight:SetLocalAngles( Angle( 0, 0, 0 ) )
 
 	self.flashlight:SetKeyValue( "enableshadows", 1 )
-	self.flashlight:SetKeyValue( "farz", self:GetDistance() )
 	self.flashlight:SetKeyValue( "nearz", 12 )
-	self.flashlight:SetKeyValue( "lightfov", self:GetLightFOV() )
+	self.flashlight:SetKeyValue( "lightfov", math.Clamp( self:GetLightFOV(), 10, 170 ) ) 
+
+	local dist = self:GetDistance()
+	if ( !game.SinglePlayer() ) then dist = math.Clamp( dist, 64, 2048 ) end
+	self.flashlight:SetKeyValue( "farz", dist )
 
 	local c = self:GetColor()
 	local b = self:GetBrightness()
+	if ( !game.SinglePlayer() ) then b = math.Clamp( b, 0, 8 ) end
 	self.flashlight:SetKeyValue( "lightcolor", Format( "%i %i %i 255", c.r * b, c.g * b, c.b * b ) )
 
 	self.flashlight:Spawn()
@@ -141,12 +145,14 @@ function ENT:OnUpdateLight( name, old, new )
 	if ( !IsValid( self.flashlight ) ) then return end
 
 	if ( name == "LightFOV" ) then
-		self.flashlight:Input( "FOV", NULL, NULL, tostring( new ) )
+		self.flashlight:Input( "FOV", NULL, NULL, tostring( math.Clamp( new, 10, 170 ) ) )
 	elseif ( name == "Distance" ) then
-		self.flashlight:SetKeyValue( "farz", self:GetDistance() )
+		if ( !game.SinglePlayer() ) then new = math.Clamp( new, 64, 2048 ) end
+		self.flashlight:SetKeyValue( "farz", new )
 	elseif ( name == "Brightness" ) then
 		local c = self:GetColor()
-		local b = self:GetBrightness()
+		local b = new
+		if ( !game.SinglePlayer() ) then b = math.Clamp( b, 0, 8 ) end
 		self.flashlight:SetKeyValue( "lightcolor", Format( "%i %i %i 255", c.r * b, c.g * b, c.b * b ) )
 	end
 
@@ -157,11 +163,15 @@ function ENT:UpdateLight()
 	if ( !IsValid( self.flashlight ) ) then return end
 
 	self.flashlight:Input( "SpotlightTexture", NULL, NULL, self:GetFlashlightTexture() )
-	self.flashlight:Input( "FOV", NULL, NULL, tostring( self:GetLightFOV() ) )
-	self.flashlight:SetKeyValue( "farz", self:GetDistance() )
+	self.flashlight:Input( "FOV", NULL, NULL, tostring( math.Clamp( self:GetLightFOV(), 10, 170 ) ) )
+
+	local dist = self:GetDistance()
+	if ( !game.SinglePlayer() ) then dist = math.Clamp( dist, 64, 2048 ) end
+	self.flashlight:SetKeyValue( "farz", dist )
 
 	local c = self:GetColor()
 	local b = self:GetBrightness()
+	if ( !game.SinglePlayer() ) then b = math.Clamp( b, 0, 8 ) end
 	self.flashlight:SetKeyValue( "lightcolor", Format( "%i %i %i 255", c.r * b, c.g * b, c.b * b ) )
 
 end
