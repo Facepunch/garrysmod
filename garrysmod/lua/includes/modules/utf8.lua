4849b9363c49bb674b1ffb8b4316fa2696f8970e
Daniel Hultgren <daniel.cf.hultgren@gmail.com>
2017-08-14 13:10:59 +0200

    Added utf8.sub and utf8.idx

--- garrysmod/lua/includes/modules/utf8.lua
+++ garrysmod/lua/includes/modules/utf8.lua
@@ -75,7 +75,7 @@ local function decode( str, startPos )
 
 	-- Validate our continuation bytes
 	for _, bX in ipairs { str:byte( startPos + 1, endPos ) } do
-		
+
 		-- Invalid continuation byte hit
 		if bit.band( bX, 0xC0 ) ~= 0x80 then
 			return nil
@@ -316,7 +316,7 @@ function force( str )
 	end
 
 	repeat
-		
+
 		local seqStartPos, seqEndPos = decode( str, curPos )
 
 		if not seqStartPos then
@@ -335,4 +335,43 @@ function force( str )
 
 	return table.concat( buf, "" )
 
+end
+
+--
+-- Converts a relative index to an absolute
+-- This is different from the above in that it cares about characters and not bytes
+--
+local function strRelToAbsChar( str, pos )
+	if pos < 0 then
+		pos = math.max( pos + utf8.len( str ) + 1, 0 )
+	end
+	return pos
+end
+
+--
+-- UTF-8 compilant version of str[idx]
+--
+function idx( str, idx )
+	idx = strRelToAbsChar( str, idx )
+
+	if idx == 0 then return "" end
+	if idx > utf8.len( str ) then return "" end
+
+	local offset = utf8.offset( str, idx - 1 )
+	return utf8.char( utf8.codepoint( str, offset ) )
+end
+
+--
+-- UTF-8 compilant version of string.sub
+--
+function sub( str, charstart, charend )
+	charstart = strRelToAbsChar( str, charstart )
+	charend = strRelToAbsChar( str, charend )
+
+	local buf = {}
+	for i = charstart, charend do
+		buf[#buf + 1] = utf8.idx( str, i )
+	end
+
+	return table.concat( buf )
 end
