870ef40bd76c92d2489380389c33fce5f2ae56e5
Deyan Dobromirov <dvd_video@abv.bg>
2017-10-10 15:53:59 +0300

    Add ability to return copy of the rotated vector

--- garrysmod/lua/includes/extensions/vector.lua
+++ garrysmod/lua/includes/extensions/vector.lua
@@ -3,10 +3,21 @@ local meta = FindMetaTable( "Vector" )
 -- Nothing in here, still leaving this file here just in case
 
 --[[---------------------------------------------------------
-	Converts Vector To Color - alpha precision lost, must reset
+Converts Vector To Color - alpha precision lost, must reset
 -----------------------------------------------------------]]
 function meta:ToColor( )
 
 	return Color( self.x * 255, self.y * 255, self.z * 255 )
 
+end
+
+--[[---------------------------------------------------------
+Returns a copy of a rotated vector. Kind of like v:GetNormalized()
+-----------------------------------------------------------]]
+function meta:GetRotated(ang)
+  if(not ang) then return nil end
+  local v = Vector()
+	v:Set(self)
+  v:Rotate(ang)
+  return v
 end
