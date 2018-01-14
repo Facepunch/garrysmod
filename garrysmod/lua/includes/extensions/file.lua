3d75ff11cbddb79888a394f9ce4506b9a70a2a3e
H3xCat <edgarasf123@users.noreply.github.com>
2018-01-14 07:50:46 -0800

    Fix spaces

--- garrysmod/lua/includes/extensions/file.lua
+++ garrysmod/lua/includes/extensions/file.lua
@@ -8,17 +8,17 @@ meta.ReadInt32 = meta.ReadLong
 
 function meta:WriteUInt32( int32 )
 	if int32 >= 0x80000000 then
-        int32 = int32 - 0x100000000
-    end
-    self:WriteInt32( int32 )
+		int32 = int32 - 0x100000000
+	end
+	self:WriteInt32( int32 )
 end
 
 function meta:ReadUInt32()
-    local int32 = self:ReadInt32()
-    if int32 < 0 then
-        int32 = int32 + 0x100000000
-    end
-    return int32
+	local int32 = self:ReadInt32()
+	if int32 < 0 then
+		int32 = int32 + 0x100000000
+	end
+	return int32
 end
 
 --
@@ -28,18 +28,18 @@ meta.WriteInt16 = meta.WriteShort
 meta.ReadInt16 = meta.ReadShort
 
 function meta:WriteUInt16( int16 )
-    if int16 >= 0x8000 then
-        int16 = int16 - 0x10000
-    end
-    self:WriteInt16( int16 )
+	if int16 >= 0x8000 then
+		int16 = int16 - 0x10000
+	end
+	self:WriteInt16( int16 )
 end
 
 function meta:ReadUInt16()
-    local int16 = self:ReadInt16()
-    if int16 < 0 then
-        int16 = int16 + 0x10000
-    end
-    return int16
+	local int16 = self:ReadInt16()
+	if int16 < 0 then
+		int16 = int16 + 0x10000
+	end
+	return int16
 end
 
 --
@@ -49,18 +49,18 @@ meta.WriteUInt8 = meta.WriteByte
 meta.ReadUInt8 = meta.ReadByte
 
 function meta:WriteInt8( int8 )
-    if int8 >= 0x80 then
-        int8 = int8 - 0x100
-    end
-    self:WriteUInt8( int8 )
+	if int8 >= 0x80 then
+		int8 = int8 - 0x100
+	end
+	self:WriteUInt8( int8 )
 end
 
 function meta:ReadInt8( )
-    local int8 = self:ReadUInt8()
-    if int8 < 0 then
-        int = int8 + 0x100
-    end
-    return int8
+	local int8 = self:ReadUInt8()
+	if int8 < 0 then
+		int = int8 + 0x100
+	end
+	return int8
 end
 
 function file.Read( filename, path )
