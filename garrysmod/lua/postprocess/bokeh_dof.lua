03c398bbef597f145c4e346664d2c071a44dc54a
Rubat <robotboy655@gmail.com>
2018-01-26 09:26:52 +0200

    Update bokeh_dof.lua

--- garrysmod/lua/postprocess/bokeh_dof.lua
+++ garrysmod/lua/postprocess/bokeh_dof.lua
@@ -45,5 +45,5 @@ hook.Add( "RenderScreenspaceEffects", "RenderBokeh", function()
 end )
 
 hook.Add( "NeedsDepthPass", "NeedsDepthPass_Bokeh", function()
-	if pp_bokeh:GetBool() then return true end 
+	if ( pp_bokeh:GetBool() ) then return true end 
 end)
