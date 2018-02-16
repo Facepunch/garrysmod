4507e3ea81962f1dba32d42fa05f020144a7ab9f
Robotboy655 <robotboy655@gmail.com>
2018-02-16 09:09:39 +0200

    DProperty_VectorColor/Tooltip changes
    DProperty_VectorColor now works with standard colors as well ( vector
    color support unchanged )
    Applied a hack to stop tooltips from being displayed when they are not
    meant to be displayed

--- garrysmod/lua/includes/util/tooltips.lua
+++ garrysmod/lua/includes/util/tooltips.lua
@@ -1,70 +1,61 @@
---=============================================================================--
---  ___  ___   _   _   _    __   _   ___ ___ __ __
--- |_ _|| __| / \ | \_/ |  / _| / \ | o \ o \\ V /
---  | | | _| | o || \_/ | ( |_n| o ||   /   / \ / 
---  |_| |___||_n_||_| |_|  \__/|_n_||_|\\_|\\ |_|  2007
---										 
---=============================================================================--
 
 local Tooltip = nil
 local TooltippedPanel = nil
 
 --[[---------------------------------------------------------
 	Name: ChangeTooltip
-		Called from engine on hovering over a panel
------------------------------------------------------------]] 
-function RemoveTooltip( PositionPanel )
+	Called from engine on hovering over a panel
+-----------------------------------------------------------]]
+function RemoveTooltip()
 
 	if ( !IsValid( Tooltip ) ) then return true end
-		
+
 	Tooltip:Close()
 	Tooltip = nil
+
 	return true
 
 end
 
 --[[---------------------------------------------------------
 	Name: FindTooltip
------------------------------------------------------------]] 
+-----------------------------------------------------------]]
 function FindTooltip( panel )
 
-	--
 	-- Look at the parent panel for tooltips.
-	--
-	while ( panel && panel:IsValid() ) do
-	
+	while ( IsValid( panel ) ) do
+
 		if ( panel.strTooltipText || panel.pnlTooltipPanel ) then
 			return panel.strTooltipText, panel.pnlTooltipPanel, panel
 		end
-		
+
 		panel = panel:GetParent()
-	
+
 	end
-	
+
 end
 
 --[[---------------------------------------------------------
 	Name: ChangeTooltip
-		Called from engine on hovering over a panel
------------------------------------------------------------]] 
+	Called from engine on hovering over a panel
+-----------------------------------------------------------]]
 function ChangeTooltip( panel )
 
 	RemoveTooltip()
-	
+
 	local Text, Panel, PositionPanel = FindTooltip( panel )
-	
 	if ( !Text && !Panel ) then return end
 
 	Tooltip = vgui.Create( "DTooltip" )
-	
+
 	if ( Text ) then
-	
+
 		Tooltip:SetText( Text )
-		
+
 	else
-	
+
 		Tooltip:SetContents( Panel, false )
-	
+
 	end
 
 	Tooltip:OpenForPanel( PositionPanel )
@@ -74,12 +65,15 @@ end
 
 --[[---------------------------------------------------------
 	Name: EndTooltip
-		Called from engine on exiting from a panel
------------------------------------------------------------]] 
+	Called from engine on exiting from a panel
+-----------------------------------------------------------]]
 function EndTooltip( panel )
 
-	if ( !TooltippedPanel ) then return end
-	if ( TooltippedPanel != panel ) then return end
+	-- Don't do these checks, the engine has a problem with determing
+	-- which panel is currently being hovered in some very specific and unknown conditions
+
+	--if ( !IsValid( TooltippedPanel ) ) then return end
+	--if ( TooltippedPanel != panel ) then return end
 
 	RemoveTooltip()
 
