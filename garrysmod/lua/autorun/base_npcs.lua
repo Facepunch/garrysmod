8a49de7e3e8419a911272695b7c052652bd123fc
Robotboy655 <robotboy655@gmail.com>
2018-04-26 12:21:31 +0300

    Randomize HLS scientists

--- garrysmod/lua/autorun/base_npcs.lua
+++ garrysmod/lua/autorun/base_npcs.lua
@@ -476,7 +476,7 @@ if ( IsMounted( "hl1" ) ) then
 	AddNPC( { Name = "Grunt", Class = "monster_human_grunt", Category = Category } )
 	AddNPC( { Name = "Cockroach", Class = "monster_cockroach", Category = Category } )
 	AddNPC( { Name = "Houndeye", Class = "monster_houndeye", Category = Category } )
-	AddNPC( { Name = "Scientist", Class = "monster_scientist", Category = Category } )
+	AddNPC( { Name = "Scientist", Class = "monster_scientist", Category = Category, KeyValues = { body = "-1" } } )
 	AddNPC( { Name = "Snark", Class = "monster_snark", Category = Category } )
 	AddNPC( { Name = "Zombie", Class = "monster_zombie", Category = Category } )
 	AddNPC( { Name = "Headcrab", Class = "monster_headcrab", Category = Category } )
