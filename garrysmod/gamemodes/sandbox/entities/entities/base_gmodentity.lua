668cdaf9b2335da149f211f5386efc678fbb24ad
Collin (code_gs) <collings509@gmail.com>
2018-07-18 10:14:13 -0500

    ENT:BeingLookedAtByLocalPlayer optimisation
    - ENT.ViewDistance can now set the maximum distance the worldtip is displayed at when looking at an entity.
    - The distance check now uses the view entity's ShootPos if its a player, just like the local player does. This makes the result consistent between a player and spectators.
    - Dist->DistToSqr
    - Order of operations changed in ENT:Think and ENT:BeingLookedAtByLocalPlayer to do traces last - optimises entities too far from the player from doing an unnecessary eyetrace.

--- garrysmod/gamemodes/sandbox/entities/entities/base_gmodentity.lua
+++ garrysmod/gamemodes/sandbox/entities/entities/base_gmodentity.lua
@@ -5,29 +5,35 @@ DEFINE_BASECLASS( "base_anim" )
 ENT.Spawnable = false
 
 if ( CLIENT ) then
+	ENT.ViewDistance = 256
 
 	function ENT:BeingLookedAtByLocalPlayer()
+		local ply = LocalPlayer()
+		if ( not IsValid( ply ) ) then return false end
 
-		if ( LocalPlayer():GetEyeTrace().Entity != self ) then return false end
-		if ( LocalPlayer():GetViewEntity() == LocalPlayer() && LocalPlayer():GetShootPos():Distance( self:GetPos() ) > 256 ) then return false end
-		if ( LocalPlayer():GetViewEntity() != LocalPlayer() && LocalPlayer():GetViewEntity():GetPos():Distance( self:GetPos() ) > 256 ) then return false end
+		local view = ply:GetViewEntity()
+		local dist = self.ViewDistance
+		dist = dist * dist
+		local pos
 
-		return true
+		if ( view:IsPlayer() ) then
+			pos = view:GetShootPos()
+		else
+			pos = view:GetPos()
+		end
 
+		return pos:DistToSqr( self:GetPos() ) <= dist && ply:GetEyeTrace().Entity == self
 	end
 
-end
+	function ENT:Think()
+		local text = self:GetOverlayText()
 
-function ENT:Think()
+		if ( text != "" && self:BeingLookedAtByLocalPlayer() ) then
+			AddWorldTip( self:EntIndex(), text, 0.5, self:GetPos(), self )
 
-	if ( CLIENT && self:BeingLookedAtByLocalPlayer() && self:GetOverlayText() != "" ) then
-
-		AddWorldTip( self:EntIndex(), self:GetOverlayText(), 0.5, self:GetPos(), self.Entity )
-
-		halo.Add( { self }, Color( 255, 255, 255, 255 ), 1, 1, 1, true, true )
-
+			halo.Add( { self }, color_white, 1, 1, 1, true, true )
+		end
 	end
-
 end
 
 function ENT:SetOverlayText( text )
