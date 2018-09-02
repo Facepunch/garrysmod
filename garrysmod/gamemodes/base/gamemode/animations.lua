6149a2285f98d5003569db3927ab95ffdb376cf2
Garrett Brown <grbrown93@sbcglobal.net>
2018-09-01 20:23:30 -0500

    Optimize mouth animation

--- garrysmod/gamemodes/base/gamemode/animations.lua
+++ garrysmod/gamemodes/base/gamemode/animations.lua
@@ -266,20 +266,22 @@ end
 --
 function GM:MouthMoveAnimation( ply )
 
-	local flexes = {
-		ply:GetFlexIDByName( "jaw_drop" ),
-		ply:GetFlexIDByName( "left_part" ),
-		ply:GetFlexIDByName( "right_part" ),
-		ply:GetFlexIDByName( "left_mouth_drop" ),
-		ply:GetFlexIDByName( "right_mouth_drop" )
-	}
+	if ply:IsSpeaking() then
+		local flexes = {
+			ply:GetFlexIDByName( "jaw_drop" ),
+			ply:GetFlexIDByName( "left_part" ),
+			ply:GetFlexIDByName( "right_part" ),
+			ply:GetFlexIDByName( "left_mouth_drop" ),
+			ply:GetFlexIDByName( "right_mouth_drop" )
+		}
 
-	local weight = ply:IsSpeaking() && math.Clamp( ply:VoiceVolume() * 2, 0, 2 ) || 0
+		local weight = math.Clamp( ply:VoiceVolume() * 2, 0, 2 )
 
-	for k, v in pairs( flexes ) do
+		for k, v in pairs( flexes ) do
 
-		ply:SetFlexWeight( v, weight )
+			ply:SetFlexWeight( v, weight )
 
+		end
 	end
 
 end
