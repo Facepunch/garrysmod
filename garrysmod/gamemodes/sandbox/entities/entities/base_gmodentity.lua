cf8a0a2f769d54c60e942a1e0b0a87a864a9edcf
Lex Robinson <lex@lex.me.uk>
2018-09-15 17:41:57 +0100

    Make GetPlayer support rejoins
    If a player disconnects and then rejoins generally entities want to
    continue as normal.

--- garrysmod/gamemodes/sandbox/entities/entities/base_gmodentity.lua
+++ garrysmod/gamemodes/sandbox/entities/entities/base_gmodentity.lua
@@ -74,11 +74,13 @@ function ENT:SetPlayer( ply )
 	if ( IsValid( ply ) ) then
 
 		self:SetNWString( "FounderName", ply:Nick() )
+		self.FounderSID = ply:SteamID64()
 		self.FounderIndex = ply:UniqueID()
 
 	else
 
 		self:SetNWString( "FounderName", "" )
+		self.FounderSID = nil
 		self.FounderIndex = nil
 
 	end
@@ -87,13 +89,42 @@ end
 
 function ENT:GetPlayer()
 
-	return self:GetVar( "Founder", NULL )
+	if ( self.Founder == nil ) then
+
+		-- SetPlayer has not been called
+		return NULL
+
+	elseif ( IsValid( self.Founder ) ) then
+
+		-- Normal operations
+		return self.Founder
+
+	end
+
+	-- See if the player has left the server then rejoined
+	local ply = player.GetBySteamID64( self.FounderSID )
+	if ( not IsValid( ply ) ) then
+
+		-- Oh well
+		return NULL
+
+	end
+
+	-- Save us the check next time
+	self:SetPlayer( ply )
+	return ply
 
 end
 
 function ENT:GetPlayerIndex()
 
-	return self:GetVar( "FounderIndex", 0 )
+	return self.FounderIndex or 0
+
+end
+
+function ENT:GetPlayerSteamID()
+
+	return self.FounderSID or ""
 
 end
 
