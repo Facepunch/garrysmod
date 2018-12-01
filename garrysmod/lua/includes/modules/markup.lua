1a5f02b25357321564a5108a8e4f3d1971ac6911
Collin (code_gs) <collings509@gmail.com>
2018-12-01 03:39:33 -0600

    Add MarkupObject metatable to the registry
    Solves https://github.com/Facepunch/garrysmod-requests/issues/1244

--- garrysmod/lua/includes/modules/markup.lua
+++ garrysmod/lua/includes/modules/markup.lua
@@ -9,6 +9,9 @@ local math = math
 local utf8 = utf8
 local Color = Color
 
+local MarkupObject = {}
+debug.getregistry().MarkupObject = MarkupObject
+
 module("markup")
 
 --[[---------------------------------------------------------
@@ -159,8 +162,6 @@ local function ProcessMatches( p1, p2, p3 )
 	if ( p2 ) then CheckTextOrTag( p2 ) end
 	if ( p3 ) then CheckTextOrTag( p3 ) end
 end
-
-local MarkupObject = {}
 
 --[[---------------------------------------------------------
 	Name: MarkupObject:Create()
