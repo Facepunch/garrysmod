2bc5fee85516124c98b658b450e3d6b8a9d5cfea
Collin (code_gs) <collings509@gmail.com>
2019-03-25 09:06:27 -0500

    Add HL1 and Garry's Mod language strings (#1562)
    * Update language files
    
    * Fix conflicts
    
    * Fix conflicts v2
    
    GitHub really hates text files
    
    * Reverted weapon_cubemap capitalisation changes
    
    * Updated HL1 weapons

--- garrysmod/lua/weapons/weapon_fists.lua
+++ garrysmod/lua/weapons/weapon_fists.lua
@@ -1,7 +1,7 @@
 
 AddCSLuaFile()
 
-SWEP.PrintName = "Fists"
+SWEP.PrintName = "#GMOD_Fists"
 SWEP.Author = "Kilburn, robotboy655, MaxOfS2D & Tenrys"
 SWEP.Purpose = "Well we sure as hell didn't use guns! We would just wrestle Hunters to the ground with our bare hands! I used to kill ten, twenty a day, just using my fists."
 
