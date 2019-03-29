65e47ff07e3da5fdddcdbbbf52ca620b2e186752
kpjVideo <masonticehurst@me.com>
2019-03-29 11:45:47 -0400

    Added SetInterval and GetInterval to DNumberWang (#1356)
    Allows for an easier way to add and subtract numbers higher/lower than 1 in DNumberWang.

--- garrysmod/lua/vgui/dnumberwang.lua
+++ garrysmod/lua/vgui/dnumberwang.lua
@@ -5,6 +5,7 @@ AccessorFunc( PANEL, "m_numMin",		"Min" )
 AccessorFunc( PANEL, "m_numMax",		"Max" )
 AccessorFunc( PANEL, "m_iDecimals",		"Decimals" ) -- The number of decimal places in the output
 AccessorFunc( PANEL, "m_fFloatValue",	"FloatValue" )
+AccessorFunc( PANEL, "m_iInterval",		"Interval" )
 
 -- AnchorValue and UnAnchorValue functions are internally used for "drag-changing" the value
 local function AnchorValue( wang, button, mcode )
@@ -35,7 +36,9 @@ function PANEL:Init()
 	self:SetDecimals( 2 )
 	self:SetTall( 20 )
 	self:SetMinMax( 0, 100 )
-
+	
+	self.m_iInterval = 1
+	
 	self:SetUpdateOnType( true )
 	self:SetNumeric( true )
 
@@ -43,7 +46,7 @@ function PANEL:Init()
 
 	self.Up = vgui.Create( "DButton", self )
 	self.Up:SetText( "" )
-	self.Up.DoClick = function( button, mcode ) self:SetValue( self:GetValue() + 1 ) end
+	self.Up.DoClick = function( button, mcode ) self:SetValue( self:GetValue() + self.m_iInterval ) end
 	self.Up.Paint = function( panel, w, h ) derma.SkinHook( "Paint", "NumberUp", panel, w, h ) end
 
 	self.Up.OldOnMousePressed = self.Up.OnMousePressed
@@ -54,7 +57,7 @@ function PANEL:Init()
 
 	self.Down = vgui.Create( "DButton", self )
 	self.Down:SetText( "" )
-	self.Down.DoClick = function( button, mcode ) self:SetValue( self:GetValue() - 1 ) end
+	self.Down.DoClick = function( button, mcode ) self:SetValue( self:GetValue() - self.m_iInterval ) end
 	self.Down.Paint = function( panel, w, h ) derma.SkinHook( "Paint", "NumberDown", panel, w, h ) end
 
 	self.Down.OldOnMousePressed = self.Down.OnMousePressed
@@ -94,6 +97,14 @@ function PANEL:SetMinMax( min, max )
 	self:SetMin( min )
 	self:SetMax( max )
 
+end
+
+function PANEL:SetInterval( val )
+	self.m_iInterval = tonumber( val ) or 1
+end
+
+function PANEL:GetInterval( val )
+	return tonumber( self.m_iInterval or 1 )
 end
 
 function PANEL:SetMin( min )
