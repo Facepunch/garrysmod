520ad22d3e2a2ede9fb701267f977ee4c830dc46
joeyjumper94 <joeyjumper94@gmail.com>
2019-05-01 10:34:02 -0700

    fix for using the CMenu in nonvehicle entities (#1581)
    * fix for using the CMenu in nonvehicle entities
    
    improved version of my fix.
    earlier description:
    if you open the context menu while in a prop_vehicle_crane you will get a lua error like this
    [ERROR] lua/includes/modules/properties.lua:114: attempt to call method 'GetThirdPersonMode' (a nil value)
      1. GetHovered - lua/includes/modules/properties.lua:114
       2. fn - lua/includes/modules/properties.lua:174
        3. Run - addons/ulib_557962238/lua/ulib/shared/hook.lua:109
         4. fn - lua/includes/modules/halo.lua:149
          5. unknown - addons/ulib_557962238/lua/ulib/shared/hook.lua:109
    
    
    this is because LocalPlayer():GetVehicle() return the crane you are in, so the IsValid check will return true, but since it's not an actual vehicle, calling a Vehicle function will cause a lua error.

--- garrysmod/lua/includes/modules/properties.lua
+++ garrysmod/lua/includes/modules/properties.lua
@@ -110,8 +110,16 @@ end
 
 function GetHovered( eyepos, eyevec )
 
-	local filter = { LocalPlayer():GetViewEntity() }
-	if ( LocalPlayer():GetViewEntity() == LocalPlayer() && IsValid( LocalPlayer():GetVehicle() ) && !LocalPlayer():GetVehicle():GetThirdPersonMode() ) then table.insert( filter, LocalPlayer():GetVehicle() ) end
+	local ply = LocalPlayer()
+	local filter = ply:GetViewEntity()
+
+	if ( filter == ply ) then
+		local veh = ply:GetVehicle()
+
+		if ( veh:IsValid() && ( !veh:IsVehicle() || !veh:GetThirdPersonMode() ) ) then
+			filter = { filter, veh }
+		end
+	end
 
 	local trace = util.TraceLine( {
 		start = eyepos,
