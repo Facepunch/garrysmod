6a97e42c78b9e4470ed272b133465f8aff7b61a4
Rubat <robotboy655@gmail.com>
2019-08-26 20:18:11 +0300

    Flechette gun npc usage example

--- garrysmod/lua/weapons/weapon_flechettegun.lua
+++ garrysmod/lua/weapons/weapon_flechettegun.lua
@@ -85,3 +85,31 @@ function SWEP:ShouldDropOnDie()
 	return false
 
 end
+
+function SWEP:GetNPCRestTimes()
+
+	-- Handles the time between bursts
+	-- Min rest time in seconds, max rest time in seconds
+
+	return 0.3, 0.6
+
+end
+
+function SWEP:GetNPCBurstSettings()
+
+	-- Handles the burst settings
+	-- Minimum amount of shots, maximum amount of shots, and the delay between each shot
+	-- The amount of shots can end up lower than specificed
+
+	return 1, 6, 0.1
+
+end
+
+function SWEP:GetNPCBulletSpread( proficiency )
+
+	-- Handles the bullet spread based on the given proficiency
+	-- return value is in degrees
+
+	return 1
+
+end
