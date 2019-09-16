d760761e5517b4907fb37f3bcbaf8806e4d3a7ab
Rubat <robotboy655@gmail.com>
2019-09-16 19:52:25 +0300

    DListView.SetFixedSize freeze fix

--- garrysmod/lua/vgui/dlistview_column.lua
+++ garrysmod/lua/vgui/dlistview_column.lua
@@ -132,7 +132,7 @@ end
 
 function PANEL:SetWidth( iSize )
 
-	iSize = math.Clamp( iSize, self:GetMinWidth(), self:GetMaxWidth() )
+	iSize = math.Clamp( iSize, self:GetMinWidth(), math.max( self:GetMaxWidth(), 0 ) )
 
 	-- If the column changes size we need to lay the data out too
 	if ( math.floor( iSize ) != self:GetWide() ) then
