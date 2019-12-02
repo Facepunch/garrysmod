db37b738d9cbfab4181b969a9a386820b2ec44d7
Deyan Dobromirov <dvd_video@abv.bg>
2019-12-02 10:46:05 +0200

    Optimized: Date-Time conversion and processing

--- garrysmod/lua/includes/extensions/util.lua
+++ garrysmod/lua/includes/extensions/util.lua
@@ -90,14 +90,28 @@ function util.LocalToWorld( ent, lpos, bone )
 	return nil
 end
 
+--[[---------------------------------------------------------
+	Returns year, month and day as a formatted string.
+	You can also provide an argument to be conveted.
+-----------------------------------------------------------]]
+function util.Date(vD)
+	return os.date("%Y-%m-%d", vD)
+end
 
 --[[---------------------------------------------------------
-   Returns year, month, day and hour, minute, second in a formatted string.
+	Returns hour, minute, seconds as a formatted string.
+	You can also provide an argument to be conveted.
 -----------------------------------------------------------]]
-function util.DateStamp()
+function util.Time(vT)
+	return os.date("%H:%M:%S", vT)
+end
 
-	local t = os.date('*t')
-	return t.year.."-"..t.month.."-"..t.day .." ".. Format( "%02i-%02i-%02i", t.hour, t.min, t.sec )
+--[[---------------------------------------------------------
+	Returns year, month, day, hour, minute and seconds as a formatted string.
+	You can also provide an argument to be conveted.
+-----------------------------------------------------------]]
+function util.DateStamp(vS)
+	return os.date("%Y-%m-%d %H:%M:%S", vS)
 end
 
 --[[---------------------------------------------------------
