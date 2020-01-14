18dbfabeaf882bb01288e913ecd139a3b6474e8b
Harry Pollard <harry@meharryp.xyz>
2020-01-14 19:31:18 +0000

    TTT: Remove unused concommand (#1629)

--- garrysmod/gamemodes/terrortown/gamemode/weaponry.lua
+++ garrysmod/gamemodes/terrortown/gamemode/weaponry.lua
@@ -175,24 +175,6 @@ function GM:UpdatePlayerLoadouts()
    end
 end
 
----- Weapon switching
-local function ForceWeaponSwitch(ply, cmd, args)
-   if not ply:IsPlayer() or not args[1] then return end
-   -- Turns out even SelectWeapon refuses to switch to empty guns, gah.
-   -- Worked around it by giving every weapon a single Clip2 round.
-   -- Works because no weapon uses those.
-   local wepname = args[1]
-   local wep = ply:GetWeapon(wepname)
-   if IsValid(wep) then
-      -- Weapons apparently not guaranteed to have this
-      if wep.SetClip2 then
-         wep:SetClip2(1)
-      end
-      ply:SelectWeapon(wepname)
-   end
-end
-concommand.Add("wepswitch", ForceWeaponSwitch)
-
 ---- Weapon dropping
 
 function WEPS.DropNotifiedWeapon(ply, wep, death_drop)
