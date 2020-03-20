1acc77e149fe2fcea4bb63ab4d3f55fd0adcf93c
Rubat <robotboy655@gmail.com>
2020-03-20 12:52:39 +0200

    Made sbox_godmode work in singleplayer

--- garrysmod/gamemodes/sandbox/gamemode/init.lua
+++ garrysmod/gamemodes/sandbox/gamemode/init.lua
@@ -83,14 +83,11 @@ end
 -----------------------------------------------------------]]
 function GM:PlayerShouldTakeDamage( ply, attacker )
 
-	-- The player should always take damage in single player..
-	if ( game.SinglePlayer() ) then return true end
-
 	-- Global godmode, players can't be damaged in any way
 	if ( cvars.Bool( "sbox_godmode", false ) ) then return false end
 
 	-- No player vs player damage
-	if ( attacker:IsValid() && attacker:IsPlayer() ) then
+	if ( attacker:IsValid() && attacker:IsPlayer() && ply != attacker ) then
 		return cvars.Bool( "sbox_playershurtplayers", true )
 	end
 
