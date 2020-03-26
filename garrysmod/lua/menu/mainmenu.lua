1609c95817ccc0def1ddb64be062013f3a6bd914
Rubat <robotboy655@gmail.com>
2020-03-26 17:09:01 +0200

    Main menu fixes/changes
    Main menu no longer errors if we failed to load API manifest or its fields changed
    Blacklisted servers display why they were blacklisted in console

--- garrysmod/lua/menu/mainmenu.lua
+++ garrysmod/lua/menu/mainmenu.lua
@@ -224,10 +224,10 @@ GetAPIManifest( function( result )
 	result = util.JSONToTable( result )
 	if ( !result ) then return end
 
-	NewsList = result.News.Blogs or {}
+	NewsList = result.News and result.News.Blogs or {}
 	LoadNewsList()
 
-	for k, v in pairs( result.Servers.Banned or {} ) do
+	for k, v in pairs( result.Servers and result.Servers.Banned or {} ) do
 		if ( v:StartWith( "map:" ) ) then
 			table.insert( BlackList.Maps, v:sub( 5 ) )
 		elseif ( v:StartWith( "desc:" ) ) then
@@ -254,35 +254,35 @@ local function IsServerBlacklisted( address, hostname, description, gm, map )
 
 	for k, v in ipairs( BlackList.Addresses ) do
 		if ( address == v || addressNoPort == v ) then
-			return true
+			return v
 		end
 	end
 
 	for k, v in ipairs( BlackList.Hostnames ) do
 		if string.match( hostname, v ) then
-			return true
+			return v
 		end
 	end
 
 	for k, v in ipairs( BlackList.Descripts ) do
 		if string.match( description, v ) then
-			return true
+			return v
 		end
 	end
 
 	for k, v in ipairs( BlackList.Gamemodes ) do
 		if string.match( gm, v ) then
-			return true
+			return v
 		end
 	end
 
 	for k, v in ipairs( BlackList.Maps ) do
 		if string.match( map, v ) then
-			return true
+			return v
 		end
 	end
 
-	return false
+	return nil
 end
 
 local Servers = {}
@@ -302,7 +302,8 @@ function GetServers( category, id )
 			if Servers[ category ] && Servers[ category ][ address ] then print( "Server Browser Error!", address, category ) return end
 			Servers[ category ][ address ] = true
 
-			if ( !IsServerBlacklisted( address, name, desc, gm, map ) ) then
+			local blackListMatch = IsServerBlacklisted( address, name, desc, gm, map )
+			if ( blackListMatch == nil ) then
 
 				name = string.JavascriptSafe( name )
 				desc = string.JavascriptSafe( desc )
@@ -313,9 +314,10 @@ function GetServers( category, id )
 
 				pnlMainMenu:Call( string.format( 'AddServer( "%s", "%s", %i, "%s", "%s", "%s", %i, %i, %i, %s, %i, "%s", "%s", "%s" );',
 					category, id, ping, name, desc, map, players, maxplayers, botplayers, tostring( pass ), lastplayed, address, gm, workshopid ) )
+
 			else
 
-				Msg( "Ignoring blacklisted server: ", name, " @ ", address, "\n" )
+				Msg( "Ignoring server '", name, "' @ ", address, " - ", blackListMatch, " is blacklisted\n" )
 
 			end
 
