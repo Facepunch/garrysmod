8deb0926b28e3fcd96f1777bab669d03fa43cf42
Daniel Hultgren <daniel.cf.hultgren@gmail.com>
2020-05-05 18:46:36 +0200

    Fixed utf8 error
    3rd argument to sub is now optional like regular string.sub

--- garrysmod/lua/includes/modules/utf8.lua
+++ garrysmod/lua/includes/modules/utf8.lua
@@ -343,7 +343,7 @@ end
 --
 local function strRelToAbsChar( str, pos )
 	if pos < 0 then
-		pos = math.max( pos + utf8.len( str ) + 1, 0 )
+		pos = math.max( pos + len( str ) + 1, 0 )
 	end
 	return pos
 end
@@ -355,10 +355,10 @@ function idx( str, idx )
 	idx = strRelToAbsChar( str, idx )
 
 	if idx == 0 then return "" end
-	if idx > utf8.len( str ) then return "" end
+	if idx > len( str ) then return "" end
 
-	local offset = utf8.offset( str, idx - 1 )
-	return utf8.char( utf8.codepoint( str, offset ) )
+	local off = offset( str, idx - 1 )
+	return char( codepoint( str, off ) )
 end
 
 --
@@ -366,11 +366,11 @@ end
 --
 function sub( str, charstart, charend )
 	charstart = strRelToAbsChar( str, charstart )
-	charend = strRelToAbsChar( str, charend )
+	charend = strRelToAbsChar( str, charend or -1 )
 
 	local buf = {}
 	for i = charstart, charend do
-		buf[#buf + 1] = utf8.idx( str, i )
+		buf[#buf + 1] = idx( str, i )
 	end
 
 	return table.concat( buf )
