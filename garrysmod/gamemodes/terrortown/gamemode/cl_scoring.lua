a1ff6ec8f90dad6051aebf56d86a05bdccd3427f
Niilyx <43385037+Niilyx@users.noreply.github.com>
2020-05-13 21:51:00 +0200

    Allows for a wider score panel (French only)

--- garrysmod/gamemodes/terrortown/gamemode/cl_scoring.lua
+++ garrysmod/gamemodes/terrortown/gamemode/cl_scoring.lua
@@ -353,8 +353,14 @@ function CLSCORE:ShowPanel()
    local margin = 15
 
    local dpanel = vgui.Create("DFrame")
-   local w, h = 700, 500
-   dpanel:SetSize(700, 500)
+   if LANG.ActiveLanguage == "français" then
+	tempw, temph = 788, 500
+	dpanel:SetSize(788, 500)
+   else
+    tempw, temph = 700, 500
+	dpanel:SetSize(700, 500)
+   end
+   local w, h = tempw, temph
    dpanel:Center()
    dpanel:SetTitle(T("report_title"))
    dpanel:SetVisible(true)
