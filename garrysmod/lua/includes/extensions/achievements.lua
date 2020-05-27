9c689aa862088937382d50f41595b076212c6f76
Spar <5685050+GitSparTV@users.noreply.github.com>
2020-05-27 22:14:49 +0300

    Create achievements.lua

---
+++ garrysmod/lua/includes/extensions/achievements.lua
@@ -0,0 +1,34 @@
+if SERVER then
+	util.AddNetworkString("achievements")
+end
+
+function achievements.Call(ply, aID, removerCount)
+	if CLIENT then return end
+	net.Start("achievements")
+	net.WriteUInt(aID, 3)
+
+	if removerCount then
+		net.WriteUInt(removerCount, 16)
+	end
+
+	net.Send(ply)
+end
+
+if CLIENT then
+	local idToAchievement = {
+		[0] = achievements.BalloonPopped,
+achievements.EatBall, achievements.Remover, achievements.SpawnedNPC, achievements.SpawnedProp, achievements.SpawnedRagdoll
+	}
+
+	net.Receive("achievements", function()
+		local aID = net.ReadUInt(3)
+		if aID == 2 then
+			local func = idToAchievement[aID]
+			for k=1, net.ReadUInt(16) do
+				func()
+			end
+		else
+			idToAchievement[aID]()
+		end
+	end)
+end
