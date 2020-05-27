85c5e17d0a6b702e988eb7c6c3ecf9f0299b2669
Spar <5685050+GitSparTV@users.noreply.github.com>
2020-05-28 00:31:50 +0300

    Rethinking the idea

--- garrysmod/lua/includes/extensions/achievements.lua
+++ garrysmod/lua/includes/extensions/achievements.lua
@@ -1,40 +1,53 @@
+local bitsize = 0
+local achievementsN = 0
+local achievementsLookup = {}
+
 if SERVER then
 	util.AddNetworkString("achievements")
 	achievements = {}
+
+	function achievements.Call(ply, aName, data)
+		local aID = achievementsLookup[aName]
+		if not aID then error("Attempt to call unregistered achievement (" .. tostring(aName) .. ")") end
+		net.Start("achievements")
+		net.WriteUInt(aID, bitsize)
+
+		if data then
+			net.WriteUInt(data, 16)
+		end
+
+		net.Send(ply)
+	end
 end
 
-function achievements.Call(ply, aID, removerCount)
-	if (CLIENT) then return end
-	net.WriteUInt(aID, 3)
+function achievements.Register(name,func)
+	if CLIENT and not isfunction(func) then error("Second argument must be a function") end
 
-	if (aID == 2) then
-		net.WriteUInt(removerCount, 16)
+	if (SERVER) then
+		achievementsLookup[name] = achievementsN
+	else
+		achievementsLookup[achievementsN] = func
 	end
 
-	net.Send(ply)
+	achievementsN = achievementsN + 1
+	bitsize = math.ceil(math.log(achievementsN) / math.log(2)) + 1
 end
 
 if CLIENT then
-	local idToAchievement = {
-		[0] = achievements.BalloonPopped,
-		achievements.EatBall,
-		achievements.Remover,
-		achievements.SpawnedNPC,
-		achievements.SpawnedProp,
-		achievements.SpawnedRagdoll
-	}
-
 	net.Receive("achievements", function()
-		local aID = net.ReadUInt(3)
+		local aID = net.ReadUInt(bitsize)
+		local func = achievementsLookup[aID]
+		print(aID,func,bitsize)
 
-		if (aID == 2) then
-			local func = idToAchievement[aID]
-
-			for k = 1, net.ReadUInt(16) do
-				func()
-			end
-		else
-			idToAchievement[aID]()
+		if func then
+			func(net.ReadUInt(16))
 		end
 	end)
 end
+
+achievements.Register("BalloonPopped",achievements.BalloonPopped)
+achievements.Register("EatBall",achievements.EatBall)
+achievements.Register("Remover",function(count) for i = 1, count do achievements.Remover() end end)
+achievements.Register("SpawnedNPC",achievements.SpawnedNPC)
+achievements.Register("SpawnedProp",achievements.SpawnedProp)
+achievements.Register("SpawnedRagdoll",achievements.SpawnedRagdoll)
