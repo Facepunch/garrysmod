642b9ad6d0b649fc02ec5912fc3b29c054cbc61a
connorh315 <37246828+connorh315@users.noreply.github.com>
2020-06-16 18:04:20 +0100

    Adds draw.Circle
    Adds the function draw.Circle to draw a filled circle

--- garrysmod/lua/includes/modules/draw.lua
+++ garrysmod/lua/includes/modules/draw.lua
@@ -299,6 +299,25 @@ function TexturedQuad( tab )
 
 end
 
+--[[---------------------------------------------------------
+	Name: Circle( x, y, radius, segments )
+	Desc: pawrapper
+-----------------------------------------------------------]]
+function Circle( x, y, radius, seg )
+	local cir = {}
+
+	table.insert( cir, { x = x, y = y, u = 0.5, v = 0.5 } )
+	for i = 0, seg do
+		local a = math.rad( ( i / seg ) * -360 )
+		table.insert( cir, { x = x + math.sin( a ) * radius, y = y + math.cos( a ) * radius, u = math.sin( a ) / 2 + 0.5, v = math.cos( a ) / 2 + 0.5 } )
+	end
+
+	local a = 0
+	table.insert( cir, { x = x + math.sin( a ) * radius, y = y + math.cos( a ) * radius, u = math.sin( a ) / 2 + 0.5, v = math.cos( a ) / 2 + 0.5 } )
+
+	surface.DrawPoly( cir )
+end
+
 function NoTexture()
 	surface.SetTexture( tex_white )
 end
