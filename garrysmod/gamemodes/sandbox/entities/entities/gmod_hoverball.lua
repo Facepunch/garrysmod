Facepunch/garrysmod

commit b9839ed24e7952fd6af1049cedd887f01c6358b1

Color( ... ) to color where possible and similar things
Hopefully nothing broke in the process

garrysmod/gamemodes/sandbox/entities/entities/gmod_hoverball.lua
@@ -133,8 +133,8 @@ function ENT:PhysicsSimulate( phys, deltatime )
 
 	Exponent = math.Clamp( Exponent, -5000, 5000 )
 
-	local Linear = Vector( 0, 0, 0 )
-	local Angular = Vector( 0, 0, 0 )
+	local Linear = vector_origin
+	local Angular = vector_origin
 
 	Linear.z = Exponent
 

garrysmod/gamemodes/base/entities/effects/dof_node.lua
@@ -50,6 +50,6 @@ function EFFECT:Render()
 	local SpriteSize = ( self.spacing + self.offset ) * 8
 
 	render.SetMaterial( self.Mat )
-	render.DrawSprite( self:GetPos(), SpriteSize, SpriteSize, Color( 255, 255, 255 ) )
+	render.DrawSprite( self:GetPos(), SpriteSize, SpriteSize, color_white )
 
 end

garrysmod/gamemodes/base/entities/effects/tooltracer.lua
@@ -45,7 +45,7 @@ function EFFECT:Render()
 					8,								-- Width
 					texcoord,						-- Start tex coord
 					texcoord + self.Length / 128,	-- End tex coord
-					Color( 255, 255, 255 ) )		-- Color (optional)
+					color_white )		-- Color (optional)
 	end
 
 	render.DrawBeam( self.StartPos,

garrysmod/gamemodes/base/entities/entities/gmod_hands.lua
@@ -61,7 +61,7 @@ function ENT:AttachToViewmodel( vm )
 	self:SetParent( vm )
 	self:SetMoveType( MOVETYPE_NONE )
 
-	self:SetPos( Vector( 0, 0, 0 ) )
-	self:SetAngles( Angle( 0, 0, 0 ) )
+	self:SetPos( vector_origin )
+	self:SetAngles( angle_zero )
 
 end

garrysmod/gamemodes/base/entities/entities/prop_effect.lua
@@ -129,7 +129,7 @@ function ENT:PhysicsUpdate( physobj )
 	-- Don't do anything if the player isn't holding us
 	if ( !self:IsPlayerHolding() && !self:IsConstrained() ) then
 
-		physobj:SetVelocity( Vector( 0, 0, 0 ) )
+		physobj:SetVelocity( vector_origin )
 		physobj:Sleep()
 
 	end

garrysmod/gamemodes/base/entities/entities/ragdoll_motion.lua
@@ -84,7 +84,7 @@ function ENT:PhysicsUpdate( physobj )
 	if ( self:IsPlayerHolding() ) then return end
 	if ( self:IsConstrained() ) then return end
 
-	physobj:SetVelocity( Vector( 0, 0, 0 ) )
+	physobj:SetVelocity( vector_origin )
 	physobj:Sleep()
 
 end
@@ -131,7 +131,7 @@ function ENT:DrawDebug( ragdoll, controller, pos, ang, rotation, scale, center, 
 
 	center = center
 
-	local col_bone = Color( 255, 255, 255, 255 )
+	local col_bone = color_white
 	local col_point = Color( 255, 0, 0, 255 )
 	local col_tran_bn = Color( 0, 255, 0, 255 )
 

garrysmod/gamemodes/base/gamemode/cl_init.lua
@@ -141,7 +141,7 @@ function GM:OnPlayerChat( player, strText, bTeamOnly, bPlayerIsDead )
 	--
 	-- I've made this all look more complicated than it is. Here's the easy version
 	--
-	-- chat.AddText( player, Color( 255, 255, 255 ), ": ", strText )
+	-- chat.AddText( player, color_white, ": ", strText )
 	--
 
 	local tab = {}
@@ -162,7 +162,7 @@ function GM:OnPlayerChat( player, strText, bTeamOnly, bPlayerIsDead )
 		table.insert( tab, "Console" )
 	end
 
-	table.insert( tab, Color( 255, 255, 255 ) )
+	table.insert( tab, color_white )
 	table.insert( tab, ": " .. strText )
 
 	chat.AddText( unpack(tab) )

garrysmod/gamemodes/base/gamemode/cl_scoreboard.lua
@@ -188,7 +188,7 @@ local SCORE_BOARD = {
 
 		self.Name = self.Header:Add( "DLabel" )
 		self.Name:SetFont( "ScoreboardDefaultTitle" )
-		self.Name:SetTextColor( Color( 255, 255, 255, 255 ) )
+		self.Name:SetTextColor( color_white )
 		self.Name:Dock( TOP )
 		self.Name:SetHeight( 40 )
 		self.Name:SetContentAlignment( 5 )
@@ -196,7 +196,7 @@ local SCORE_BOARD = {
 
 		--self.NumPlayers = self.Header:Add( "DLabel" )
 		--self.NumPlayers:SetFont( "ScoreboardDefault" )
-		--self.NumPlayers:SetTextColor( Color( 255, 255, 255, 255 ) )
+		--self.NumPlayers:SetTextColor( color_white )
 		--self.NumPlayers:SetPos( 0, 100 - 30 )
 		--self.NumPlayers:SetSize( 300, 30 )
 		--self.NumPlayers:SetContentAlignment( 4 )

commit 5d5bbabcb67e7a0b0cf516e6a4f7ab3c0e8db7e1

Fix mass vector_origin replacement bug
Caused by https://github.com/Facepunch/garrysmod/commit/b9839ed24e7952fd6af1049cedd887f01c6358b1#diff-5dec0ab92ef177ebb4780d5ee7fd4cddb6b4701902f4496738d4c7b51cd4d69cR136

garrysmod/gamemodes/sandbox/entities/entities/gmod_hoverball.lua
@@ -106,6 +106,8 @@ function ENT:DrawTranslucent( flags )
 	BaseClass.DrawTranslucent( self, flags )
 end
 
+local Linear = Vector(0,0,0)
+
 function ENT:PhysicsSimulate( phys, deltatime )
 
 	if ( !self:GetEnabled() ) then return end
@@ -143,9 +145,9 @@ function ENT:PhysicsSimulate( phys, deltatime )
 
 	Exponent = math.Clamp( Exponent, -5000, 5000 )
 
-	local Linear = vector_origin
 	local Angular = vector_origin
 
+	Linear:Zero()
 	Linear.z = Exponent
 
 	if ( AirResistance > 0 ) then