b733f6feb8fbab748be82eb89b9bc953cf9206c6
Deyan Dobromirov <dvdvideo123@gmail.com>
2021-01-13 14:23:51 +0200

    Utilized: Closing the context menu in couple of cases

--- garrysmod/lua/vgui/dcombobox.lua
+++ garrysmod/lua/vgui/dcombobox.lua
@@ -1,13 +1,3 @@
---[[   _                                
-    ( )                               
-   _| |   __   _ __   ___ ___     _ _ 
- /'_` | /'__`\( '__)/' _ ` _ `\ /'_` )
-( (_| |(  ___/| |   | ( ) ( ) |( (_| |
-`\__,_)`\____)(_)   (_) (_) (_)`\__,_) 
-
-	DComboBox
-
---]]
 
 local PANEL = {}
 
@@ -15,9 +5,8 @@ Derma_Hook( PANEL, "Paint", "Paint", "ComboBox" )
 
 Derma_Install_Convar_Functions( PANEL )
 
---[[---------------------------------------------------------
-   Name: Init
------------------------------------------------------------]]
+AccessorFunc( PANEL, "m_bDoSort", "SortItems", FORCE_BOOL )
+
 function PANEL:Init()
 
 	self.DropButton = vgui.Create( "DPanel", self )
@@ -31,74 +20,86 @@ function PANEL:Init()
 	self:SetContentAlignment( 4 )
 	self:SetTextInset( 8, 0 )
 	self:SetIsMenu( true )
+	self:SetSortItems( true )
 
 end
 
---[[---------------------------------------------------------
-   Name: Clear
------------------------------------------------------------]]
-function PANEL:Clear()
-
-	self:SetText( "" )
-	self.Choices = {}
-	self.Data = {}
+function PANEL:CloseMenu()
 
 	if ( self.Menu ) then
 		self.Menu:Remove()
 		self.Menu = nil
 	end
-	
+
 end
 
---[[---------------------------------------------------------
-   Name: GetOptionText
------------------------------------------------------------]]
+function PANEL:Clear()
+
+	self:SetText( "" )
+	self.Choices = {}
+	self.Data = {}
+	self.ChoiceIcons = {}
+	self.selected = nil
+	self:CloseMenu()
+
+end
+
 function PANEL:GetOptionText( id )
 
 	return self.Choices[ id ]
 
 end
 
---[[---------------------------------------------------------
-   Name: GetOptionData
------------------------------------------------------------]]
 function PANEL:GetOptionData( id )
 
 	return self.Data[ id ]
 
 end
 
---[[---------------------------------------------------------
-   Name: PerformLayout
------------------------------------------------------------]]
+function PANEL:GetOptionTextByData( data )
+
+	for id, dat in pairs( self.Data ) do
+		if ( dat == data ) then
+			return self:GetOptionText( id )
+		end
+	end
+
+	-- Try interpreting it as a number
+	for id, dat in pairs( self.Data ) do
+		if ( dat == tonumber( data ) ) then
+			return self:GetOptionText( id )
+		end
+	end
+
+	-- In case we fail
+	return data
+
+end
+
 function PANEL:PerformLayout()
 
 	self.DropButton:SetSize( 15, 15 )
 	self.DropButton:AlignRight( 4 )
 	self.DropButton:CenterVertical()
 
+	-- Make sure the text color is updated
+	DButton.PerformLayout( self, w, h )
+
 end
 
---[[---------------------------------------------------------
-   Name: ChooseOption
------------------------------------------------------------]]
 function PANEL:ChooseOption( value, index )
 
-	if ( self.Menu ) then
-		self.Menu:Remove()
-		self.Menu = nil
-	end
-
+	self:CloseMenu()
 	self:SetText( value )
-	
+
+	-- This should really be the here, but it is too late now and convar changes are handled differently by different child elements
+	--self:ConVarChanged( self.Data[ index ] )
+
 	self.selected = index
-	self:OnSelect( index, value, self.Data[index] )
-	
+	self:OnSelect( index, value, self.Data[ index ] )
+
 end
 
---[[---------------------------------------------------------
-   Name: ChooseOptionID
------------------------------------------------------------]]
 function PANEL:ChooseOptionID( index )
 
 	local value = self:GetOptionText( index )
@@ -106,42 +107,27 @@ function PANEL:ChooseOptionID( index )
 
 end
 
---[[---------------------------------------------------------
-   Name: GetSelected
------------------------------------------------------------]]
 function PANEL:GetSelectedID()
 
 	return self.selected
 
 end
 
-
-
---[[---------------------------------------------------------
-   Name: GetSelected
------------------------------------------------------------]]
 function PANEL:GetSelected()
-	
+
 	if ( !self.selected ) then return end
-	
-	return self:GetOptionText(self.selected), self:GetOptionData(self.selected)
-	
+
+	return self:GetOptionText( self.selected ), self:GetOptionData( self.selected )
+
 end
 
-
---[[---------------------------------------------------------
-   Name: OnSelect
------------------------------------------------------------]]
 function PANEL:OnSelect( index, value, data )
 
 	-- For override
 
 end
 
---[[---------------------------------------------------------
-   Name: AddChoice
------------------------------------------------------------]]
-function PANEL:AddChoice( value, data, select )
+function PANEL:AddChoice( value, data, select, icon )
 
 	local i = table.insert( self.Choices, value )
 
@@ -149,12 +135,16 @@ function PANEL:AddChoice( value, data, select )
 		self.Data[ i ] = data
 	end
 
+	if ( icon ) then
+		self.ChoiceIcons[ i ] = icon
+	end
+
 	if ( select ) then
 
 		self:ChooseOption( value, i )
 
 	end
-	
+
 	return i
 
 end
@@ -165,53 +155,67 @@ function PANEL:IsMenuOpen()
 
 end
 
---[[---------------------------------------------------------
-   Name: OpenMenu
------------------------------------------------------------]]
 function PANEL:OpenMenu( pControlOpener )
 
-	if ( pControlOpener ) then
-		if ( pControlOpener == self.TextEntry ) then
-			return
-		end
+	if ( pControlOpener && pControlOpener == self.TextEntry ) then
+		return
 	end
 
 	-- Don't do anything if there aren't any options..
 	if ( #self.Choices == 0 ) then return end
-	
+
 	-- If the menu still exists and hasn't been deleted
 	-- then just close it and don't open a new one.
-	if ( IsValid( self.Menu ) ) then
-		self.Menu:Remove()
-		self.Menu = nil
+	self:CloseMenu()
+
+	self.Menu = DermaMenu( false, self )
+
+	if ( self:GetSortItems() ) then
+		local sorted = {}
+		for k, v in pairs( self.Choices ) do
+			local val = tostring( v ) --tonumber( v ) || v -- This would make nicer number sorting, but SortedPairsByMemberValue doesn't seem to like number-string mixing
+			if ( string.len( val ) > 1 && !tonumber( val ) && val:StartWith( "#" ) ) then val = language.GetPhrase( val:sub( 2 ) ) end
+			table.insert( sorted, { id = k, data = v, label = val } )
+		end
+		for k, v in SortedPairsByMemberValue( sorted, "label" ) do
+			local option = self.Menu:AddOption( v.data, function() self:ChooseOption( v.data, v.id ) end )
+			if ( self.ChoiceIcons[ v.id ] ) then
+				option:SetIcon( self.ChoiceIcons[ v.id ] )
+			end
+		end
+	else
+		for k, v in pairs( self.Choices ) do
+			local option = self.Menu:AddOption( v, function() self:ChooseOption( v, k ) end )
+			if ( self.ChoiceIcons[ k ] ) then
+				option:SetIcon( self.ChoiceIcons[ k ] )
+			end
+		end
 	end
 
-	self.Menu = DermaMenu()
-	
-	local sorted = {}
-	for k, v in pairs( self.Choices ) do table.insert( sorted, { id = k, data = v } ) end
-	for k, v in SortedPairsByMemberValue( sorted, "data" ) do
-		self.Menu:AddOption( v.data, function() self:ChooseOption( v.data, v.id ) end )
-	end
-	
 	local x, y = self:LocalToScreen( 0, self:GetTall() )
-	
+
 	self.Menu:SetMinimumWidth( self:GetWide() )
 	self.Menu:Open( x, y, false, self )
 
 end
 
-function PANEL:CloseMenu()
+-- This really should use a convar change hook
+function PANEL:CheckConVarChanges()
 
-	if ( IsValid( self.Menu ) ) then
-		self.Menu:Remove()
-	end
-	
+	if ( !self.m_strConVar ) then return end
+
+	local strValue = GetConVarString( self.m_strConVar )
+	if ( self.m_strConVarValue == strValue ) then return end
+
+	self.m_strConVarValue = strValue
+
+	self:SetValue( self:GetOptionTextByData( self.m_strConVarValue ) )
+
 end
 
 function PANEL:Think()
 
-	self:ConVarNumberThink()
+	self:CheckConVarChanges()
 
 end
 
@@ -226,19 +230,18 @@ function PANEL:DoClick()
 	if ( self:IsMenuOpen() ) then
 		return self:CloseMenu()
 	end
-	
+
 	self:OpenMenu()
 
 end
 
---[[---------------------------------------------------------
-   Name: GenerateExample
------------------------------------------------------------]]
 function PANEL:GenerateExample( ClassName, PropertySheet, Width, Height )
 
 	local ctrl = vgui.Create( ClassName )
 	ctrl:AddChoice( "Some Choice" )
-	ctrl:AddChoice( "Another Choice" )
+	ctrl:AddChoice( "Another Choice", "myData" )
+	ctrl:AddChoice( "Default Choice", "myData2", true )
+	ctrl:AddChoice( "Icon Choice", "myData3", false, "icon16/star.png" )
 	ctrl:SetWide( 150 )
 
 	PropertySheet:AddSheet( ClassName, ctrl, nil, true, true )
