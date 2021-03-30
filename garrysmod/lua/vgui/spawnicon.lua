44062a88f7e2539c9e058f612810d3228093caf1
Rubat <robotboy655@gmail.com>
2021-03-30 05:50:53 +0300

    Fixed an issue with resizing drag'n'dropped spawnicons
    It was trying to relayout the container the icon was initially created on, no the container the icon is currently in

--- garrysmod/lua/vgui/spawnicon.lua
+++ garrysmod/lua/vgui/spawnicon.lua
@@ -254,6 +254,9 @@ spawnmenu.AddContentType( "model", function( container, obj )
 	icon.DoClick = function( s ) surface.PlaySound( "ui/buttonclickrelease.wav") RunConsoleCommand( "gm_spawn", s:GetModelName(), s:GetSkinID() or 0, s:GetBodyGroup() or "" ) end
 	icon.OpenMenu = function( icon )
 
+		-- Use the containter that we are dragged onto, not the one we were created on
+		local container = icon:GetParent()
+
 		local menu = DermaMenu()
 		menu:AddOption( "#spawnmenu.menu.copy", function() SetClipboardText( string.gsub( obj.model, "\\", "/" ) ) end ):SetIcon( "icon16/page_copy.png" )
 		menu:AddOption( "#spawnmenu.menu.spawn_with_toolgun", function() RunConsoleCommand( "gmod_tool", "creator" ) RunConsoleCommand( "creator_type", "4" ) RunConsoleCommand( "creator_name", obj.model ) end ):SetIcon( "icon16/brick_add.png" )
@@ -298,7 +301,7 @@ spawnmenu.AddContentType( "model", function( container, obj )
 		container:Add( icon )
 	end
 
-/*
+--[[
 	if ( iSkin != 0 ) then return end
 
 	local iSkinCount = NumModelSkins( strModel )
@@ -309,7 +312,7 @@ spawnmenu.AddContentType( "model", function( container, obj )
 		self:AddModel( strModel, i )
 
 	end
-*/
+]]
 
 	return icon
 
