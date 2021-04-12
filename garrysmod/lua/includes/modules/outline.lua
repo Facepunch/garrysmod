9837354b3e578ccacd2ecc884d81c4c86b44ff7a
Shadow Bonnie (RUS) <49488893+ShadowBonnieRUS@users.noreply.github.com>
2021-04-12 21:34:41 +0300

    Many updates
    Visual file clean up.
    Added ability to change outline render type: before view model; before effects; after effects. Enums OUTLINE_RENDERTYPE_*.
    Added ability to change thickness to doubled version.
    Now using depth buffer instead IsLineOfSightClear.

--- garrysmod/lua/includes/modules/outline.lua
+++ garrysmod/lua/includes/modules/outline.lua
@@ -1,128 +1,227 @@
-
-OUTLINE_MODE_BOTH	= 0
+OUTLINE_MODE_BOTH		= 0		-- Render always
 OUTLINE_MODE_NOTVISIBLE	= 1
 OUTLINE_MODE_VISIBLE	= 2
 
-local istable		= istable
-local render		= render
-local LocalPlayer	= LocalPlayer
-local Material		= Material
+OUTLINE_RENDERTYPE_BEFORE_VM	= 0		-- Render before drawing the view model
+OUTLINE_RENDERTYPE_BEFORE_EF	= 1		-- Render before drawing all effects (after drawing the viewmodel)
+OUTLINE_RENDERTYPE_AFTER_EF		= 2		-- Render after drawing all effects
+
+local istable			= istable
+local render			= render
+local Material			= Material
 local CreateMaterial	= CreateMaterial
-local hook		= hook
-local cam		= cam
-local ScrW		= ScrW
-local ScrH		= ScrH
-local IsValid		= IsValid
-local surface		= surface
+local hook				= hook
+local cam				= cam
+local ScrW				= ScrW
+local ScrH				= ScrH
+local IsValid			= IsValid
+local surface			= surface
 
 module( "outline", package.seeall )
 
-local List, ListSize = {}, 0
-local RenderEnt = NULL
+local List, ListSize		= {}, 0
+local RenderEnt				= NULL
+local RenderType			= OUTLINE_RENDERTYPE_AFTER_EF
+local OutlineThickness		= 1
 
-local OutlineMatSettings = {
-	[ "$ignorez" ] = 1,
-	[ "$alphatest" ] = 1
+local StoreTexture			= render.GetScreenEffectTexture( 0 )
+local DrawTexture			= render.GetScreenEffectTexture( 1 )
+
+local OutlineMatSettings	= {
+	[ "$basetexture" ]	= DrawTexture:GetName(),
+	[ "$ignorez" ]		= 1,
+	[ "$alphatest" ]	= 1
 }
 
-local CopyMat		= Material( "pp/copy" )
-local OutlineMat	= CreateMaterial( "OutlineMat", "UnlitGeneric", OutlineMatSettings )
-local StoreTexture	= render.GetScreenEffectTexture( 0 )
-local DrawTexture	= render.GetScreenEffectTexture( 1 )
+local CopyMat				= Material( "pp/copy" )
+local OutlineMat			= CreateMaterial( "outline", "UnlitGeneric", OutlineMatSettings )
 
-local ENTS	= 1
-local COLOR	= 2
-local MODE	= 3
+local ENTS, COLOR, MODE		= 1, 2, 3
 
 function Add( ents, color, mode )
-
-	if ( ListSize >= 255 ) then return end			--Maximum 255 reference values
-	if ( !istable( ents ) ) then ents = { ents } end	--Support for passing Entity as first argument
-	if ( ents[ 1 ] == nil ) then return end			--Do not pass empty tables
 	
-	local t = {
+	if ( ListSize >= 255 ) then return end				--Maximum 255 reference values
+	if ( !istable( ents ) ) then ents = { ents } end	--Support for passing Entity as first argument
+	if ( ents[ 1 ] == nil ) then return end				--Do not pass empty tables
+	
+	if (
+		mode != OUTLINE_MODE_BOTH &&
+		mode != OUTLINE_MODE_NOTVISIBLE &&
+		mode != OUTLINE_MODE_VISIBLE
+	) then
+		mode = OUTLINE_MODE_BOTH
+	end
+	
+	local data = {
 		[ ENTS ] = ents,
 		[ COLOR ] = color,
-		[ MODE ] = mode or OUTLINE_MODE_BOTH
+		[ MODE ] = mode
 	}
 	
 	ListSize = ListSize + 1
-	List[ ListSize ] = t
+	List[ ListSize ] = data
+	
 end
 
 function RenderedEntity()
-
+	
 	return RenderEnt
 	
 end
 
-local function Render()
-	local ply = LocalPlayer()
-	local IsLineOfSightClear = ply.IsLineOfSightClear
+function SetRenderType( render_type )
+	
+	if (
+		render_type != OUTLINE_RENDERTYPE_BEFORE_VM &&
+		render_type != OUTLINE_RENDERTYPE_BEFORE_EF &&
+		render_type != OUTLINE_RENDERTYPE_AFTER_EF
+	) then
+		return
+	end
 
+	local old_type = RenderType
+	RenderType = render_type
+	
+	return old_type
+	
+end
+
+function GetRenderType()
+	
+	return RenderType
+	
+end
+
+function SetDoubleThickness( thickness )
+
+	local old_thickness = OutlineThickness == 2
+	OutlineThickness = thickness and 2 or 1
+	
+	return old_thickness
+	
+end
+
+function IsDoubleThickness()
+	
+	return OutlineThickness == 2
+	
+end
+
+local function Render()
+	
 	local scene = render.GetRenderTarget()
 	render.CopyRenderTargetToTexture( StoreTexture )
 	
-	local w = ScrW()
-	local h = ScrH()
+	local w, h = ScrW(), ScrH()
 	
-	render.Clear( 0, 0, 0, 0, true, true )
-
+	render.Clear( 0, 0, 0, 0, false, true )
+	
 	render.SetStencilEnable( true )
-		cam.IgnoreZ( true )
 		render.SuppressEngineLighting( true )
-	
+		
 		render.SetStencilWriteMask( 0xFF )
 		render.SetStencilTestMask( 0xFF )
 		
-		render.SetStencilCompareFunction( STENCIL_ALWAYS )
+		render.SetStencilCompareFunction( STENCIL_GREATER )
 		render.SetStencilFailOperation( STENCIL_KEEP )
-		render.SetStencilZFailOperation( STENCIL_REPLACE )
-		render.SetStencilPassOperation( STENCIL_REPLACE )
 		
 		cam.Start3D()
-		
+			render.SetBlend(0)
+			
 			for i = 1, ListSize do
-				local v = List[ i ]
-				local mode = v[ MODE ]
-				local ents = v[ ENTS ]
 				
-				render.SetStencilReferenceValue( i )
-		
-				for j = 1, #ents do
-					local ent = ents[ j ]
+				local reference = 0xFF - ( i - 1 )
+				
+				local data = List[ i ]
+				local mode = data[ MODE ]
+				local ents = data[ ENTS ]
+				
+				render.SetStencilReferenceValue( reference )
+				
+				if ( mode == OUTLINE_MODE_BOTH || mode == OUTLINE_MODE_VISIBLE ) then
 					
-					if ( !IsValid( ent ) ) then continue end
+					render.SetStencilZFailOperation( mode == OUTLINE_MODE_BOTH and STENCIL_REPLACE or STENCIL_KEEP )
+					render.SetStencilPassOperation( STENCIL_REPLACE )
 					
-					if ( ( mode == OUTLINE_MODE_NOTVISIBLE && IsLineOfSightClear( ply, ent ) ) || ( mode == OUTLINE_MODE_VISIBLE && !IsLineOfSightClear( ply, ent ) ) ) then
-						continue
+					for j = 1, #ents do
+						
+						local ent = ents[ j ]
+						
+						if ( IsValid( ent ) ) then
+							
+							RenderEnt = ent
+							ent:DrawModel()
+							
+						end
+						
 					end
 					
-					RenderEnt = ent
+				elseif ( mode == OUTLINE_MODE_NOTVISIBLE ) then
 					
-					ent:DrawModel()
+					render.SetStencilZFailOperation( STENCIL_REPLACE )
+					render.SetStencilPassOperation( STENCIL_KEEP )
+					
+					for j = 1, #ents do
+						
+						local ent = ents[ j ]
+						
+						if ( IsValid( ent ) ) then
+							
+							RenderEnt = ent
+							ent:DrawModel()
+							
+						end
+						
+					end
+					
+					render.SetStencilCompareFunction( STENCIL_EQUAL )
+					render.SetStencilZFailOperation( STENCIL_KEEP )
+					render.SetStencilPassOperation( STENCIL_ZERO )
+					
+					for j = 1, #ents do
+						
+						local ent = ents[ j ]
+						
+						if ( IsValid( ent ) ) then
+							
+							RenderEnt = ent
+							ent:DrawModel()
+							
+						end
+						
+					end
+					
+					render.SetStencilCompareFunction( STENCIL_GREATER )
+					
 				end
+				
 			end
 			
 			RenderEnt = NULL
 			
+			render.SetBlend(1)
 		cam.End3D()
 		
 		render.SetStencilCompareFunction( STENCIL_EQUAL )
+		render.SetStencilZFailOperation( STENCIL_KEEP )
+		render.SetStencilPassOperation( STENCIL_KEEP )
 		
 		cam.Start2D()
-		
+			
 			for i = 1, ListSize do
-				render.SetStencilReferenceValue( i )
+				
+				local reference = 0xFF - ( i - 1 )
+				
+				render.SetStencilReferenceValue( reference )
 				
 				surface.SetDrawColor( List[ i ][ COLOR ] )
 				surface.DrawRect( 0, 0, w, h )
+				
 			end
 			
 		cam.End2D()
 		
 		render.SuppressEngineLighting( false )
-		cam.IgnoreZ( false )
 	render.SetStencilEnable( false )
 	
 	render.CopyRenderTargetToTexture( DrawTexture )
@@ -133,33 +232,66 @@ local function Render()
 	render.DrawScreenQuad()
 	
 	render.SetStencilEnable( true )
+	
 		render.SetStencilReferenceValue( 0 )
+		
 		render.SetStencilCompareFunction( STENCIL_EQUAL )
 		
 		OutlineMat:SetTexture( "$basetexture", DrawTexture )
 		render.SetMaterial( OutlineMat )
 		
-		render.DrawScreenQuadEx( -1, -1, w ,h )
-		render.DrawScreenQuadEx( -1, 0, w, h )
-		render.DrawScreenQuadEx( -1, 1, w, h )
-		render.DrawScreenQuadEx( 0, -1, w, h )
-		render.DrawScreenQuadEx( 0, 1, w, h )
-		render.DrawScreenQuadEx( 1, 1, w, h )
-		render.DrawScreenQuadEx( 1, 0, w, h )
-		render.DrawScreenQuadEx( 1, 1, w, h )
-	
+		render.DrawScreenQuadEx( -OutlineThickness, -OutlineThickness, w ,h )
+		render.DrawScreenQuadEx( -OutlineThickness, 0, w, h )
+		render.DrawScreenQuadEx( -OutlineThickness, OutlineThickness, w, h )
+		render.DrawScreenQuadEx( 0, -OutlineThickness, w, h )
+		render.DrawScreenQuadEx( 0, OutlineThickness, w, h )
+		render.DrawScreenQuadEx( OutlineThickness, -OutlineThickness, w, h )
+		render.DrawScreenQuadEx( OutlineThickness, 0, w, h )
+		render.DrawScreenQuadEx( OutlineThickness, OutlineThickness, w, h )
+		
 	render.SetStencilEnable( false )
 	
+	render.ClearDepth()		-- Allows to render view model and other stuff in front of outline
 end
 
-hook.Add( "PostDrawEffects", "RenderOutlines", function()
-
-	hook.Run( "PreDrawOutlines" )
+local function RenderOutlines()
+	
+	hook.Run( "SetupOutlines", Add )
 	
 	if ( ListSize == 0 ) then return end
 	
 	Render()
 	
 	List, ListSize = {}, 0
+	
+end
+
+hook.Add( "PreDrawViewModels", "RenderOutlines", function()
+	
+	if ( RenderType == OUTLINE_RENDERTYPE_BEFORE_VM ) then
+		
+		RenderOutlines()
+		
+	end
+	
+end )
+
+hook.Add( "PreDrawEffects", "RenderOutlines", function()
+	
+	if ( RenderType == OUTLINE_RENDERTYPE_BEFORE_EF ) then
+		
+		RenderOutlines()
+		
+	end
+	
+end )
+
+hook.Add( "PostDrawEffects", "RenderOutlines", function()
+	
+	if ( RenderType == OUTLINE_RENDERTYPE_AFTER_EF ) then
+		
+		RenderOutlines()
+		
+	end
 	
 end )
