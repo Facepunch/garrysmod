a3bb7989f3329196a928f01699a5c093230edce4
Rubat <robotboy655@gmail.com>
2021-05-27 17:28:06 +0300

    Update super_dof.lua

--- garrysmod/lua/postprocess/super_dof.lua
+++ garrysmod/lua/postprocess/super_dof.lua
@@ -160,7 +160,7 @@ local texFSB = render.GetSuperFPTex()
 local matFSB = Material( "pp/motionblur" )
 local matFB = Material( "pp/fb" )
 
-surface.CreateFont( "DofText",
+surface.CreateFont( "SuperDofText",
 {
 	font		= "Helvetica",
 	size		= 20,
@@ -249,8 +249,8 @@ function RenderDoF( vOrigin, vAngle, vFocus, fAngleSize, radial_steps, passes, b
 				cam.Start2D()
 					local add = ( i / ( math.pi * 2 ) ) * ( 1 / passes )
 					local percent = string.format( "%.1f", ( mul - ( 1 / passes ) + add ) * 100 )
-					draw.DrawText( percent .. "%", "DofText", view.w - 100, view.h - 100, color_black, TEXT_ALIGN_CENTER )
-					draw.DrawText( percent .. "%", "DofText", view.w - 101, view.h - 101, color_white, TEXT_ALIGN_CENTER )
+					draw.DrawText( percent .. "%", "SuperDofText", view.w - 100, view.h - 100, color_black, TEXT_ALIGN_CENTER )
+					draw.DrawText( percent .. "%", "SuperDofText", view.w - 101, view.h - 101, color_white, TEXT_ALIGN_CENTER )
 				cam.End2D()
 
 				render.Spin()
