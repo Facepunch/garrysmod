d881335d69598fa2a7fdd7f06b5148e3188020bc
Down <53242610+Down-s@users.noreply.github.com>
2021-06-09 21:23:59 +0100

    Update dynamite.lua

--- garrysmod/gamemodes/sandbox/entities/weapons/gmod_tool/stools/dynamite.lua
+++ garrysmod/gamemodes/sandbox/entities/weapons/gmod_tool/stools/dynamite.lua
@@ -152,6 +152,10 @@ function TOOL:Think()
 		self:MakeGhostEntity( mdl, vector_origin, angle_zero )
 	end
 
+end
+
+function TOOL:DrawHUD()
+
 	self:UpdateGhostDynamite( self.GhostEntity, self:GetOwner() )
 
 end
