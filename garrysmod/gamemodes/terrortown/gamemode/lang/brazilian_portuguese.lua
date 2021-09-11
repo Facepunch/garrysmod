e9df1c2a0d5eed1ef09eaea0bf202aadc24f6b7f
Tiagoquix <30274161+Tiagoquix@users.noreply.github.com>
2021-09-11 17:34:17 -0300

    Update L.item_disg_desc
    The text was cutting off at the end. Here's the fix.

--- garrysmod/gamemodes/terrortown/gamemode/lang/brazilian_portuguese.lua
+++ garrysmod/gamemodes/terrortown/gamemode/lang/brazilian_portuguese.lua
@@ -307,9 +307,8 @@ Configure-o na aba Radar deste menu.]]
 
 L.item_disg       = "Disfarce"
 L.item_disg_desc  = [[
-Oculta sua identidade enquanto estiver habilitado.
-Também evita ser a última pessoa vista
-por uma vítima.
+Oculta sua identidade enquanto habilitado. Também
+evita ser a última pessoa vista por uma vítima.
 
 Habilite-o na aba Disfarce deste menu
 ou aperte a tecla Enter do teclado numérico.]]
