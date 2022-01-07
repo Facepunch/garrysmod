77ad5d1ff3faf6a312508a56ffed10868c15e2a5
Garrett Brown <grbrown93@sbcglobal.net>
2022-01-07 05:38:07 -0500

    Nocollides shouldn't use phys_constraintsystem

--- garrysmod/lua/includes/modules/constraint.lua
+++ garrysmod/lua/includes/modules/constraint.lua
@@ -922,17 +922,13 @@ function NoCollide( Ent1, Ent2, Bone1, Bone2 )
 	end
 
 	-- Make Constraint
-	onStartConstraint( Ent1, Ent2 )
+	local Constraint = ents.Create( "logic_collision_pair" )
+	Constraint:SetKeyValue( "startdisabled", 1 )
+	Constraint:SetPhysConstraintObjects( Phys1, Phys2 )
+	Constraint:Spawn()
+	Constraint:Activate()
+	Constraint:Input( "DisableCollisions", nil, nil, nil )
 
-		local Constraint = ents.Create( "logic_collision_pair" )
-		ConstraintCreated( Constraint )
-		Constraint:SetKeyValue( "startdisabled", 1 )
-		Constraint:SetPhysConstraintObjects( Phys1, Phys2 )
-		Constraint:Spawn()
-		Constraint:Activate()
-		Constraint:Input( "DisableCollisions", nil, nil, nil )
-
-	onFinishConstraint( Ent1, Ent2 )
 	AddConstraintTable( Ent1, Constraint, Ent2 )
 
 	local ctable = {
