bdd6158f88584109dedac833faa11bd7c7088477
Rubat <3299036+robotboy655@users.noreply.github.com>
2022-02-03 19:54:25 +0200

    Fixed a few server lag exploits

--- garrysmod/lua/autorun/properties/kinect_controller.lua
+++ garrysmod/lua/autorun/properties/kinect_controller.lua
@@ -8,6 +8,8 @@ if ( SERVER ) then
 
 end
 
+local playerTimeouts = {}
+
 properties.Add( "motioncontrol_ragdoll", {
 	MenuLabel = "#control_with_motion_sensor",
 	Order = 2500,
@@ -44,6 +46,22 @@ properties.Add( "motioncontrol_ragdoll", {
 		local ent = net.ReadEntity()
 		if ( !self:Filter( ent, player ) ) then return end
 
+		-- Do not spam please!
+		local timeout = playerTimeouts[ player ]
+		if ( timeout && timeout.time > CurTime() ) then
+			if ( !timeout.sentMessage ) then
+				ServerLog( "Player " .. tostring( player ) .. " tried to use 'motioncontrol_ragdoll' property too rapidly!\n" )
+				player:PrintMessage( HUD_PRINTTALK, "Please wait at least 0.2 seconds before trying to control another ragdoll." )
+				timeout.sentMessage = true
+			end
+			return
+		end
+
+		-- Only 1 controller per ragdoll please!
+		if ( IsValid( ent.MotionSensorController ) ) then
+			ent.MotionSensorController:Remove()
+		end
+
 		local ragdoll_motion = ents.Create( "ragdoll_motion" )
 		ragdoll_motion:SetPos( player:EyePos() + player:EyeAngles():Forward() * 10 )
 		ragdoll_motion:SetAngles( Angle( 0, player:EyeAngles().yaw, 0 ) )
@@ -55,6 +73,8 @@ properties.Add( "motioncontrol_ragdoll", {
 			undo.AddEntity( ragdoll_motion )
 			undo.SetPlayer( player )
 		undo.Finish()
+
+		playerTimeouts[ player ] = { time = CurTime() + 0.2, sentMessage = false }
 
 	end
 
