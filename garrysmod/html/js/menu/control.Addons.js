f53f39f1ae74822fa1fa0c77a9386cb2ba78eb8c
Rubat <3299036+robotboy655@users.noreply.github.com>
2022-08-02 21:04:11 +0300

    Added "Select Page" button to addons menu

--- garrysmod/html/js/menu/control.Addons.js
+++ garrysmod/html/js/menu/control.Addons.js
@@ -177,10 +177,11 @@ function ControllerAddons( $scope, $element, $rootScope, $location )
 	}
 	$scope.SelectAllPage = function()
 	{
-		$scope.UnselectAll(); // Unselect items that might be not in $scope.Files
+		//$scope.UnselectAll(); // Unselect items that might be not in $scope.Files
 
 		for ( var k in $scope.Files )
 		{
+			if ( parseInt( $scope.Files[ k ].id ) < 1 ) continue;
 			$scope.SelectedItems[ $scope.Files[ k ].id ] = true;
 		}
 	}
