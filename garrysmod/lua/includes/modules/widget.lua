Facepunch/garrysmod

commit b547174e5f75e6540754c320cdaf5103088114dd

Optimize some points (#1609)
* pairs -> ipairs

in places where the table is only a numeric orderly table

* new vector -> modify vector

Thanks ExtReMLapin

* micro oprimization

* string.Implode -> table.concat

* string.Implode to concatenate

* string.Implode -> table.concat

* micro optimize ScrW, ScrH calc

* a little better readability

Okay, it's gonna be like this.

* revert -> table.concat

* some changes

* resolving confict

* resolve confict

* resolve confict

* Fix unresolved

* Localized children table and little changes

One self.ChildNodes:GetChildren() call to locale
using a cheap method of calculating the length of a serial array

* Resolve confict

* Using the usual numerical for

There is no need to spend time on comparison every time, when you can just walk through the cycle to the penultimate element and say that it is not the last one, and then take the last element from the table and tell it that it is the last one.

* Let's not create new vectors to create a box.

* Added the math.ceil

that has been lost in past changes.

* A more readable MakeNiceName (#3)

* A more readable MakeNiceName
* Use string library instead of string methods
* Performance Enhancements

Co-authored-by: JarosLucky <snyashka@gmail.com>

* Fix extra "end" in outputs.lua

* Remove useless changes

* Fix usage of non existent function

Co-authored-by: CornerPin <cornerpinart@gmail.com>
Co-authored-by: Rubat <3299036+robotboy655@users.noreply.github.com>

garrysmod/lua/includes/modules/widget.lua
@@ -8,38 +8,43 @@ widgets = {}
 
 --
 -- Holds the currently hovered widget
--- 
+--
 widgets.Hovered = nil
 widgets.HoveredPos = vector_origin
 
 --
 -- Holds the current pressed widget
--- 
+--
 widgets.Pressed = nil
 
 local function UpdateHovered( pl, mv )
 
 	if ( !IsValid( pl ) ) then return end
 
-	if ( !pl:Alive() ) then 
-		pl:SetHoveredWidget( NULL ) 
-		return 
+	if ( !pl:Alive() ) then
+		pl:SetHoveredWidget( NULL )
+		return
 	end
 
 	local OldHovered = pl:GetHoveredWidget()
 	pl:SetHoveredWidget( NULL )
-
-	local trace = 
+	
+	local eyePos = pl:EyePos()
+	local aimVector = pl:GetAimVector()
+	aimVector:Mult( 256 )
+	aimVector:Add( eyePos )
+	
+	local trace =
 	{
-		start	= pl:EyePos(),
-		endpos	= pl:EyePos() + pl:GetAimVector() * 256,
+		start	= eyePos,
+		endpos	= aimVector,
 		filter	= function( ent )
-		
+
 			return IsValid( ent ) && ent:IsWidget()
-		
+
 		end
 	}
-	
+
 --	debugoverlay.Line( trace.start, trace.endpos, 0.5 )
 
 	widgets.Tracing = true
@@ -51,13 +56,13 @@ local function UpdateHovered( pl, mv )
 	if ( !tr.Entity:IsWidget() ) then return end
 
 --	debugoverlay.Cross( tr.HitPos, 1, 60 )
-	
+
 	if ( OldHovered != tr.Entity ) then
-		
+
 		-- On hover changed? why bother?
-		
+
 	end
-	
+
 	pl:SetHoveredWidget( tr.Entity )
 	pl.WidgetHitPos = tr.HitPos
 
@@ -69,34 +74,34 @@ local function UpdateButton( pl, mv, btn, mousebutton )
 	local was = mv:KeyWasDown( btn )
 	local hvr = pl:GetHoveredWidget()
 	local prs = pl:GetPressedWidget()
-	
+
 	if ( now && !was && IsValid( hvr ) ) then
-		hvr:OnPress( pl, mousebutton, mv ) 
+		hvr:OnPress( pl, mousebutton, mv )
 	end
-	
+
 	if ( !now && was && IsValid( prs ) ) then
-		prs:OnRelease( pl, mousebutton, mv )	
+		prs:OnRelease( pl, mousebutton, mv )
 	end
 end
 
 
 --
--- The idea here is to have exactly the same 
+-- The idea here is to have exactly the same
 -- behaviour on the client as the server.
 --
 function widgets.PlayerTick( pl, mv )
 
 	UpdateHovered( pl, mv )
-	
+
 	UpdateButton( pl, mv, IN_ATTACK, 1 )
 	UpdateButton( pl, mv, IN_ATTACK2, 2 )
-	
+
 	local prs = pl:GetPressedWidget()
-	
+
 	if ( IsValid( prs ) ) then
 		prs:PressedThinkInternal( pl, mv )
 	end
-	
+
 end
 
 
@@ -112,19 +117,19 @@ function widgets.RenderMe( ent )
 	-- The pressed widget gets to decide what should draw
 	--
 	if ( LocalPlayer() && IsValid(LocalPlayer():GetPressedWidget()) ) then
-	
-		if ( !LocalPlayer():GetPressedWidget():PressedShouldDraw( ent ) ) then 
-			return 
+
+		if ( !LocalPlayer():GetPressedWidget():PressedShouldDraw( ent ) ) then
+			return
 		end
-	
+
 	end
-	
+
 
 	table.insert( RenderList, ent )
 
 end
 
-hook.Add( "PostDrawEffects", "RenderWidgets", function() 
+hook.Add( "PostDrawEffects", "RenderWidgets", function()
 
 	--
 	-- Don't do anything if we don't have widgets to render!
@@ -133,14 +138,14 @@ hook.Add( "PostDrawEffects", "RenderWidgets", function()
 
 	cam.Start3D( EyePos(), EyeAngles() )
 
-		for k, v in pairs( RenderList ) do
-	
+		for k, v in ipairs( RenderList ) do
+
 			v:OverlayRender()
-	
+
 		end
-	
+
 	cam.End3D()
-	
+
 	RenderList = {}
 
 end )

garrysmod/gamemodes/base/entities/entities/base_ai/tasks.lua
@@ -60,8 +60,7 @@ end
 -----------------------------------------------------------]]
 function ENT:TaskStart_FindEnemy( data )
 
-	local et = ents.FindInSphere( self:GetPos(), data.Radius or 512 )
-	for k, v in ipairs( et ) do
+	for k, v in ipairs( ents.FindInSphere( self:GetPos(), data.Radius or 512 ) ) do
 
 		if ( v:IsValid() && v != self && v:GetClass() == data.Class ) then
 

garrysmod/gamemodes/base/entities/entities/base_entity/outputs.lua
@@ -89,7 +89,7 @@ local function FireSingleOutput( output, this, activator, data )
 		entitiesToFire = ents.FindByName( output.entities )
 	end
 
-	for _, ent in pairs( entitiesToFire ) do
+	for _, ent in ipairs( entitiesToFire ) do
 		ent:Fire( output.input, data or output.param, output.delay, activator, this )
 	end
 

garrysmod/gamemodes/base/entities/entities/base_nextbot/sv_nextbot.lua
@@ -219,14 +219,14 @@ function ENT:FindSpots( tbl )
 	local found = {}
 
 	-- In each area
-	for _, area in pairs( areas ) do
+	for _, area in ipairs( areas ) do
 
 		-- get the spots
 		local spots
 
 		if ( tbl.type == 'hiding' ) then spots = area:GetHidingSpots() end
 
-		for k, vec in pairs( spots ) do
+		for k, vec in ipairs( spots ) do
 
 			-- Work out the length, and add them to a table
 			path:Invalidate()

garrysmod/gamemodes/base/entities/entities/ragdoll_motion.lua
@@ -166,7 +166,7 @@ function ENT:DrawDebug( ragdoll, controller, pos, ang, rotation, scale, center, 
 	--
 	-- Draw bones
 	--
-	for k, v in pairs( motionsensor.DebugBones ) do
+	for k, v in ipairs( motionsensor.DebugBones ) do
 
 		debugoverlay.Line( realbonepos[ v[1] ], realbonepos[ v[2] ], StayTime, col_bone, true )
 
@@ -175,7 +175,7 @@ function ENT:DrawDebug( ragdoll, controller, pos, ang, rotation, scale, center, 
 	--
 	-- Draw translated sensor bones
 	--
-	for k, v in pairs( motionsensor.DebugBones ) do
+	for k, v in ipairs( motionsensor.DebugBones ) do
 
 		debugoverlay.Line( fixedbonepos[ v[1] ], fixedbonepos[ v[2] ], StayTime, col_tran_bn, true )
 

garrysmod/gamemodes/base/gamemode/animations.lua
@@ -269,7 +269,7 @@ function GM:MouthMoveAnimation( ply )
 
 	local weight = ply:IsSpeaking() && math.Clamp( ply:VoiceVolume() * 2, 0, 2 ) || 0
 
-	for k, v in pairs( flexes ) do
+	for k, v in ipairs( flexes ) do
 
 		ply:SetFlexWeight( v, weight )
 

garrysmod/gamemodes/base/gamemode/cl_deathnotice.lua
@@ -191,7 +191,7 @@ function GM:DrawDeathNotice( x, y )
 	y = y * ScrH()
 
 	-- Draw
-	for k, Death in pairs( Deaths ) do
+	for k, Death in ipairs( Deaths ) do
 
 		if ( Death.time + hud_deathnotice_time > CurTime() ) then
 
@@ -213,7 +213,7 @@ function GM:DrawDeathNotice( x, y )
 	-- We want to maintain the order of the table so instead of removing
 	-- expired entries one by one we will just clear the entire table
 	-- once everything is expired.
-	for k, Death in pairs( Deaths ) do
+	for k, Death in ipairs( Deaths ) do
 		if ( Death.time + hud_deathnotice_time > CurTime() ) then
 			return
 		end

garrysmod/gamemodes/base/gamemode/cl_init.lua
@@ -196,7 +196,7 @@ function GM:OnChatTab( str )
 
 	if ( LastWord == nil ) then return str end
 
-	for k, v in pairs( player.GetAll() ) do
+	for k, v in ipairs( player.GetAll() ) do
 
 		local nickname = v:Nick()
 

commit bb2e029ac090e88f035bee431cd6c83cbc126bb4

Fixed a regression caused by PR #1609

garrysmod/lua/includes/modules/widget.lua
@@ -31,7 +31,7 @@ local function UpdateHovered( pl, mv )
 	
 	local eyePos = pl:EyePos()
 	local aimVector = pl:GetAimVector()
-	aimVector:Mult( 256 )
+	aimVector:Mul( 256 )
 	aimVector:Add( eyePos )
 	
 	local trace =