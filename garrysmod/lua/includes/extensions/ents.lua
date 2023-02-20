4a1048bc66dd29de468622bcc08561c2bb55688c
Vicente <70909800+vicentefelipechile@users.noreply.github.com>
2023-02-19 22:16:26 -0300

    Add ents.Find First Class and ents.FindFirstModel

--- garrysmod/lua/includes/extensions/ents.lua
+++ garrysmod/lua/includes/extensions/ents.lua
@@ -24,3 +24,35 @@ function ents.FindByClassAndParent( classname, entity )
 	return out
 
 end
+
+
+--[[---------------------------------------------------------
+	Name: FindFirstClass
+	Desc: Gets all entities of the classname then returns the first value found, returns nil if not found
+---------------------------------------------------------]]--
+function ents.FindFirstClass( classname )
+
+	if type(classname) ~= "string" then return end
+	
+	for _, v in ipairs( ents.FindByClass( classname ) do
+		return v
+	end
+
+	return nil
+end
+
+
+--[[---------------------------------------------------------
+	Name: FindFirstModel
+	Desc: Gets all entities of the model then returns the first value found, returns nil if not found
+---------------------------------------------------------]]--
+function ents.FindFirstModel( model )
+
+	if type(model) ~= "string" then return end
+	
+	for _, v in ipairs( ents.FindByModel( model ) do
+		return v
+	end
+
+	return nil
+end
