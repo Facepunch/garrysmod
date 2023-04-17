cdbc94b81acb07ce614de50ba86b239aaf3b4fb7
code_gs <collings509@gmail.com>
2023-04-17 12:32:01 -0500

    Refactor weapon_medkit (#1943)
    * Refactor weapon_medkit
    
    - Removed the ammo/idle timers and converted them to predicted netvars like weapon_fists
    - Divided up the Primary/SecondaryAttack functions into SWEP:CanHeal(entity), SWEP:DoHeal(entity), SWEP:HealEntity(entity), and SWEP:HealFail(entity)
    - Changed many constants to class variables to promote addon SWEP modification instead of copying and modifying the code
    - Heal success/failures now punishes both primary and secondary fire no matter which heal type it is to prevent being able to interrupt the medpack animation with the opposite heal type attack
    - Removed SWEP.MaxAmmo as SWEP.Primary.ClipSize was already serving its purpose
    - General optimisations/cleanup
    
    * Hanging space cleanup
    
    * GetHealth->Health
    
    * Remove default SWEP:Reload
    
    Doesn't cause issues currently, just doesn't need to call DefaultReload
    
    * Final cleanup
    
    - Changed Ammo to an empty string. "none" makes no difference
    - Only update the NextAmmoRegen netvar if ammo was given. Prevents regen from being on a constant cycle instead of action-based
    - Changed TakePrimaryAmmo to a plain SetClip1 to match what's done in SWEP:Think + skip some unnecessary logic
    
    * Small fixes
    
    - GetClip1->Clip1
    - math.min(..., 0)->math.max(..., 0) when setting the clip - preventing it from being set negative instead of always being set to 0
    - Healing sets the NextAmmoRegen to ensure regen always happens AmmoRegenFrequency seconds after the last heal
    
    * Use the local in CustomAmmoDisplay
    
    * deuglify trace code
    
    ---------
    
    Co-authored-by: Rubat <3299036+robotboy655@users.noreply.github.com>

--- garrysmod/lua/weapons/weapon_medkit.lua
+++ garrysmod/lua/weapons/weapon_medkit.lua
@@ -2,8 +2,8 @@
 AddCSLuaFile()
 
 SWEP.PrintName = "#GMOD_MedKit"
-SWEP.Author = "robotboy655 & MaxOfS2D"
-SWEP.Purpose = "Heal people with your primary attack, or yourself with the secondary."
+SWEP.Author = "robotboy655, MaxOfS2D, code_gs"
+SWEP.Purpose = "Heal other people with primary attack, heal yourself with secondary attack."
 
 SWEP.Slot = 5
 SWEP.SlotPos = 3
@@ -18,135 +18,178 @@ SWEP.UseHands = true
 SWEP.Primary.ClipSize = 100
 SWEP.Primary.DefaultClip = 100
 SWEP.Primary.Automatic = false
-SWEP.Primary.Ammo = "none"
+SWEP.Primary.Ammo = ""
 
 SWEP.Secondary.ClipSize = -1
 SWEP.Secondary.DefaultClip = -1
 SWEP.Secondary.Automatic = false
-SWEP.Secondary.Ammo = "none"
+SWEP.Secondary.Ammo = ""
+
+SWEP.HoldType = "slam"
+
+SWEP.HealSound = Sound( "HealthKit.Touch" )
+SWEP.DenySound = Sound( "WallHealth.Deny" )
+
+SWEP.HealCooldown = 0.5
+SWEP.DenyCooldown = 1
 
 SWEP.HealAmount = 20 -- Maximum heal amount per use
-SWEP.MaxAmmo = 100 -- Maxumum ammo
+SWEP.HealRange = 64
 
-local HealSound = Sound( "HealthKit.Touch" )
-local DenySound = Sound( "WallHealth.Deny" )
+SWEP.AmmoRegenFrequency = 1 -- Number of seconds before each ammo regen
+SWEP.AmmoRegenAmount = 2 -- Amount of ammo refilled every AmmoRegenFrequency seconds
 
 function SWEP:Initialize()
 
-	self:SetHoldType( "slam" )
+	self:SetHoldType( self.HoldType )
 
-	if ( CLIENT ) then return end
+	if ( CLIENT ) then
+		self.AmmoDisplay = {
+			Draw = true,
+			PrimaryClip = 0
+		}
+	end
 
-	timer.Create( "medkit_ammo" .. self:EntIndex(), 1, 0, function()
-		if ( self:Clip1() < self.MaxAmmo ) then self:SetClip1( math.min( self:Clip1() + 2, self.MaxAmmo ) ) end
-	end )
+end
+
+function SWEP:SetupDataTables()
+
+	self:NetworkVar( "Float", 0, "NextAmmoRegen" )
+	self:NetworkVar( "Float", 1, "NextIdle" )
 
 end
 
 function SWEP:PrimaryAttack()
 
-	if ( CLIENT ) then return end
-
 	local owner = self:GetOwner()
+	local isplayer = SERVER and owner:IsPlayer()
 
-	if ( owner:IsPlayer() ) then
+	if ( isplayer ) then
 		owner:LagCompensation( true )
 	end
 
+	local startpos = owner:GetShootPos()
 	local tr = util.TraceLine( {
-		start = owner:GetShootPos(),
-		endpos = owner:GetShootPos() + owner:GetAimVector() * 64,
+		start = startpos,
+		endpos = startpos + owner:GetAimVector() * 64,
 		filter = owner
 	} )
 
-	if ( owner:IsPlayer() ) then
+	if ( isplayer ) then
 		owner:LagCompensation( false )
 	end
 
-	local ent = tr.Entity
-
-	local need = self.HealAmount
-	if ( IsValid( ent ) ) then need = math.min( ent:GetMaxHealth() - ent:Health(), self.HealAmount ) end
-
-	if ( IsValid( ent ) && self:Clip1() >= need && ( ent:IsPlayer() or ent:IsNPC() ) && ent:Health() < ent:GetMaxHealth() ) then
-
-		self:TakePrimaryAmmo( need )
-
-		ent:SetHealth( math.min( ent:GetMaxHealth(), ent:Health() + need ) )
-		ent:EmitSound( HealSound )
-
-		self:SendWeaponAnim( ACT_VM_PRIMARYATTACK )
-
-		self:SetNextPrimaryFire( CurTime() + self:SequenceDuration() + 0.5 )
-		owner:SetAnimation( PLAYER_ATTACK1 )
-
-		-- Even though the viewmodel has looping IDLE anim at all times, we need this to make fire animation work in multiplayer
-		timer.Create( "weapon_idle" .. self:EntIndex(), self:SequenceDuration(), 1, function() if ( IsValid( self ) ) then self:SendWeaponAnim( ACT_VM_IDLE ) end end )
-
-	else
-
-		owner:EmitSound( DenySound )
-		self:SetNextPrimaryFire( CurTime() + 1 )
-
-	end
+	self:DoHeal( tr.Entity )
 
 end
 
 function SWEP:SecondaryAttack()
 
-	if ( CLIENT ) then return end
+	self:DoHeal( self:GetOwner() )
 
-	local ent = self:GetOwner()
+end
 
-	local need = self.HealAmount
-	if ( IsValid( ent ) ) then need = math.min( ent:GetMaxHealth() - ent:Health(), self.HealAmount ) end
+-- Basic black/whitelist function
+-- Checking if the entity's health is below its max is done in SWEP:DoHeal
+function SWEP:CanHeal( ent )
 
-	if ( IsValid( ent ) && self:Clip1() >= need && ent:Health() < ent:GetMaxHealth() ) then
+	-- ent may be NULL here, but these functions return false for it
+	return ent:IsPlayer() or ent:IsNPC()
 
-		self:TakePrimaryAmmo( need )
+end
 
-		ent:SetHealth( math.min( ent:GetMaxHealth(), ent:Health() + need ) )
-		ent:EmitSound( HealSound )
+function SWEP:HealEntity( ent, amount )
 
-		self:SendWeaponAnim( ACT_VM_PRIMARYATTACK )
+	-- Heal ent
+	self:SetClip1( math.max( self:Clip1() - amount, 0 ) )
+	ent:SetHealth( ent:Health() + amount )
 
-		self:SetNextSecondaryFire( CurTime() + self:SequenceDuration() + 0.5 )
-		ent:SetAnimation( PLAYER_ATTACK1 )
+	-- Do effects
+	self:EmitSound( self.HealSound )
+	self:SendWeaponAnim( ACT_VM_PRIMARYATTACK )
 
-		timer.Create( "weapon_idle" .. self:EntIndex(), self:SequenceDuration(), 1, function() if ( IsValid( self ) ) then self:SendWeaponAnim( ACT_VM_IDLE ) end end )
+	local owner = self:GetOwner()
 
-	else
-
-		ent:EmitSound( DenySound )
-		self:SetNextSecondaryFire( CurTime() + 1 )
-
+	if ( owner:IsValid() ) then
+		owner:SetAnimation( PLAYER_ATTACK1 )
 	end
 
+	local curtime = CurTime()
+
+	-- Set next regen time
+	self:SetNextAmmoRegen( curtime + self.AmmoRegenFrequency )
+
+	-- Set next idle time
+	local endtime = curtime + self:SequenceDuration()
+	self:SetNextIdle( endtime )
+
+	-- Set next firing time
+	endtime = endtime + self.HealCooldown
+	self:SetNextPrimaryFire( endtime )
+	self:SetNextSecondaryFire( endtime )
+
 end
 
-function SWEP:OnRemove()
+function SWEP:HealFail( ent )
 
-	if ( CLIENT ) then return end
+	-- Do effects
+	self:EmitSound( self.DenySound )
 
-	timer.Remove( "medkit_ammo" .. self:EntIndex() )
-	timer.Remove( "weapon_idle" .. self:EntIndex() )
+	-- Setup next firing time
+	local endtime = CurTime() + self.DenyCooldown
+	self:SetNextPrimaryFire( endtime )
+	self:SetNextSecondaryFire( endtime )
 
 end
 
-function SWEP:Holster()
+function SWEP:DoHeal( ent )
 
-	timer.Stop( "weapon_idle" .. self:EntIndex() )
+	if ( !self:CanHeal( ent ) ) then self:HealFail( ent ) return false end
+
+	local health, maxhealth = ent:Health(), ent:GetMaxHealth()
+	if ( health >= maxhealth ) then self:HealFail( ent ) return false end
+
+	local need = math.min( maxhealth - health, self.HealAmount )
+	if ( self:Clip1() < need ) then self:HealFail( ent ) return false end
+
+	self:HealEntity( ent, need )
 
 	return true
 
 end
 
+function SWEP:Think()
+
+	local curtime = CurTime()
+
+	if ( curtime >= self:GetNextAmmoRegen() ) then
+		local clip1 = self:Clip1()
+		local maxammo = self.Primary.ClipSize
+
+		if ( clip1 < maxammo ) then
+			self:SetClip1( math.min( clip1 + self.AmmoRegenAmount, maxammo ) )
+			self:SetNextAmmoRegen( curtime + self.AmmoRegenFrequency )
+		end
+	end
+
+	if ( curtime >= self:GetNextIdle() ) then
+		self:SendWeaponAnim( ACT_VM_IDLE )
+		self:SetNextIdle( curtime + self:SequenceDuration() )
+	end
+
+end
+
+function SWEP:Reload()
+end
+
+-- The following code does not need to exist on the server, so bail
+if ( SERVER ) then return end
+
 function SWEP:CustomAmmoDisplay()
 
-	self.AmmoDisplay = self.AmmoDisplay or {}
-	self.AmmoDisplay.Draw = true
-	self.AmmoDisplay.PrimaryClip = self:Clip1()
+	local display = self.AmmoDisplay
+	display.PrimaryClip = self:Clip1()
 
-	return self.AmmoDisplay
+	return display
 
 end
