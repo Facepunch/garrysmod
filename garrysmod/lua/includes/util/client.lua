8710fc586d347aa0daba0b26b333d6d211fdc668
Zaurzo <101999193+Zaurzo@users.noreply.github.com>
2023-05-15 17:11:40 -0400

    Move RealFrameTime()
    Expose RealFrameTime() to the menu state

--- garrysmod/lua/includes/util/client.lua
+++ garrysmod/lua/includes/util/client.lua
@@ -1,23 +1,4 @@
 
---[[---------------------------------------------------------
-	Get the real frame time, instead of the
-		host_timescale linked frametime. This is for things
-		like GUI effects. NOT FOR REAL IN GAME STUFF(!!!)
------------------------------------------------------------]]
-local FrameTime = 0
-local LastQuery = 0
-
-function RealFrameTime() return FrameTime end
-
-local function RealFrameTimeThink()
-
-	FrameTime = math.Clamp( SysTime() - LastQuery, 0, 0.1 )
-	LastQuery = SysTime()
-
-end
-
-hook.Add( "Think", "RealFrameTime", RealFrameTimeThink ) -- Think is called after every frame on the client.
-
 local function RenderSpawnIcon_Prop( model, pos, middle, size )
 
 	if ( size < 900 ) then
