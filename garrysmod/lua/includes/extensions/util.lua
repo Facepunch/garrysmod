f13714693f315273f02280f1495a5c17a3e20076
Zaurzo <101999193+Zaurzo@users.noreply.github.com>
2023-05-15 17:10:50 -0400

    Move RealFrameTime()
    Expose RealFrameTime() to the menu state

--- garrysmod/lua/includes/extensions/util.lua
+++ garrysmod/lua/includes/extensions/util.lua
@@ -395,3 +395,24 @@ function util.IsBinaryModuleInstalled( name )
 
 	return false
 end
+
+if ( SERVER ) then return end
+
+--[[---------------------------------------------------------
+	Get the real frame time, instead of the
+		host_timescale linked frametime. This is for things
+		like GUI effects. NOT FOR REAL IN GAME STUFF(!!!)
+-----------------------------------------------------------]]
+local FrameTime = 0
+local LastQuery = 0
+
+function RealFrameTime() return FrameTime end
+
+local function RealFrameTimeThink()
+
+	FrameTime = math.Clamp( SysTime() - LastQuery, 0, 0.1 )
+	LastQuery = SysTime()
+
+end
+
+hook.Add( "Think", "RealFrameTime", RealFrameTimeThink ) -- Think is called after every frame on the client.
