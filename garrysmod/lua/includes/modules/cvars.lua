da542156287b4ed39529f0ef602c334b1c9c3341
PrikolMen <olegis019@gmail.com>
2023-05-29 22:29:27 +0400

    `4 spaces` -> `4 tabs`

--- garrysmod/lua/includes/modules/cvars.lua
+++ garrysmod/lua/includes/modules/cvars.lua
@@ -4,131 +4,131 @@ local type				= type
 local GetConVar			= GetConVar
 
 --[[---------------------------------------------------------
-    Name: cvar
-    Desc: Callbacks when cvars change
+	Name: cvar
+	Desc: Callbacks when cvars change
 -----------------------------------------------------------]]
 module( "cvars" )
 
 local ConVars = {}
 
 --[[---------------------------------------------------------
-    Name: GetConVarCallbacks
-    Desc: Returns a table of the given ConVars callbacks
+	Name: GetConVarCallbacks
+	Desc: Returns a table of the given ConVars callbacks
 -----------------------------------------------------------]]
 function GetConVarCallbacks( name, createIfNotFound )
 
-    local tab = ConVars[ name ]
-    if ( createIfNotFound and !tab ) then
-        tab = {}; ConVars[ name ] = tab
-    end
+	local tab = ConVars[ name ]
+	if ( createIfNotFound and !tab ) then
+		tab = {}; ConVars[ name ] = tab
+	end
 
-    return tab
+	return tab
 
 end
 
 --[[---------------------------------------------------------
-    Name: OnConVarChanged
-    Desc: Called by the engine
+	Name: OnConVarChanged
+	Desc: Called by the engine
 -----------------------------------------------------------]]
 function OnConVarChanged( name, old, new )
 
-    local tab = GetConVarCallbacks( name )
-    if ( !tab ) then return end
+	local tab = GetConVarCallbacks( name )
+	if ( !tab ) then return end
 
-    for i = 1, #tab do
-        local callback = tab[ i ]
-        if ( type( callback ) == "table" ) then
-            callback[ 1 ]( name, old, new )
-        else
-            callback( name, old, new )
-        end
-    end
+	for i = 1, #tab do
+		local callback = tab[ i ]
+		if ( type( callback ) == "table" ) then
+			callback[ 1 ]( name, old, new )
+		else
+			callback( name, old, new )
+		end
+	end
 
 end
 
 --[[---------------------------------------------------------
-    Name: AddChangeCallback
-    Desc: Adds a callback to be called when convar changes
+	Name: AddChangeCallback
+	Desc: Adds a callback to be called when convar changes
 -----------------------------------------------------------]]
 function AddChangeCallback( name, func, identifier )
 
-    local tab = GetConVarCallbacks( name, true )
+	local tab = GetConVarCallbacks( name, true )
 
-    if ( !identifier ) then
-        table.insert( tab, func )
-        return
-    end
+	if ( !identifier ) then
+		table.insert( tab, func )
+		return
+	end
 
-    for i = 1, #tab do
-        local callback = tab[ i ]
-        if ( type( callback ) == "table" and callback[ 2 ] == identifier ) then
-            callback[ 1 ] = func
-            return
-        end
-    end
+	for i = 1, #tab do
+		local callback = tab[ i ]
+		if ( type( callback ) == "table" and callback[ 2 ] == identifier ) then
+			callback[ 1 ] = func
+			return
+		end
+	end
 
-    table.insert( tab, { func, identifier } )
+	table.insert( tab, { func, identifier } )
 
 end
 
 --[[---------------------------------------------------------
-    Name: RemoveChangeCallback
-    Desc: Removes callback with identifier
+	Name: RemoveChangeCallback
+	Desc: Removes callback with identifier
 -----------------------------------------------------------]]
 function RemoveChangeCallback( name, identifier )
 
-    local tab = GetConVarCallbacks( name, true )
-    for i = 1, #tab do
-        local callback = tab[ i ]
-        if ( type( callback ) == "table" and callback[ 2 ] == identifier ) then
-            table.remove( tab, i )
-            break
-        end
-    end
+	local tab = GetConVarCallbacks( name, true )
+	for i = 1, #tab do
+		local callback = tab[ i ]
+		if ( type( callback ) == "table" and callback[ 2 ] == identifier ) then
+			table.remove( tab, i )
+			break
+		end
+	end
 
 end
 
 --[[---------------------------------------------------------
-    Name: String
-    Desc: Retrieves console variable as a string
+	Name: String
+	Desc: Retrieves console variable as a string
 -----------------------------------------------------------]]
 function String( name, default )
 
-    local convar = GetConVar( name )
-    if ( convar ~= nil ) then
-        return convar:GetString()
-    end
+	local convar = GetConVar( name )
+	if ( convar ~= nil ) then
+		return convar:GetString()
+	end
 
-    return default
+	return default
 
 end
 
 --[[---------------------------------------------------------
-    Name: Number
-    Desc: Retrieves console variable as a number
+	Name: Number
+	Desc: Retrieves console variable as a number
 -----------------------------------------------------------]]
 function Number( name, default )
 
-    local convar = GetConVar( name )
-    if ( convar ~= nil ) then
-        return convar:GetFloat()
-    end
+	local convar = GetConVar( name )
+	if ( convar ~= nil ) then
+		return convar:GetFloat()
+	end
 
-    return default
+	return default
 
 end
 
 --[[---------------------------------------------------------
-    Name: Bool
-    Desc: Retrieves console variable as a boolean
+	Name: Bool
+	Desc: Retrieves console variable as a boolean
 -----------------------------------------------------------]]
 function Bool( name, default )
 
-    local convar = GetConVar( name )
-    if ( convar ~= nil ) then
-        return convar:GetBool()
-    end
+	local convar = GetConVar( name )
+	if ( convar ~= nil ) then
+		return convar:GetBool()
+	end
 
-    return default
+	return default
 
 end
