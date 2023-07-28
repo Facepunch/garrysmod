c6429797248222e93d957dd1293a36501d6a887f
Rubat <3299036+robotboy655@users.noreply.github.com>
2023-07-28 10:37:18 -0700

    Update cleanup.lua

--- garrysmod/lua/includes/modules/cleanup.lua
+++ garrysmod/lua/includes/modules/cleanup.lua
@@ -62,7 +62,7 @@ if ( SERVER ) then
 
 		if ( !IsType( type ) ) then return end
 
-		local id = pl:AccountID() or 0
+		local id = pl:UniqueID()
 
 		cleanup_list[ id ] = cleanup_list[ id ] or {}
 		cleanup_list[ id ][ type ] = cleanup_list[ id ][ type ] or {}
@@ -99,7 +99,7 @@ if ( SERVER ) then
 
 		if ( !IsValid( pl ) ) then return end
 
-		local id = pl:AccountID() or 0
+		local id = pl:UniqueID()
 
 		if ( !cleanup_list[ id ] ) then return end
 
