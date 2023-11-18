3a177160e0d039e21226913ef8374d00b14b31e6
T-Rizzle12 <92559393+T-Rizzle12@users.noreply.github.com>
2023-11-17 23:43:08 -0500

    Update cl_transfer.lua
    - Forgot to change SteamID to SteamID64

--- garrysmod/gamemodes/terrortown/gamemode/cl_transfer.lua
+++ garrysmod/gamemodes/terrortown/gamemode/cl_transfer.lua
@@ -34,7 +34,7 @@ function CreateTransferMenu(parent)
    local r = LocalPlayer():GetRole()
    for _, p in ipairs(player.GetAll()) do
       if IsValid(p) and p:IsActiveRole(r) and p != LocalPlayer() then
-         dpick:AddChoice(p:Nick(), p:SteamID())
+         dpick:AddChoice(p:Nick(), p:SteamID64())
       end
    end
 
