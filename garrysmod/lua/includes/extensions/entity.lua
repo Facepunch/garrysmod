Facepunch/garrysmod

commit 84fe637bde40942c76b8db1cd5b25a2d8f893ab3

DT/NetworkVar Changes (#1787)
This is an attempt to close garrysmod-requests/issues#798. This allows you to omit the slot of DT/NetworkVars, which simplifies DataTable inheritance. ```Entity:IsDTVarSlotUsed(typename, slot)``` and minor optimizations are also included.

The DT/NetworkVars slot parameter can either be nil or completely omitted:
```
local ENT = {}

ENT.Type = "anim"

function ENT:SetupDataTables()
	self:NetworkVar("Int", 1, "Base1")
	self:NetworkVar("Int", 3, "Base2")
	self:NetworkVar("Int", 5, "Base3")
	
	self:NetworkVar("Float", 0, "Base4")
	self:NetworkVar("Float", 1, "Base5")
	
	if SERVER then
		self:SetBase1(1)
		self:SetBase2(2)
		self:SetBase3(3)
		self:SetBase4(4.4)
		self:SetBase5(5.5)
	end
end

function ENT:Initialize()
	print(self, self:GetClass())
	for i=0, 31 do
		if self:IsDTVarSlotUsed("Int", i) then
			print("Int " .. i .. " used", self:GetDTInt(i))
		end
	end
	for i=0, 31 do
		if self:IsDTVarSlotUsed("Float", i) then
			print("Float " .. i .. " used", self:GetDTFloat(i))
		end
	end
	PrintTable(self:GetNetworkVars())
end

scripted_ents.Register(ENT, "github_test")



local ENT = {}

DEFINE_BASECLASS("github_test")

ENT.Base = "github_test"

function ENT:SetupDataTables()
	BaseClass.SetupDataTables(self)
	
	self:NetworkVar("Int", nil, "Test1") -- backfills Int 0
	self:NetworkVar("Int", "Test2") -- backfills Int 2
	self:NetworkVar("Int", "Test3") -- backfills Int 4
	self:NetworkVar("Int", "Test4") -- uses Int 6
	-- Int 0 through 6 are used
	
	self:NetworkVar("Float", "Test5") -- uses Float 2
	self:NetworkVar("Float", "Test6") -- uses Float 3
	-- Float 0 through 3 are used
	
	if SERVER then
		self:SetTest1(-1)
		self:SetTest2(-2)
		self:SetTest3(-3)
		self:SetTest4(-4)
		self:SetTest5(-5.5)
		self:SetTest6(-6.6)
	end
end

scripted_ents.Register(ENT, "github_test_child")
```

garrysmod/lua/includes/extensions/entity.lua
@@ -218,6 +218,7 @@ end
 function meta:InstallDataTable()
 
 	self.dt = {}
+	local typetable = {}
 	local datatable = {}
 	local keytable = {}
 	local meta = {}
@@ -240,26 +241,57 @@ function meta:InstallDataTable()
 		dt.SetFunc( self, dt.index, value )
 
 	end
+	
+	local function FindUnusedIndex( typename )
+	
+		local tbl = typetable[ typename ]
+		if ( !tbl ) then return 0 end
+		
+		for i=0, 31 do
+			if ( !tbl[i] ) then return i end
+		end
+		
+	end
+	
+	self.IsDTVarSlotUsed = function( self, typename, index )
+	
+		local tbl = typetable[ typename ]
+		if ( !tbl || !tbl[index] ) then return false end
+		return true
+		
+	end
 
 	self.DTVar = function( ent, typename, index, name )
+
+		if ( isstring( index ) ) then
+			name = index
+			index = FindUnusedIndex( typename )
+		elseif ( !index && isstring( name ) ) then
+			index = FindUnusedIndex(typename)
+		end
 
 		local SetFunc = ent[ "SetDT" .. typename ]
 		local GetFunc = ent[ "GetDT" .. typename ]
 
 		if ( !SetFunc || !GetFunc ) then
-			MsgN( "Couldn't addvar " , name, " - type ", typename," is invalid!" )
+			MsgN( "Couldn't addvar ", name, " - type ", typename, " is invalid!" )
 			return
 		end
 
-		datatable[ name ] = {
+		local data = {
 			index = index,
+			name = name,
 			SetFunc = SetFunc,
 			GetFunc = GetFunc,
 			typename = typename,
 			Notify = {}
 		}
+		
+		typetable[ typename ] = typetable[ typename ] || {}
+		typetable[ typename ][ index ] = data
+		datatable[ name ] = data
 
-		return datatable[ name ]
+		return data
 
 	end
 
@@ -304,32 +336,40 @@ function meta:InstallDataTable()
 
 	local CallProxies = function( ent, tbl, name, oldval, newval )
 
-		for k, v in pairs( tbl ) do
-			v( ent, name, oldval, newval )
+		for i = 1, #tbl do
+			tbl[ i ]( ent, name, oldval, newval )
 		end
 
 	end
 
 	self.CallDTVarProxies = function( ent, typename, index, newVal )
-		for name, t in pairs( datatable ) do
-			if ( t.index == index && t.typename == typename ) then
-				CallProxies( ent, t.Notify, name, self.dt[ name ], newVal )
-				break
-			end
+		
+		local t = typetable[ typename ] && typetable[ typename ][ index ] || nil
+		if ( t ) then
+			CallProxies( ent, t.Notify, t.name, t.GetFunc( ent, index ), newVal )
 		end
+		
 	end
 
 	self.NetworkVar = function( ent, typename, index, name, other_data )
 
+		if ( isstring( index ) ) then
+			other_data = name
+			name = index
+			index = FindUnusedIndex( typename )
+		elseif ( !index && isstring( name ) ) then
+			index = FindUnusedIndex( typename )
+		end
+
 		local t = ent.DTVar( ent, typename, index, name )
 
 		ent[ "Set" .. name ] = function( self, value )
-			CallProxies( ent, t.Notify, name, self.dt[ name ], value )
-			self.dt[ name ] = value
+			CallProxies( ent, t.Notify, name, t.GetFunc( self, index ), value )
+			t.SetFunc( self, index, value )
 		end
 
 		ent[ "Get" .. name ] = function( self )
-			return self.dt[ name ]
+			return t.GetFunc( self, index )
 		end
 
 		if ( !other_data ) then return end
@@ -361,17 +401,26 @@ function meta:InstallDataTable()
 	--
 	self.NetworkVarElement = function( ent, typename, index, element, name, other_data )
 
-		local datatab = ent.DTVar( ent, typename, index, name, keyname )
-		datatab.element = element
+		if ( isstring( index ) ) then
+			other_data = name
+			name = element
+			element = index
+			index = FindUnusedIndex( typename )
+		elseif ( !index && isstring( name ) ) then
+			index = FindUnusedIndex( typename )
+		end
+
+		local t = ent.DTVar( ent, typename, index, name )
+		t.element = element
 
 		ent[ "Set" .. name ] = function( self, value )
-			local old = self.dt[ name ]
+			local old = t.GetFunc( self, index )
 			old[ element ] = value
-			self.dt[ name ] = old
+			t.SetFunc( self, index, old )
 		end
 
 		ent[ "Get" .. name ] = function( self )
-			return self.dt[ name ][ element ]
+			return t.GetFunc( self, index )[ element ]
 		end
 
 		if ( !other_data ) then return end

commit 45ff9bdf7da8d661200dc20540f970212571aef8

Fixed a regression introduced by PR #1787
Fixes issues with addons passing incorrect data types to Entity NetworkVar

garrysmod/lua/includes/extensions/entity.lua
@@ -263,7 +263,7 @@ function meta:InstallDataTable()
 
 	self.DTVar = function( ent, typename, index, name )
 
-		if ( isstring( index ) ) then
+		if ( isstring( index ) && !name ) then
 			name = index
 			index = FindUnusedIndex( typename )
 		elseif ( !index && isstring( name ) ) then
@@ -353,7 +353,7 @@ function meta:InstallDataTable()
 
 	self.NetworkVar = function( ent, typename, index, name, other_data )
 
-		if ( isstring( index ) ) then
+		if ( isstring( index ) && ( istable( name ) or !name ) ) then
 			other_data = name
 			name = index
 			index = FindUnusedIndex( typename )
@@ -401,7 +401,7 @@ function meta:InstallDataTable()
 	--
 	self.NetworkVarElement = function( ent, typename, index, element, name, other_data )
 
-		if ( isstring( index ) ) then
+		if ( isstring( index ) && isstring( element ) ) then
 			other_data = name
 			name = element
 			element = index