Facepunch/garrysmod

commit 2d76f8327324cbab1b50660cebfd48b991f1caae

Contain DButton's image size if it's too big (#1954)
* Crop DButton's image size if it's too big

* Preserving aspect ratio of DImage

* Clamp the icon's width as well, and position predictably

Change positioning so that clamped icons are aligned with smaller icons nicely.

---------

Co-authored-by: Rubat <3299036+robotboy655@users.noreply.github.com>

garrysmod/lua/vgui/dbutton.lua
@@ -102,7 +102,20 @@ function PANEL:PerformLayout( w, h )
 	--
 	if ( IsValid( self.m_Image ) ) then
 
-		self.m_Image:SetPos( 4, ( self:GetTall() - self.m_Image:GetTall() ) * 0.5 )
+		local targetSize = math.min( self:GetWide() - 4, self:GetTall() - 4 )
+
+		local zoom = math.min( targetSize / self.m_Image:GetWide(), targetSize / self.m_Image:GetTall(), 1 )
+		local newSizeX = math.ceil( self.m_Image:GetWide() * zoom )
+		local newSizeY = math.ceil( self.m_Image:GetTall() * zoom )
+
+		self.m_Image:SetWide( newSizeX )
+		self.m_Image:SetTall( newSizeY )
+
+		if ( self:GetWide() < self:GetTall() ) then
+			self.m_Image:SetPos( 4, ( self:GetTall() - self.m_Image:GetTall() ) * 0.5 )
+		else
+			self.m_Image:SetPos( 2 + ( targetSize - self.m_Image:GetWide() ) * 0.5, ( self:GetTall() - self.m_Image:GetTall() ) * 0.5 )
+		end 
 
 		self:SetTextInset( self.m_Image:GetWide() + 16, 0 )
 

commit f0d5e66484d40f5480c672cf233009c85876dff8

Fixed spawnmenu "Save Changes" / "Revert" buttons not having icons
Regression from https://github.com/Facepunch/garrysmod/pull/1954

garrysmod/lua/vgui/dbutton.lua
@@ -104,9 +104,10 @@ function PANEL:PerformLayout( w, h )
 
 		local targetSize = math.min( self:GetWide() - 4, self:GetTall() - 4 )
 
-		local zoom = math.min( targetSize / self.m_Image:GetWide(), targetSize / self.m_Image:GetTall(), 1 )
-		local newSizeX = math.ceil( self.m_Image:GetWide() * zoom )
-		local newSizeY = math.ceil( self.m_Image:GetTall() * zoom )
+		local imgW, imgH = self.m_Image.ActualWidth, self.m_Image.ActualHeight
+		local zoom = math.min( targetSize / imgW, targetSize / imgH, 1 )
+		local newSizeX = math.ceil( imgW * zoom )
+		local newSizeY = math.ceil( imgH * zoom )
 
 		self.m_Image:SetWide( newSizeX )
 		self.m_Image:SetTall( newSizeY )

garrysmod/lua/vgui/dimage.lua
@@ -140,7 +140,7 @@ function PANEL:FixVertexLitMaterial()
 
 end
 
-function PANEL:SizeToContents( strImage )
+function PANEL:SizeToContents()
 
 	self:SetSize( self.ActualWidth, self.ActualHeight )
 
@@ -153,6 +153,7 @@ function PANEL:Paint()
 end
 
 function PANEL:PaintAt( x, y, dw, dh )
+
 	dw, dh = dw or self:GetWide(), dh or self:GetTall()
 	self:LoadMaterial()
 