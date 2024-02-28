e7c2d0626c130b6bc129e0c23a3da2c38b954408
Timmy <timmy.the.nobody@gmail.com>
2024-02-28 14:43:33 +0100

    Prevent infinite loop from `panel:SetDragParent` (#2058)
    * Prevent infinite loop from `panel:SetDragParent`
    
    * Prevent DND infinite loop from `panel:DragMousePress`
    
    * Remove DND infinite loop prevention from `panel:SetDragParent`
    
    * Remove early return in `p:DraMousePress`
    
    This will allow the rest of the method to run even if the drag parent is the panel itself

--- garrysmod/lua/includes/extensions/client/panel/dragdrop.lua
+++ garrysmod/lua/includes/extensions/client/panel/dragdrop.lua
@@ -414,7 +414,7 @@ function meta:DragMousePress( mcode )
 	if ( IsValid( dragndrop.m_DropMenu ) ) then return end
 	if ( dragndrop.IsDragging() ) then dragndrop.StopDragging() return end
 
-	if ( IsValid( self.m_pDragParent ) ) then
+	if ( IsValid( self.m_pDragParent ) and self.m_pDragParent ~= self ) then
 		return self.m_pDragParent:DragMousePress( mcode )
 	end
 
