Facepunch/garrysmod

commit df0fe68c8aaca7cbdcd722ed40d2f2c756e471f2

Replaced util.TraceLine by util.TraceHull for tool traces. Fixes trace bypassing parented props (#1890)

garrysmod/gamemodes/sandbox/entities/weapons/gmod_tool/shared.lua
@@ -208,7 +208,9 @@ function SWEP:PrimaryAttack()
 
 	local tr = util.GetPlayerTrace( owner )
 	tr.mask = toolmask
-	local trace = util.TraceLine( tr )
+	tr.mins = vector_origin
+	tr.maxs = tr.mins
+	local trace = util.TraceHull( tr )
 	if ( !trace.Hit ) then return end
 
 	local tool = self:GetToolObject()
@@ -235,7 +237,9 @@ function SWEP:SecondaryAttack()
 
 	local tr = util.GetPlayerTrace( owner )
 	tr.mask = toolmask
-	local trace = util.TraceLine( tr )
+	tr.mins = vector_origin
+	tr.maxs = tr.mins
+	local trace = util.TraceHull( tr )
 	if ( !trace.Hit ) then return end
 
 	local tool = self:GetToolObject()
@@ -265,7 +269,9 @@ function SWEP:Reload()
 
 	local tr = util.GetPlayerTrace( owner )
 	tr.mask = bit.bor( CONTENTS_SOLID, CONTENTS_MOVEABLE, CONTENTS_MONSTER, CONTENTS_WINDOW, CONTENTS_DEBRIS, CONTENTS_GRATE, CONTENTS_AUX )
-	local trace = util.TraceLine( tr )
+	tr.mins = vector_origin
+	tr.maxs = tr.mins
+	local trace = util.TraceHull( tr )
 	if ( !trace.Hit ) then return end
 
 	local tool = self:GetToolObject()

commit 385da2bb5960e5977ef4dd7e563dfda056f2df71

Fixed inflator tool not working on NPCs (Regression from #1890)

garrysmod/gamemodes/sandbox/entities/weapons/gmod_tool/shared.lua
@@ -210,7 +210,8 @@ function SWEP:PrimaryAttack()
 	tr.mask = toolmask
 	tr.mins = vector_origin
 	tr.maxs = tr.mins
-	local trace = util.TraceHull( tr )
+	local trace = util.TraceLine( tr )
+	if ( !trace.Hit ) then trace = util.TraceHull( tr ) end
 	if ( !trace.Hit ) then return end
 
 	local tool = self:GetToolObject()
@@ -239,7 +240,8 @@ function SWEP:SecondaryAttack()
 	tr.mask = toolmask
 	tr.mins = vector_origin
 	tr.maxs = tr.mins
-	local trace = util.TraceHull( tr )
+	local trace = util.TraceLine( tr )
+	if ( !trace.Hit ) then trace = util.TraceHull( tr ) end
 	if ( !trace.Hit ) then return end
 
 	local tool = self:GetToolObject()
@@ -271,7 +273,8 @@ function SWEP:Reload()
 	tr.mask = toolmask
 	tr.mins = vector_origin
 	tr.maxs = tr.mins
-	local trace = util.TraceHull( tr )
+	local trace = util.TraceLine( tr )
+	if ( !trace.Hit ) then trace = util.TraceHull( tr ) end
 	if ( !trace.Hit ) then return end
 
 	local tool = self:GetToolObject()