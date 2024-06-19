9ccaa34da8a62dd38fa25e470ea1f6bbe3617522
Rubat <3299036+robotboy655@users.noreply.github.com>
2024-06-19 19:47:22 +0300

    mat_viewportscale problem

--- garrysmod/lua/menu/problems/problems.lua
+++ garrysmod/lua/menu/problems/problems.lua
@@ -229,9 +229,15 @@ timer.Create( "menu_check_for_problems", 1, 0, function()
 	end
 
 	if ( math.abs( GetConVarNumber( "voice_fadeouttime" ) - 0.1 ) > 0.001 ) then
-		FireProblem( { id = "voice_fadeouttime", text = "#problem.voice_fadeouttime", type = "config", fix = function() RunConsoleCommand( "voice_fadeouttime", "0.1" ) end } )
+		FireProblem( { id = "voice_fadeouttime", text = "#problem.voice_fadeouttime", type = "config", fix = function() RunConsoleCommand( "voice_fadeouttime", "0.1" ) ClearProblem( "voice_fadeouttime" ) end } )
 	else
 		ClearProblem( "voice_fadeouttime" )
+	end
+
+	if ( GetConVarNumber( "mat_viewportscale" ) < 0.1 ) then
+		FireProblem( { id = "mat_viewportscale", text = "#problem.mat_viewportscale", type = "config", fix = function() RunConsoleCommand( "mat_viewportscale", "1" ) ClearProblem( "mat_viewportscale" ) end } )
+	else
+		ClearProblem( "mat_viewportscale" )
 	end
 
 	if ( ScrW() < 1000 or ScrH() < 700 ) then
