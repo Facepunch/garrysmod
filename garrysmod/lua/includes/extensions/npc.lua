c4369cbc757a63e92be62f12b8cb2cd705f97752
eon (bloodycop) <minerva.servers.eon@gmail.com>
2024-10-17 17:01:40 +0300

    Update npc.lua

--- garrysmod/lua/includes/extensions/npc.lua
+++ garrysmod/lua/includes/extensions/npc.lua
@@ -1,8 +1,6 @@
 local NPC = FindMetaTable("NPC")
 
 function NPC:GiveSpawnFlag( flag )
-    if self:HasSpawnFlags( flag ) then return end
-
     local spawnFlags = self:GetSpawnFlags()
     local newSpawnFlags = bit.bor( spawnFlags, flag )
 
@@ -10,8 +8,6 @@ function NPC:GiveSpawnFlag( flag )
 end
 
 function NPC:RemoveSpawnFlag( flag )
-    if !self:HasSpawnFlags( flag ) then return end
-
     local spawnFlags = self:GetSpawnFlags()
     local newSpawnFlags = bit.band( spawnFlags, bit.bnot( flag ) )
 
