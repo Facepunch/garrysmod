e9d05c5c71d841899c8028c321615f89e1f07e0f
thecraftianman <64441307+thecraftianman@users.noreply.github.com>
2024-11-01 11:40:30 -0400

    Fix Bad SetLocalOrigin hands warnings

--- garrysmod/gamemodes/base/entities/entities/gmod_hands.lua
+++ garrysmod/gamemodes/base/entities/entities/gmod_hands.lua
@@ -34,10 +34,8 @@ end
 
 function ENT:GetPlayerColor()
 
-	--
 	-- Make sure there's an owner and they have this function
 	-- before trying to call it!
-	--
 	local owner = self:GetOwner()
 	if ( !IsValid( owner ) ) then return end
 	if ( !owner.GetPlayerColor ) then return end
@@ -46,22 +44,33 @@ function ENT:GetPlayerColor()
 
 end
 
-function ENT:ViewModelChanged( vm, old, new )
+function ENT:ViewModelChanged( vm )
 
-	-- Ignore other peoples viewmodel changes!
+	-- Ignore other people's viewmodel changes!
 	if ( vm:GetOwner() != self:GetOwner() ) then return end
+	-- Don't try to attach again if we're already attached
+	if ( self:GetParent() == vm ) then return end
 
 	self:AttachToViewmodel( vm )
 
 end
 
+function ENT:OnRemove( fullUpdate )
+
+	if ( fullUpdate ) then return end
+
+	-- Resolve engine complaints when unparenting from the viewmodel
+	self:SetPos( vector_origin )
+
+end
+
 function ENT:AttachToViewmodel( vm )
+
+	self:SetPos( self:GetOwner():GetPos() )
+	self:SetAngles( angle_zero )
 
 	self:AddEffects( EF_BONEMERGE )
 	self:SetParent( vm )
 	self:SetMoveType( MOVETYPE_NONE )
-
-	self:SetPos( vector_origin )
-	self:SetAngles( angle_zero )
 
 end
