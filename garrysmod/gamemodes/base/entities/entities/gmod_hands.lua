627956155c9f48ddb516163375fd6a654fb29a30
thecraftianman <64441307+thecraftianman@users.noreply.github.com>
2024-11-04 15:16:45 -0500

    Update formatting

--- garrysmod/gamemodes/base/entities/entities/gmod_hands.lua
+++ garrysmod/gamemodes/base/entities/entities/gmod_hands.lua
@@ -34,8 +34,10 @@ end
 
 function ENT:GetPlayerColor()
 
+	--
 	-- Make sure there's an owner and they have this function
 	-- before trying to call it!
+	--
 	local owner = self:GetOwner()
 	if ( !IsValid( owner ) ) then return end
 	if ( !owner.GetPlayerColor ) then return end
@@ -44,10 +46,11 @@ function ENT:GetPlayerColor()
 
 end
 
-function ENT:ViewModelChanged( vm )
+function ENT:ViewModelChanged( vm, old, new )
 
 	-- Ignore other people's viewmodel changes!
 	if ( vm:GetOwner() != self:GetOwner() ) then return end
+
 	-- Don't try to attach again if we're already attached
 	if ( self:GetParent() == vm ) then return end
 
