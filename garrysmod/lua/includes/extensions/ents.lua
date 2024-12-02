e29d6cedcafd10fa785a5e2712e7b9073f022692
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2024-12-03 02:40:15 +0300

    Optimize ents.FindByClassAndParent

--- garrysmod/lua/includes/extensions/ents.lua
+++ garrysmod/lua/includes/extensions/ents.lua
@@ -1,25 +1,20 @@
 
 function ents.FindByClassAndParent( classname, entity )
 
-	if ( !IsValid( entity ) ) then return end
-
-	local list = ents.FindByClass( classname )
-	if ( !list ) then return end
-
+    local entslist = ents.FindByClass( classname )
 	local out = {}
-	for k, v in ipairs( list ) do
 
-		if ( !IsValid(v) ) then continue end
+    for i = 1, #entslist do
 
-		local p = v:GetParent()
+        local p = entslist[i]:GetParent()
 		if ( !IsValid(p) ) then continue end
 		if ( p != entity ) then continue end
 
 		table.insert( out, v )
 
-	end
+    end
 
-	if ( #out == 0 ) then return end
+	if ( out[1] == nil ) then return end
 
 	return out
 
