e6de9daab40829c54f92cba4ebf05c2f3d1ef8e0
Beelzebub <34854689+Be1zebub@users.noreply.github.com>
2025-01-22 05:39:33 +0700

    Update color.lua
    fix style, rm :SetValue alias, add HSLToColor & HSVToColor lua implementation (100500x faster than C)

--- garrysmod/lua/includes/util/color.lua
+++ garrysmod/lua/includes/util/color.lua
@@ -31,7 +31,7 @@ end
 -----------------------------------------------------------]]
 function IsColor( obj )
 
-	return getmetatable(obj) == COLOR
+	return getmetatable( obj ) == COLOR
 
 end
 
@@ -196,9 +196,6 @@ function COLOR:AddBrightness( brightnessAdd )
 
 end
 
--- hsv value == hsb brightness
-COLOR.GetValue = COLOR.GetBrightness
-
 -- hsl lightness
 function COLOR:GetLightness()
 
@@ -288,6 +285,63 @@ function HWBToColor( h, w, b )
 	end
 
 	return HSVToColor( h, s, v )
+
+end
+
+function HSVToColor( h, s, v )
+
+	local c = v * s
+	local x = c * ( 1 - math.abs( ( h / 60 ) % 2 - 1 ) )
+	local m = v - c
+
+	local r, g, b
+	if ( h < 60 ) then
+		r, g, b = c, x, 0
+	elseif ( h < 120 ) then
+		r, g, b = x, c, 0
+	elseif ( h < 180 ) then
+		r, g, b = 0, c, x
+	elseif ( h < 240 ) then
+		r, g, b = 0, x, c
+	elseif ( h < 300 ) then
+		r, g, b = x, 0, c
+	else
+		r, g, b = c, 0, x
+	end
+
+	return Color(
+		( r + m ) * 255,
+		( g + m ) * 255,
+		( b + m ) * 255
+	)
+
+end
+
+local function HSLToColor( h, s, l )
+	local c = ( 1 - math.abs( 2 * l - 1 ) ) * s
+	local x = c * (1 - math.abs( ( h / 60 ) % 2 - 1) )
+	local m = l - c / 2
+
+	local r, g, b
+	if ( h < 60 ) then
+		r, g, b = c, x, 0
+	elseif ( h < 120 ) then
+		r, g, b = x, c, 0
+	elseif ( h < 180 ) then
+		r, g, b = 0, c, x
+	elseif ( h < 240 ) then
+		r, g, b = 0, x, c
+	elseif ( h < 300 ) then
+		r, g, b = x, 0, c
+	else
+		r, g, b = c, 0, x
+	end
+
+	return Color(
+		( r + m ) * 255,
+		( g + m ) * 255,
+		( b + m ) * 255
+	)
 
 end
 
