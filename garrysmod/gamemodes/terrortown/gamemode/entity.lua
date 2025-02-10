063c8d8d4eb4d750f2a96c519afebe9f203c9a71
figardo <figardo@users.noreply.github.com>
2025-02-10 13:29:19 +0000

    add audible range check

--- garrysmod/gamemodes/terrortown/gamemode/entity.lua
+++ garrysmod/gamemodes/terrortown/gamemode/entity.lua
@@ -19,8 +19,27 @@ end
 
 -- Some sounds are important enough that they shouldn't be affected by CPASAttenuationFilter
 function meta:BroadcastSound(snd, lvl, pitch, vol, channel, flags, dsp)
+   lvl = lvl or 75
+
    local rf = RecipientFilter()
-   rf:AddAllPlayers()
+
+   if lvl == 0 then
+      rf:AddAllPlayers()
+   else
+      -- Overriding the PAS filter means this will no longer check if players
+      -- are within audible range before sending them the sound message.
+      -- Instead, we reimplement this check in lua.
+      local pos = self:GetPos()
+
+      local attenuation = lvl > 50 and 20.0 / (lvl - 50) or 4.0
+      local maxAudible = math.min(2500, 2000 / attenuation)
+
+      for _, ply in player.Iterator() do
+         if (ply:EyePos() - pos):Length() > maxAudible then continue end
+
+         rf:AddPlayer(ply)
+      end
+   end
 
    self:EmitSound(snd, lvl, pitch, vol, channel, flags, dsp, rf)
 end
