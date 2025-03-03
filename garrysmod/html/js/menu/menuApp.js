d8189b033aac32b744a299b6e3efd946062dd5b9
Rubat <3299036+robotboy655@users.noreply.github.com>
2025-03-03 08:46:37 -0800

    Update menuApp.js (Formatting)

--- garrysmod/html/js/menu/menuApp.js
+++ garrysmod/html/js/menu/menuApp.js
@@ -1,3 +1,4 @@
+
 var IN_ENGINE = navigator.userAgent.indexOf( "Valve Source Client" ) != -1;
 var IS_SPAWN_MENU = false;
 
@@ -22,41 +23,42 @@ App.config( function( $routeProvider, $compileProvider, $locationProvider, $cont
 	$controllerProvider.register( 'ControllerSaves', ControllerSaves );
 	$controllerProvider.register( 'ControllerDemos', ControllerDemos );
 
-	//Update trusted protocols to include asset://
-	$compileProvider.imgSrcSanitizationTrustedUrlList( /^\s*((https?|ftp|file|blob|asset):|data:image\/)/ ); //Default: /^\s*((https?|ftp|file|blob):|data:image\/)/
-	$compileProvider.aHrefSanitizationTrustedUrlList( /^\s*(https?|s?ftp|mailto|tel|file|asset):/ ); //Default: /^\s*(https?|s?ftp|mailto|tel|file):/
+	// Update trusted protocols to include asset://
+	// Default: /^\s*((https?|ftp|file|blob):|data:image\/)/
+	$compileProvider.imgSrcSanitizationTrustedUrlList( /^\s*((https?|ftp|file|blob|asset):|data:image\/)/ ); 
+	// Default: /^\s*(https?|s?ftp|mailto|tel|file):/
+	$compileProvider.aHrefSanitizationTrustedUrlList( /^\s*(https?|s?ftp|mailto|tel|file|asset):/ );
 
-	//Default prefix is '!'. Gmod doesn't use this.
+	// Default prefix is '!'. Gmod doesn't use this.
 	$locationProvider.hashPrefix( '' );
 	
 	// AngularJS 1.8.2 doesn't apply 'href' on <a> unless ng-href is present. This breaks hover styles in Awesomium compared to 1.1.2.
 	// So we're copying the htmlAnchorDirective from https://github.com/angular/angular.js/blob/master/src/ng/directive/a.js
 	// and re-adding an empty href where needed.
-	$compileProvider.directive('a', function($compile)
+	$compileProvider.directive( 'a', function( $compile )
 	{
 		return {
 			restrict: 'E',
-			compile: function(element, attr) {
-				if (!attr.href && !attr.xlinkHref) {
-					return function(scope, element) {
-						if (element[0].nodeName.toLowerCase() !== 'a') return;
+			compile: function( element, attr )
+			{
+				if ( attr.href || attr.xlinkHref ) return;
+				
+				return function( scope, element )
+				{
+					if ( element[0].nodeName.toLowerCase() !== 'a') return;
 
-						if (!attr.href && !attr.ngHref) element.attr("href",""); // Re-add an empty href. see above comment.
+					if ( !attr.href && !attr.ngHref ) element.attr( "href", "" ); // Re-add an empty href. see above comment.
 
-						var href = toString.call(element.prop('href')) === '[object SVGAnimatedString]' ?
-							   'xlink:href' : 'href';
-						element.on('click', function(event) {
-							if (!element.attr(href)) {
-								event.preventDefault();
-							}
-						});
-					};
-				}
+					var href = toString.call( element.prop( 'href' ) ) === '[object SVGAnimatedString]' ? 'xlink:href' : 'href';
+					element.on( 'click', function( event ) {
+						if ( !element.attr( href ) ) event.preventDefault();
+					} );
+				};
 			}
 		};
-	});
+	} );
 
-	//Disable debug info for "a significant performance boost" https://docs.angularjs.org/api/ng/provider/$compileProvider#debugInfoEnabled
+	// Disable debug info for "a significant performance boost" https://docs.angularjs.org/api/ng/provider/$compileProvider#debugInfoEnabled
 	$compileProvider.debugInfoEnabled( false ); //Comment out for debugging
 } );
 
@@ -73,8 +75,8 @@ function UpdateDigest( scope, timeout )
 	}, timeout );
 }
 
-//We already have a limitTo filter built-in to angular,
-//let's make a startFrom filter
+// We already have a limitTo filter built-in to angular,
+// let's make a startFrom filter
 App.filter( 'startFrom', function()
 {
 	return function( input, start )
