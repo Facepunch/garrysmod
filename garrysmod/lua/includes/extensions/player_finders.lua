0074bebd7d51690fc797f6efee5e764152c00b76
noaccessl <rodion.noaccessl@gmail.com>
2025-04-06 17:00:25 +0300

    Rework
    * OnEntityCreated hook for update -> Validation-in-place
    * EntityRemoved hook for GC -> Timed GC

--- garrysmod/lua/includes/extensions/player_finders.lua
+++ garrysmod/lua/includes/extensions/player_finders.lua
@@ -1,143 +1,125 @@
 
+--
+-- Validator
+--
+local player_metatable = FindMetaTable( "Player" )
+
+local function Validate( idType, id, map )
+
+	local GetThisID = player_metatable[idType]
+
+	for _, ply in player.Iterator() do
+
+		if ( id == GetThisID( ply ) ) then
+
+			map[id] = ply
+			return ply
+
+		end
+
+	end
+
+	return false
+
+end
+
 --[[---------------------------------------------------------
 	Name: GetByAccountID( ID )
-	Desc: Attempts to get the player by the AccountID
+	Desc: Attempts to get the player by the passed AccountID
 -----------------------------------------------------------]]
 local AccountIDMap = {}
 
 function player.GetByAccountID( ID )
 
-	ID = tostring( ID )
 	local ply = AccountIDMap[ID]
 
-	if ( ply ) then
+	if ( IsValid( ply ) ) then
 		return ply
 	end
 
-	return false
+	return Validate( "AccountID", ID, AccountIDMap )
 
 end
 
 --[[---------------------------------------------------------
 	Name: GetByUniqueID( ID )
-	Desc: Attempts to get the player by the UniqueID
+	Desc: Attempts to get the player by the passed UniqueID
 -----------------------------------------------------------]]
 local UniqueIDMap = {}
 
 function player.GetByUniqueID( ID )
 
-	ID = tostring( ID )
 	local ply = UniqueIDMap[ID]
 
-	if ( ply ) then
+	if ( IsValid( ply ) ) then
 		return ply
 	end
 
-	return false
+	return Validate( "UniqueID", ID, UniqueIDMap )
 
 end
 
 --[[---------------------------------------------------------
 	Name: GetBySteamID( ID )
-	Desc: Attempts to get the player by the SteamID
+	Desc: Attempts to get the player by the passed SteamID
 -----------------------------------------------------------]]
 local SteamIDMap = {}
 
 function player.GetBySteamID( ID )
 
-	ID = string.upper( tostring( ID ) )
+	ID = string.upper( ID )
 	local ply = SteamIDMap[ID]
 
-	if ( ply ) then
+	if ( IsValid( ply ) ) then
 		return ply
 	end
 
-	return false
+	return Validate( "SteamID", ID, SteamIDMap )
 
 end
 
 --[[---------------------------------------------------------
 	Name: GetBySteamID64( ID )
-	Desc: Attempts to get the player by the SteamID64
+	Desc: Attempts to get the player by the passed SteamID64
 -----------------------------------------------------------]]
 local SteamID64Map = {}
 
 function player.GetBySteamID64( ID )
 
-	ID = tostring( ID )
 	local ply = SteamID64Map[ID]
 
-	if ( ply ) then
+	if ( IsValid( ply ) ) then
 		return ply
 	end
 
-	return false
+	return Validate( "SteamID64", ID, SteamID64Map )
 
 end
 
---[[---------------------------------------------------------
-	Manage the maps
------------------------------------------------------------]]
-hook.Add( "OnEntityCreated", "PlayerFinders", function( ent )
+--
+-- GC for the maps
+--
+local inext = ipairs( {} )
+local next = next
 
-	if ( not ent:IsPlayer() ) then
-		return
+local maps = { AccountIDMap; UniqueIDMap; SteamIDMap; SteamID64Map }
+
+local function PlayerFindersGC()
+
+	local IsValid = IsValid
+
+	for _, map in inext, maps, 0 do
+
+		for id, ply in next, map do
+
+			if ( not IsValid( ply ) ) then
+				map[id] = nil
+			end
+
+		end
+
 	end
 
-	local ply = ent
+end
 
-	--
-	-- Store in the maps
-	--
-	local AID = tostring( ply:AccountID() )
-	local UID = tostring( ply:UniqueID() )
-	local SID = ply:SteamID()
-	local S64 = ply:SteamID64()
-
-	AccountIDMap[AID] = ply
-	AccountIDMap[ply] = AID
-
-	UniqueIDMap[UID] = ply
-	UniqueIDMap[ply] = UID
-
-	SteamIDMap[SID] = ply
-	SteamIDMap[ply] = SID
-
-	SteamID64Map[S64] = ply
-	SteamID64Map[ply] = S64
-
-end )
-
-hook.Add( "EntityRemoved", "PlayerFinders", function( ent, fullUpdate )
-
-	if ( fullUpdate ) then
-		return
-	end
-
-	if ( not ent:IsPlayer() ) then
-		return
-	end
-
-	local ply = ent
-
-	--
-	-- Clear from the maps
-	--
-	local AID = AccountIDMap[ply]
-	local UID = UniqueIDMap[ply]
-	local SID = SteamIDMap[ply]
-	local S64 = SteamID64Map[ply]
-
-	AccountIDMap[AID] = nil
-	AccountIDMap[ply] = nil
-
-	UniqueIDMap[UID] = nil
-	UniqueIDMap[ply] = nil
-
-	SteamIDMap[SID] = nil
-	SteamIDMap[ply] = nil
-
-	SteamID64Map[S64] = nil
-	SteamID64Map[ply] = nil
-
-end )
+timer.Create( "PlayerFinders_GC", 30, 0, PlayerFindersGC )
