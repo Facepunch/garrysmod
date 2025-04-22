04ce80298fb40f928862956f401bc3aeca81fc86
TheObtey <95309102+TheObtey@users.noreply.github.com>
2025-04-22 23:36:50 +0200

    Update sent_ball.lua
    Improved the entire system ( ﾉ ﾟｰﾟ)ﾉ

--- garrysmod/lua/entities/sent_ball.lua
+++ garrysmod/lua/entities/sent_ball.lua
@@ -27,12 +27,11 @@ function ENT:SetupDataTables()
 
 end
 
--- This is the spawn function. It's called when a client calls the entity to be spawned.
--- If you want to make your SENT spawnable you need one of these functions to properly create the entity
+-- This is the spawn function. It is called when a client calls the entity to be spawned.
+-- If you want to make your SENT spawnable, you need one of these functions to properly create the entity
 --
 -- ply is the name of the player that is spawning it
 -- tr is the trace from the player's eyes
---
 function ENT:SpawnFunction( ply, tr, ClassName )
 
 	if ( !tr.Hit ) then return end
