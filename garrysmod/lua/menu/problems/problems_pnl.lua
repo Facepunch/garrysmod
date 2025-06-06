27e085ad0272bba783543023ceb83a5694736d51
darkfated <49955245+darkfated@users.noreply.github.com>
2025-06-06 19:04:50 +0300

    Improving the problems menu (#2261)
    * Optimize problem list handling
    
    * Optimize error panels sorting algorithm
    
    * Switch space to tabs
    
    * Add ESCAPE handling to close problems menu
    
    * Small fix
    
    * Cancellation of problem list handling
    
    * Use ESC in MainMenu
    
    * Another way to close the menu
    
    * Return close btn
    
    * Remove unnecessary change
    
    * Remove unnecessary changes to ProblemsPanel
    
    Simpler method exists
    
    ---------
    
    Co-authored-by: Rubat <3299036+robotboy655@users.noreply.github.com>

--- garrysmod/lua/menu/problems/problems_pnl.lua
+++ garrysmod/lua/menu/problems/problems_pnl.lua
@@ -48,6 +48,22 @@ function PANEL:Init()
 
 end
 
+function PANEL:OnMousePressed( mcode )
+
+	if ( mcode == MOUSE_LEFT ) then
+		self:Remove()
+	end
+
+end
+
+function PANEL:Think()
+
+	if ( input.IsKeyDown( KEY_ESCAPE ) and !IsInGame() ) then
+		self:Remove()
+	end
+
+end
+
 function PANEL:AddEmptyWarning( txt, parent )
 
 	local lab = parent:Add( "DLabel" )
@@ -66,9 +82,11 @@ function PANEL:AddEmptyWarning( txt, parent )
 
 end
 
+local color_background = Color( 0, 0, 0, 240 )
+
 function PANEL:Paint( w, h )
 
-	draw.RoundedBox( 0, 0, 0, w, h, Color( 0, 0, 0, 240 ) )
+	draw.RoundedBox( 0, 0, 0, w, h, color_background )
 
 end
 
