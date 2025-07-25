e269f826c987f37037e9d1cc10ffe92e9e2dcd92
AlexG345 <alexgernez1@gmail.com>
2025-07-25 14:25:40 +0200

    motor bug fix

--- garrysmod/lua/includes/modules/constraint.lua
+++ garrysmod/lua/includes/modules/constraint.lua
@@ -1083,6 +1083,8 @@ function Motor( Ent1, Ent2, Bone1, Bone2, LPos1, LPos2, friction, torque, forcet
 		Constraint:SetPhysConstraintObjects( Phys1, Phys1 )
 		Constraint:Spawn()
 		Constraint:Activate()
+		Constraint:Fire( "Scale", 0 )
+        Constraint:Fire( "Activate" )
 
 	onFinishConstraint()
 
