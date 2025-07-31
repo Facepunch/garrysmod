efd5a860cef840f5e77c713d0d553ecd161cf485
riggs9162 <riggs9162@gmx.de>
2025-07-31 14:29:09 +0200

    Add input validation to ents.FindInCube
    Added checks to ensure 'center' is a Vector and 'radius' is a positive number in ents.FindInCube.

--- garrysmod/lua/includes/extensions/ents.lua
+++ garrysmod/lua/includes/extensions/ents.lua
@@ -21,6 +21,14 @@ end
 
 function ents.FindInCube( center, radius )
 
+	if ( !isvector( center ) ) then
+		return error( "Invalid argument: 'center' must be a Vector" )
+	end
+
+	if ( !isnumber( radius ) or radius <= 0 ) then
+		return error( "Invalid argument: 'radius' must be a positive number" )
+	end
+
 	local rvec = Vector( radius, radius, radius )
 
 	return ents.FindInBox( center - rvec, center + rvec )
