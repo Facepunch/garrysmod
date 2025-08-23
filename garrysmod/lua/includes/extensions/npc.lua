99abb987b88d3f3e1bbc1dfd428c79673a0dec91
bloodycop <89155277+bloodycop6385@users.noreply.github.com>
2025-08-23 03:09:03 +0300

    Rename method name for consistency

--- garrysmod/lua/includes/extensions/npc.lua
+++ garrysmod/lua/includes/extensions/npc.lua
@@ -2,7 +2,7 @@ local meta = FindMetaTable( "NPC" )
 if ( !meta ) then return end
 
 if ( SERVER ) then
-    function meta:HasCapability( cap )
+    function meta:CapabilitiesHas( cap )
         return bit.band( self:CapabilitiesGet(), cap ) == cap
     end
 end
