50afb8336ef5e748215491c977950a4e57e6f695
DarthTealc <DarthTealc@users.noreply.github.com>
2025-09-30 23:49:49 +1000

    Swap flags from 16x11 to 72x52
    Flags are now higher resolution, and much more up-to-date.
    
    Flag graphics sourced from Twemoji 16.0.1 https://github.com/jdecked/twemoji
    Twemoji's graphics are licensed under CC-BY 4.0 https://github.com/jdecked/twemoji/blob/main/LICENSE-GRAPHICS https://creativecommons.org/licenses/by/4.0/
    Unicode "regional indicator symbols" range 1F1E6 to 1F1FF - Icons were cherry-picked from this range.
    Graphics are unmodified.
    
    Additional regions available for server list: South Sudan (SS), Canary Islands IC), Kosovo (XK).

--- garrysmod/html/js/menu/control.Servers.js
+++ garrysmod/html/js/menu/control.Servers.js
@@ -327,6 +327,28 @@ function ControllerServers( $scope, $element, $rootScope, $location )
 		return local + "\n" + eng; // Return the localised version, with the English version on a second line
 		
 	}
+	
+	$scope.DisplayingAnyFlags = function()
+	{
+		// Is the server list showing any servers with flags other than the blank "unknown"?
+		//
+		// CEF 105+ can just do:
+		//   ".serverlist:has(.server .flag:not(.noflag))" for the same as DisplayingAnyFlags() == true
+		//	 ".serverlist:not(:has(.server .flag:not(.noflag)))" for the same as DisplayingAnyFlags() == false
+		//
+		// But Awesomium and CEF 86 require this javascript function
+		
+		if ( !$scope.CurrentGamemode ) return false;
+		
+		if ( !$scope.CurrentGamemode.hasflags ) return false;
+		if ( !$scope.CurrentGamemode.flags[ "unknown" ] ) return true;
+		if ( $scope.CurrentGamemode.FilterFlags[ "unknown" ] === false ) return true;
+		if ( $scope.CurrentGamemode.HasPreferFlags && $scope.CurrentGamemode.FilterFlags[ "unknown" ] != true ) return true;
+		
+		if ( !$scope.filteredServers ) return false;
+
+		return $scope.filteredServers.some( function( svr ) { return svr.flag && svr.flag != "unknown" } ) ;
+	}
 
 	$scope.serverFilter = function( server )
 	{
@@ -454,11 +476,11 @@ function CalculateRank( server )
 	return recommended;
 }
 
-// Gmod's flag16 icons
-var flagicons = [ "ad", "ae", "af", "ag", "ai", "al", "am", "an", "ao", "ar", "as", "at", "au", "aw", "ax", "az", "ba", "bb", "bd", "be", "bf", "bg", "bh", "bi", "bj", "bm", "bn", "bo", "br", "bs", "bt", "bv", "bw", "by", "bz", "ca", "catalonia", "cc", "cd", "cf", "cg", "ch", "ci", "ck", "cl", "cm", "cn", "co", "cr", "cs", "cu", "cv", "cx", "cy", "cz", "de", "dj", "dk", "dm", "do", "dz", "ec", "ee", "eg", "eh", "england", "er", "es", "et", "europeanunion", "fi", "fj", "fk", "fm", "fo", "fr", "ga", "gb", "gd", "ge", "gf", "gh", "gi", "gl", "gm", "gn", "gp", "gq", "gr", "gs", "gt", "gu", "gw", "gy", "hk", "hm", "hn", "hr", "ht", "hu", "id", "ie", "il", "in", "io", "iq", "ir", "is", "it", "jm", "jo", "jp", "ke", "kg", "kh", "ki", "km", "kn", "kp", "kr", "kw", "ky", "kz", "la", "lb", "lc", "li", "lk", "lr", "ls", "lt", "lu", "lv", "ly", "ma", "mc", "md", "me", "mg", "mh", "mk", "ml", "mm", "mn", "mo", "mp", "mq", "mr", "ms", "mt", "mu", "mv", "mw", "mx", "my", "mz", "na", "nc", "ne", "nf", "ng", "ni", "nl", "no", "np", "nr", "nu", "nz", "om", "pa", "pe", "pf", "pg", "ph", "pk", "pl", "pm", "pn", "pr", "ps", "pt", "pw", "py", "qa", "re", "ro", "rs", "ru", "rw", "sa", "sb", "sc", "scotland", "sd", "se", "sg", "sh", "si", "sj", "sk", "sl", "sm", "sn", "so", "sr", "st", "sv", "sy", "sz", "tc", "td", "tf", "tg", "th", "tj", "tk", "tl", "tm", "tn", "to", "tr", "tt", "tv", "tw", "tz", "ua", "ug", "um", "us", "uy", "uz", "va", "vc", "ve", "vg", "vi", "vn", "vu", "wales", "wf", "ws", "ye", "yt", "za", "zm", "zw" ];
+// Gmod's flags72 icons
+var flagicons = [ "ad", "ae", "af", "ag", "ai", "al", "am", "ao", "ar", "as", "at", "au", "aw", "ax", "az", "ba", "bb", "bd", "be", "bf", "bg", "bh", "bi", "bj", "bm", "bn", "bo", "br", "bs", "bt", "bv", "bw", "by", "bz", "ca", "cc", "cd", "cf", "cg", "ch", "ci", "ck", "cl", "cm", "cn", "co", "cr", "cu", "cv", "cx", "cy", "cz", "de", "dj", "dk", "dm", "do", "dz", "ec", "ee", "eg", "eh", "er", "es", "et", "eu", "fi", "fj", "fk", "fm", "fo", "fr", "ga", "gb", "gd", "ge", "gf", "gh", "gi", "gl", "gm", "gn", "gp", "gq", "gr", "gs", "gt", "gu", "gw", "gy", "hk", "hm", "hn", "hr", "ht", "hu", "ic", "id", "ie", "il", "in", "io", "iq", "ir", "is", "it", "jm", "jo", "jp", "ke", "kg", "kh", "ki", "km", "kn", "kp", "kr", "kw", "ky", "kz", "la", "lb", "lc", "li", "lk", "lr", "ls", "lt", "lu", "lv", "ly", "ma", "mc", "md", "me", "mg", "mh", "mk", "ml", "mm", "mn", "mo", "mp", "mq", "mr", "ms", "mt", "mu", "mv", "mw", "mx", "my", "mz", "na", "nc", "ne", "nf", "ng", "ni", "nl", "no", "np", "nr", "nu", "nz", "om", "pa", "pe", "pf", "pg", "ph", "pk", "pl", "pm", "pn", "pr", "ps", "pt", "pw", "py", "qa", "re", "ro", "rs", "ru", "rw", "sa", "sb", "sc", "sd", "se", "sg", "sh", "si", "sj", "sk", "sl", "sm", "sn", "so", "sr", "ss", "st", "sv", "sy", "sz", "tc", "td", "tf", "tg", "th", "tj", "tk", "tl", "tm", "tn", "to", "tr", "tt", "tv", "tw", "tz", "ua", "ug", "um", "us", "uy", "uz", "va", "vc", "ve", "vg", "vi", "vn", "vu", "wf", "ws", "xk", "ye", "yt", "za", "zm", "zw" ];
 // Special remapping cases
-var locationremap = { "en":"gb","eng":"gb","uk":"gb", "eu":"europeanunion", "ger":"de", "rus":"ru", "usa":"us" };
-// Valid sv_location list = remaps plus flag16. Any sv_locations not on the list are filtered and fallback to secondary methods such as extraction from server name.
+var locationremap = { "en":"gb","eng":"gb","uk":"gb", "ger":"de", "rus":"ru", "usa":"us" };
+// Valid sv_location list = remaps plus flag72. Any sv_locations not on the list are filtered and fallback to secondary methods such as extraction from server name.
 var svlocations = Object.keys( locationremap ).concat( flagicons );
 
 // Generate a flag from server name if the server doesn't have it set.
@@ -625,8 +647,9 @@ function MissingGamemodeIcon( element )
 
 function MissingFlag( element )
 {
-	element.src = "img/unk_flag.png";
-	element.classList.add( "noflag" );
+	element.src = "img/twemoji_2754.png";
+	element.style.webkitMaskImage = "";
+	element.parentElement.classList.add( "noflag" );
 	return true;
 }
 
