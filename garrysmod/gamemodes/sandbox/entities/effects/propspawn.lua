4558c5e3a3082493477c9415776b8358e1c70345
Rubat <3299036+robotboy655@users.noreply.github.com>
2025-12-11 22:47:27 +0200

    Improve prop spawn effect handing of depth passes

--- garrysmod/gamemodes/sandbox/entities/effects/propspawn.lua
+++ garrysmod/gamemodes/sandbox/entities/effects/propspawn.lua
@@ -100,19 +100,24 @@ function EFFECT:RenderOverlay( entity )
 
 end
 
-function EFFECT:RenderParent()
+function EFFECT:RenderParent( flags )
+
+	-- Do not draw some things during depth passes
+	local isDepthPass = ( bit.band( flags, STUDIO_SSAODEPTHTEXTURE ) != 0 || bit.band( flags, STUDIO_SHADOWDEPTHTEXTURE ) != 0 )
 
 	if ( !IsValid( self ) ) then return end
 	if ( !IsValid( self.SpawnEffect ) ) then self.RenderOverride = nil return end
 
 	local bClipping = self.SpawnEffect:StartClip( self, 1 )
 
-	self:DrawModel()
+	self:DrawModel( flags )
 
 	render.PopCustomClipPlane()
 	render.EnableClipping( bClipping )
 
-	self.SpawnEffect:RenderOverlay( self )
+	if ( !isDepthPass ) then
+		self.SpawnEffect:RenderOverlay( self )
+	end
 
 end
 
