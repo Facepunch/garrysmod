0a04f78b2b530c417f92ff810c8102d275a49d3a
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2026-01-05 18:38:56 +0300

    Fix edit entity lua error (#2412)
    * Fix edit entity lua error
    
    Prob regresession after #2317
    
    * Fix newlines
    
    * Woops
    
    * Fix newlines

--- garrysmod/lua/autorun/properties/editentity.lua
+++ garrysmod/lua/autorun/properties/editentity.lua
@@ -19,6 +19,7 @@ properties.Add( "editentity", {
 
 	Action = function( self, ent )
 
+		if ( !IsValid( ent ) ) then return end
 		local printName = ent.PrintName
 
 		if ( !isstring( printName ) ) then
