b57ace8b478dabb96f0c208231d8bc762cceccd0
grant <80866912+acnbhop@users.noreply.github.com>
2026-01-10 00:16:18 -0600

    Save cursor position on Spawnmenu on Linux
    Save the cursor position when we re-open the spawnmenu once we closed it, for some reason (I am assuming it's relating to the game code), the cursor resets to the center every time you re-open the spawnmenu.
    
    This doesn't happen on Windows.

---
+++ garrysmod/lua/autorun/client/linux_cursor.lua
@@ -0,0 +1,40 @@
+if ( SERVER ) then return end
+
+if ( not system.IsLinux() ) then return end
+
+local lastX = ScrW() / 2
+local lastY = ScrH() / 2
+local forceFrames = 3
+
+hook.Add( "OnSpawnMenuClose", "SaveMenuCursorLinux", function()
+
+	lastX, lastY = input.GetCursorPos()
+
+end )
+
+hook.Add( "OnSpawnMenuOpen", "RestoreMenuCursorLinux", function()
+
+	local framesCount = 0
+
+	hook.Add( "Think", "ForceCursorPosLinuxFix", function()
+
+		if ( not g_SpawnMenu:IsVisible() ) then
+
+			hook.Remove( "Think", "ForceCursorPosLinuxFix" )
+			return
+
+		end
+
+		input.SetCursorPos( lastX, lastY )
+
+		framesCount = forceFrames + 1
+
+		if ( framesCount >= forceFrames ) then
+
+			hook.Remove( "Think", "ForceCursorPosLinuxFix" )
+
+		end
+
+	end )
+
+end )
