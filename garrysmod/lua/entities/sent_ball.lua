0c537fb4f8590a09b196f270fb67dfb96b5fc979
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2026-03-13 20:40:40 +0300

    Fix bouncy ball bouncing infinitely at low tickrate
    I tested this on a 33-tick server and didn't notice any changes in behavior. But the issue went away
    
    Fixes (#6759)[https://github.com/Facepunch/garrysmod-issues/issues/6759]

--- garrysmod/lua/entities/sent_ball.lua
+++ garrysmod/lua/entities/sent_ball.lua
@@ -140,7 +140,7 @@ function ENT:PhysicsCollide( data, physobj )
 
 	local TargetVelocity = NewVelocity * LastSpeed * 0.9
 
-	physobj:SetVelocity( TargetVelocity )
+	physobj:SetVelocityInstantaneous( TargetVelocity )
 
 end
 
