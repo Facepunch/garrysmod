77c84ddd1556d6a84f6f6f302a04bf28c8ebd805
Akabenko <danlimons322@gmail.com>
2026-04-20 09:37:25 +0300

    `string.ToColor` `.rgb` suppot without alpha input
    `string.ToColor` by default returns `Color(255, 255, 255, 255)` as fallback, if argument to input has no alpha value. This pull request allow to input string of color wihtout alpha value. For example `128 0 0` as argument will return same Color.

--- garrysmod/lua/includes/extensions/string.lua
+++ garrysmod/lua/includes/extensions/string.lua
@@ -343,6 +343,7 @@ end
 function string.ToColor( str )
 
 	local r, g, b, a = string.match( str, "(%d+) (%d+) (%d+) (%d+)" )
+	if ( !a ) then r, g, b = string.match( str, "(%d+) (%d+) (%d+)" )
 	return Color( tonumber( r ) or 255, tonumber( g ) or 255, tonumber( b ) or 255, tonumber( a ) or 255 )
 
 end
