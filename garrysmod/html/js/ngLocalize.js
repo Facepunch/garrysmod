c31396977f9de074e91d6fc33e787e83d0597b59
DarthTealc <DarthTealc@users.noreply.github.com>
2026-05-08 00:45:17 +1000

    Set lang attribute on localised elements

--- garrysmod/html/js/ngLocalize.js
+++ garrysmod/html/js/ngLocalize.js
@@ -1,6 +1,7 @@
 
 var languageCache = {};
 var languageCurrent = "";
+var languageCurrentAttr = "en";
 
 angular.module( "ngLocalize", [] )
 
@@ -39,6 +40,9 @@ angular.module( "ngLocalize", [] )
 
 		var updateElement = function( str )
 		{
+			if ( !IS_SPAWN_MENU && element.attr( "lang" ) != languageCurrentAttr )
+				element.attr( "lang", languageCurrentAttr );
+
 			if ( "placeholder" in element[0] )
 			{
 				if ( element.attr( "placeholder" ) != str )
@@ -63,6 +67,7 @@ angular.module( "ngLocalize", [] )
 			if ( languageCurrent != gScope.Language )
 			{
 				languageCurrent = gScope.Language;
+				languageCurrentAttr = ( languageCurrent == "" || languageCurrent == "en-pt" ) ? "en" : languageCurrent;
 				languageCache = {};
 			}
 			update();
