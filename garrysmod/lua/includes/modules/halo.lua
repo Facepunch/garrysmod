fc36b65f3b5e2bd2edc4fd355739c2d50006a3d6
Evgeny Akabenko <danlimons322@gmail.com>
2026-06-06 22:53:48 +0300

    Update halo.lua

--- garrysmod/lua/includes/modules/halo.lua
+++ garrysmod/lua/includes/modules/halo.lua
@@ -9,7 +9,7 @@ local rt_Blur		= render.GetScreenEffectTexture( 1 )
 
 local List = {}
 local RenderEnt = NULL
-local flags = bit.band( STUDIO_RENDER, STUDIO_SKIP_DECALS )
+local flags = bit.bor( STUDIO_RENDER, STUDIO_SKIP_DECALS )
 
 function Add( entities, color, blurx, blury, passes, add, ignorez )
 
