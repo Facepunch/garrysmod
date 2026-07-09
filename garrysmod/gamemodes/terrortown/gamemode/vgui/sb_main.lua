926ae9064977074b24b7eae70b05e3bdbe7636d8
Tiagoquix <30274161+Tiagoquix@users.noreply.github.com>
2026-07-08 22:36:17 -0300

    [TTT] Fix italic fonts on Linux
    This creates a new gloval variable that sets the font to DejaVu Sans instead of Tahoma if it detects that the system is Linux.
    
    The current Tahoma definition makes the fallback font render as italic, somehow. This fixes the italic font problem in many places (scoreboard, HUD, C4 menu, weapon menu etc.).
    
    Co-Authored-By: Samuel R. R. <64384760+razorzinho@users.noreply.github.com>

--- garrysmod/gamemodes/terrortown/gamemode/vgui/sb_main.lua
+++ garrysmod/gamemodes/terrortown/gamemode/vgui/sb_main.lua
@@ -18,7 +18,7 @@ surface.CreateFont("cool_small", {font = "coolvetica",
 surface.CreateFont("cool_large", {font = "coolvetica",
                                   size = 24,
                                   weight = 400})
-surface.CreateFont("treb_small", {font = "Tahoma",
+surface.CreateFont("treb_small", {font = GAMEMODE_DEFAULT_UI_FONT,
                                   size = 14,
                                   weight = 700})
 
