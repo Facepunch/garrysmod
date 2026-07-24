7c2b70f3bb1db121795d459d5c30d7fa6c5fe273
Vitroze <azouaouiaziz4@gmail.com>
2026-07-24 20:13:50 +0200

    Adding an error message

--- garrysmod/lua/autorun/properties/kinect_controller.lua
+++ garrysmod/lua/autorun/properties/kinect_controller.lua
@@ -64,7 +64,12 @@ properties.Add( "motioncontrol_ragdoll", {
 		end
 
 		local ragdoll_motion = ents.Create( "ragdoll_motion" )
-		if ( !IsValid( ragdoll_motion ) ) then return end
+		if ( !IsValid( ragdoll_motion ) ) then
+			ServerLog( "Failed to create ragdoll_motion entity for " .. ent:GetClass() .. " (" .. ent:EntIndex() .. ")!\n" )
+			player:PrintMessage( HUD_PRINTTALK, "Failed to create ragdoll_motion entity for " .. ent:GetClass() )
+			return
+		end
+
 		ragdoll_motion:SetPos( player:EyePos() + player:EyeAngles():Forward() * 10 )
 		ragdoll_motion:SetAngles( Angle( 0, player:EyeAngles().yaw, 0 ) )
 		ragdoll_motion:SetRagdoll( ent )
