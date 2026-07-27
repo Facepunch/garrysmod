7f888962026a78cd5034190c59234571eb397734
Rubat <3299036+robotboy655@users.noreply.github.com>
2026-07-27 13:05:13 -0700

    Update vector.lua

--- garrysmod/lua/includes/extensions/vector.lua
+++ garrysmod/lua/includes/extensions/vector.lua
@@ -4,7 +4,7 @@ local meta = FindMetaTable( "Vector" )
 --[[---------------------------------------------------------
 	Converts Vector To Color - alpha precision lost, must reset
 -----------------------------------------------------------]]
-function meta:ToColor( )
+function meta:ToColor()
 
 	local x, y, z = meta.Unpack( self )
 	return Color( x * 255, y * 255, z * 255 )
