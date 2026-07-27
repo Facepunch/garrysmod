331ceb54cf8e87551b7e8e8869572189d173cc47
Rubat <3299036+robotboy655@users.noreply.github.com>
2026-07-27 13:04:50 -0700

    Apply the extra optimization, fix whitespace/comments

--- garrysmod/lua/includes/extensions/vector.lua
+++ garrysmod/lua/includes/extensions/vector.lua
@@ -1,13 +1,12 @@
-local meta = FindMetaTable( "Vector" )
 
--- Nothing in here, still leaving this file here just in case
+local meta = FindMetaTable( "Vector" )
 
 --[[---------------------------------------------------------
 	Converts Vector To Color - alpha precision lost, must reset
 -----------------------------------------------------------]]
 function meta:ToColor( )
 
-	local x, y, z = self:Unpack()
+	local x, y, z = meta.Unpack( self )
 	return Color( x * 255, y * 255, z * 255 )
 
 end
