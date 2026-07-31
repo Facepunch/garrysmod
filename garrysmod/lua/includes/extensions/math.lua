19fb8a0e3a51d702ed65ab6d03b3b928eb86e74a
Rubat <3299036+robotboy655@users.noreply.github.com>
2026-07-31 21:02:19 +0300

    Remove math.Cot
    It's too simple of a function, shouldn't have included it in the first place

--- garrysmod/lua/includes/extensions/math.lua
+++ garrysmod/lua/includes/extensions/math.lua
@@ -271,12 +271,3 @@ function math.IsNearlyEqual( a, b, tolerance )
 
 	return math.abs( a - b ) <= tolerance
 end
-
---[[---------------------------------------------------------
-	Name: Cot( num )
-	Desc: Calculate cotangent of num
------------------------------------------------------------]]
-function math.Cot( num )
-	return 1 / math.tan( num )
-end
-
