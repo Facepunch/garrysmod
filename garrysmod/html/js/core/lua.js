e1164d7dbf52f07211bd3ea90b4c57133c539dfc
darkfated <darkfated@icloud.com>
2026-08-25 14:43:04 +0300

    Simplify Lua bridge

--- garrysmod/html/js/core/lua.js
+++ garrysmod/html/js/core/lua.js
@@ -1,50 +1,15 @@
-function luaFormat(cmd, args) {
-	var str = "";
-	var arg = 0;
-
-	for (var i = 0; i < cmd.length; i++) {
-		if (cmd[i] === "%") {
-			i++;
-
-			if (cmd[i] === "s") {
-				str +=
-					'"' + String(args[arg++]).replace(/["\\]/g, "\\$&") + '"';
-				continue;
-			}
-
-			if (cmd[i] === "i") {
-				str += args[arg++];
-				continue;
-			}
-		}
-
-		str += cmd[i];
-	}
-
-	return str;
-}
-
-function luaArgs(args) {
-	return Array.prototype.slice.call(args);
-}
-
 function luaRun(cmd) {
-	var args = luaArgs(arguments).slice(1);
+	var args = Array.prototype.slice.call(arguments, 1);
 
-	if (typeof lua !== "undefined" && typeof lua.Run === "function") {
+	if (typeof lua !== "undefined") {
 		lua.Run.apply(lua, [cmd].concat(args));
 		return;
 	}
 
-	console.log("RUNLUA:" + (args.length ? luaFormat(cmd, args) : cmd));
+	console.log("RUNLUA:", cmd, args.join(" "));
 }
 
 function luaPlaySound(name) {
-	if (typeof lua !== "undefined" && typeof lua.PlaySound === "function") {
-		lua.PlaySound(String(name));
-		return;
-	}
-
 	luaRun("surface.PlaySound( %s )", String(name));
 }
 
